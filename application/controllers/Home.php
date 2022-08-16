4ff7fe2b89f3b1cafd7f6e6a405272d0b07977de
Micah Villaruz <mmvillaruz@iskolarngbayan.pup.edu.ph>
2022-08-16 10:57:03 +0800

    Add Landing Page

---
+++ application/controllers/Home.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Home extends CI_Controller
+{
+
+	public function index()
+	{
+		$this->load->view('home/landing');
+	}
+}
