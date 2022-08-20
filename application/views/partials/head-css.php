6047567dbfcf28d1e6b558ed406a333b46667870
Micah Villaruz <mmvillaruz@iskolarngbayan.pup.edu.ph>
2022-08-21 01:47:32 +0800

    Add Memorandum Orders page

--- application/views/partials/head-css.php
+++ application/views/partials/head-css.php
@@ -1,15 +1,18 @@
-	<!--Swiper slider css-->
-	<link href="<?= base_url() ?>public/libs/swiper/swiper-bundle.min.css" rel="stylesheet" type="text/css" />
+		<!-- glightbox css -->
+		<link rel="stylesheet" href="<?= base_url() ?>public/libs/glightbox/css/glightbox.min.css" />
 
-	<!-- Layout config Js -->
-	<script src="<?= base_url() ?>public/js/layout.js"></script>
-	<!-- Bootstrap Css -->
-	<link href="<?= base_url() ?>public/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-	<!-- Icons Css -->
-	<link href="<?= base_url() ?>public/css/icons.min.css" rel="stylesheet" type="text/css" />
-	<!-- App Css-->
-	<link href="<?= base_url() ?>public/css/app.min.css" rel="stylesheet" type="text/css" />
-	<!-- custom Css-->
-	<link href="<?= base_url() ?>public/css/custom.css" rel="stylesheet" type="text/css" />
+		<!--Swiper slider css-->
+		<link href="<?= base_url() ?>public/libs/swiper/swiper-bundle.min.css" rel="stylesheet" type="text/css" />
 
-	</head>
+		<!-- Layout config Js -->
+		<script src="<?= base_url() ?>public/js/layout.js"></script>
+		<!-- Bootstrap Css -->
+		<link href="<?= base_url() ?>public/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+		<!-- Icons Css -->
+		<link href="<?= base_url() ?>public/css/icons.min.css" rel="stylesheet" type="text/css" />
+		<!-- App Css-->
+		<link href="<?= base_url() ?>public/css/app.min.css" rel="stylesheet" type="text/css" />
+		<!-- custom Css-->
+		<link href="<?= base_url() ?>public/css/custom.css" rel="stylesheet" type="text/css" />
+
+		</head>
