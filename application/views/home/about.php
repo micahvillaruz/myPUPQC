874288b1146f36db41bb3691a051be5fed3affc7
Micah Villaruz <mmvillaruz@iskolarngbayan.pup.edu.ph>
2022-08-20 17:48:08 +0800

    Modify University Profile page

---
+++ application/views/home/about.php
@@ -0,0 +1,190 @@
+<body>
+  <!-- Begin page -->
+  <div id="layout-wrapper">
+    <!-- ============================================================== -->
+    <!-- Start right Content here -->
+    <!-- ============================================================== -->
+    <div class="main-content ms-0 bg-light">
+      <div class="page-content pt-4">
+        <div class="container-fluid">
+          <div class="profile-foreground position-relative mx-n4 mt-n4">
+            <div class="profile-wid-bg">
+              <img src="<?= base_url() ?>public/images/pupqc-bg.jpg" alt="" class="profile-wid-img" />
+            </div>
+          </div>
+          <div class="pt-4 mb-4 mb-lg-3 pb-lg-4">
+            <div class="row g-4">
+              <div class="col-auto">
+                <div class="avatar-lg">
+                  <img src="<?= base_url() ?>public/images/pupqc-logo.png" class="img-thumbnail rounded-circle" />
+                </div>
+              </div>
+              <!--end col-->
+              <div class="col">
+                <div class="p-2">
+                  <h3 class="text-white mb-3">Poltechnic University of the Philippines</h3>
+                  <h3 class="text-white">Quezon City Branch</h3>
+                  <div class="hstack text-white-50 gap-1">
+                    <div class="me-2">
+                      <i class="ri-road-map-line me-1 text-white-75 fs-16 align-middle"></i>
+                      <span class="ms-1">Don Fabian St., Commonwealth Quezon City</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!--end col-->
+            </div>
+            <!--end row-->
+          </div>
+
+          <div class="row">
+            <div class="col-lg-12">
+              <div>
+                <div class="row pt-4 text-muted">
+                  <div class="col-xxl-9">
+                    <div class="card">
+                      <div class="card-body">
+                        <h3 class="mb-3 text-primary">About</h3>
+                        <div class="mx-3">
+                          <div class="mb-3 d-flex justify-content-center">
+                            <img src="<?= base_url() ?>public/images/about/img-1.jpg" alt="" height="400">
+                          </div>
+                          <p>
+                            The Polytechnic University of the Philippines Quezon City (abbreviated as PUPQC; also known as PUP Commonwealth Campus) is one of the satellite campuses of the Polytechnic University of the Philippines located in Don Fabian Street., Barangay Commonwealth, Quezon City, Philippines. It was established in 1997. It confers undergraduate and diploma degrees.
+                          </p>
+                          <p>
+                            The vision of PUP QC is to become a committed and comprehensive institution of learning that provides a culture of excellence. It is to become the pioneering and leading National Polytechnic University in the 21st century. With a National Polytechnic University status, it will bring more opportunities for the university to further strengthen its academic programs and improve organizational performance as an institution for a higher education.
+                          </p>
+                          <p>
+                            In pursuit of this vision, the mission of PUP QC is to become responsive to the needs of its stakeholders by being committed in promoting the cultural and academic excellence guided with ethical and moral leadership in pursuit of quality education. It should recognize its catalytic role for national development. PUP QC will ensure inclusive and equitable quality education and promote life-long learning opportunities through re-engineered polytechnic education. By re-engineering, existing processes and programs are looked at in order to achieve higher quality of academic programs and services, following the principles of effectiveness and efficiency.
+                          </p>
+                          <p>
+                            With this mission in mind, PUP QC follows the 10 pillars as a reform agenda for the university.
+                          <ul>
+                            <li>Pillar 1: Dynamic, Transformational and Responsible Leadership.</li>
+                            <li>Pillar 2: Responsive and Innovative Curricula and Instruction.</li>
+                            <li>Pillar 3: Enabling and Productive Learning Environment.</li>
+                            <li>Pillar 4: Holistic Student Development and Engagement.</li>
+                            <li>Pillar 5: Empowered Faculty Members and Employees.</li>
+                            <li>Pillar 6: Vigorous Research Production and Utilization.</li>
+                            <li>Pillar 7: Global Academic Standards and Excellence.</li>
+                            <li>Pillar 8: Synergistic, Productive, Strategic Networks and Partnerships.</li>
+                            <li>Pillar 9: Active and Sustained Stakeholders' Engagement</li>
+                            <li>Pillar 10: Sustainable Social Development Programs and Projects.</li>
+                          </ul>
+                          </p>
+                          <div class="mb-3 d-flex justify-content-center">
+                            <img src="<?= base_url() ?>public/images/about/img-2.jpg" alt="" height="500">
+                          </div>
+                          <p>
+                            PUPQC was established through the generosity and benevolence of Walter Rothlehner, a German church leader who donated his property in Barangay Commonwealth, Quezon City to PUP. The campus lot with an area of 1.9 hectares is donated by SIKHAY, an association led by Rev. Fr. Joel Tabora. The PUP Open University was tasked to administer and maintain the campus and its facilities. PUPQC was formally inaugurated on July 29, 1997. Although it was in existing prior to 1997, it only started its operations and formal classes after its inauguration.
+                          </p>
+                          <p>
+                            The goals and objectives of PUP Quezon City are as follows:
+                          <ul>
+                            <li>Transparency and Efficiency<br>
+                              Responsibly manage school-related activities that may contribute to the holistic development of all stakeholders in instruction, research, and extension.
+                            </li>
+                            <li>System Performance<br>
+                              Utilize leadership, collaboration, and strategic partnership in institutional, national, and international activities.
+                            </li>
+                            <li>Organizational Excellence<br>
+                              Taking accountability in delivering quality service through excellent performance of faculty members and administrative staff.
+                            </li>
+                            <li>Safety and Health<br>
+                              Promote health through worthwhile school activities involving faculty members, staffs, and students.
+                            </li>
+                          </ul>
+                          </p>
+                          <p>
+                            PUPQC Branch main objective is to extend quality education to the youth of the marginalized sector of our society. Through the years, it has already produced several graduates who are successful in their chosen field. Likewise, these alumni continue to practice what they have learned in our Sintang Paaralan as indicated in the lyrics of PUP Hymn "Gagamitin ang karunungan mula sa iyo para sa bayan" for the betterment of our country.
+                          </p>
+                          <p>
+                            Its Faculty Members are encouraged to have further educations (such as Master's Degree and Doctorate Degree), conduct research, and attend seminars and training to update with the needs of the industry in the preparation of their students for their chosen career.
+                            Its Administrative Staff, likewise, are sent to seminars and training related to their duties and responsibilities to ensure the delivery of quality service to stakeholders.
+                            PUPQC Branch commits through its qualified, competent, and dedicated Faculty Members and Administrative Staff to continue to be the way for all less privileged but deserving students in grasping their dreams.
+                          </p>
+                        </div>
+                      </div>
+                      <!--end card-body-->
+                    </div>
+                    <!-- end card -->
+                    <div class="card">
+                      <div class="card-body">
+                        <h3 class="mb-3 text-primary">Academic Programs</h3>
+                        <h5 class="text-primary">Graduate Studies</h5>
+                        <div class="mx-3">
+                          <p>
+                            Offered through the PUP Open University System
+                          <ul>
+                            <li>Master in Information Technology</li>
+                            <li>Master in Educational Management</li>
+                            <li>Master in Public Administration</li>
+                          </ul>
+                          </p>
+                        </div>
+                        <h5 class="text-primary">Undergraduate Degrees</h5>
+                        <div class="mx-3">
+                          <ul>
+                            <li>Bachelor of Science in Business Administration major in Marketing Management (BSBA-MM)</li>
+                            <li>Bachelor of Science in Information Technology (BSIT)</li>
+                            <li>Bachelor in Business Teacher Education major in Home Economics (BBTE-HE)</li>
+                            <li>Bachelor in Business TeacherEducation major in Information Communication Technology (BBTE-ICT)</li>
+                            <li>Bachelor of Science in Business Administration major in Human Resource Management (BSBA-HRM)</li>
+                            <li>Bachelor of Science in Entrepreneurship (BSEnt)</li>
+                            <li>HR</li>
+                          </ul>
+                        </div>
+                        <h5 class="text-primary">Diploma</h5>
+                        <div class="mx-3">
+                          <ul>
+                            <li>Diploma in Office Management Technology (DOMT)</li>
+                            <li>Diploma in Information and Communication Technology (DICT)</li>
+                          </ul>
+                        </div>
+                      </div>
+                      <!--end card-body-->
+                    </div>
+                    <!-- end card -->
+                  </div>
+                  <!--end col-->
+                  <div class="col-xxl-3">
+                    <div class="card">
+                      <div class="card-body">
+                        <div class="d-flex align-items-center mb-4">
+                          <div class="flex-grow-1">
+                            <h5 class="card-title mb-0 text-primary">Contents</h5>
+                          </div>
+                        </div>
+                        <div>
+                          <ul class="list-group list-group-flush">
+                            <li class="list-group-item">PUP Quezon City Branch</li>
+                            <li class="list-group-item">History</li>
+                            <li class="list-group-item">Facilities</li>
+                            <li class="list-group-item">Offices</li>
+                            <li class="list-group-item">Officials and Staff</li>
+                            <li class="list-group-item">Faculty</li>
+                            <li class="list-group-item">Contact Information</li>
+                          </ul>
+                        </div>
+                      </div>
+                      <!-- end card body -->
+                    </div>
+                    <!--end card-->
+                  </div>
+                  <!--end col-->
+                </div>
+                <!--end row-->
+              </div>
+            </div>
+            <!--end col-->
+          </div>
+          <!--end row-->
+        </div>
+        <!-- container-fluid -->
+      </div>
+      <!-- End Page-content -->
+    </div>
+    <!-- end main content-->
+  </div>
+  <!-- END layout-wrapper -->
---
+++ application/views/home/scripts/about-scripts.php
@@ -0,0 +1,8 @@
+		<!-- swiper js -->
+		<script src="<?= base_url() ?>public/libs/swiper/swiper-bundle.min.js"></script>
+
+		<!-- profile init js -->
+		<script src="<?= base_url() ?>/public/js/pages/profile.init.js"></script>
+		</body>
+
+		</html>
