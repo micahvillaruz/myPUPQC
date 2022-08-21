fc900b10b25ca3da1330fc6326e25b228b125472
Micah Villaruz <mmvillaruz@iskolarngbayan.pup.edu.ph>
2022-08-22 00:59:43 +0800

    Add Annual Reports page

---
+++ application/views/home/about/annual-reports.php
@@ -0,0 +1,217 @@
+<body>
+  <!-- Begin page -->
+  <div id="layout-wrapper">
+    <!-- ============================================================== -->
+    <!-- Start right Content here -->
+    <!-- ============================================================== -->
+    <div class="main-content ms-0 bg-light">
+      <div class="page-content pt-4">
+        <div class="container-fluid">
+          <div class="profile-foreground position-relative mx-n4 mt-n4">
+            <div class="profile-wid-bg">
+              <img src="<?= base_url() ?>public/images/pupqc-bg.jpg" alt="" class="profile-wid-img" />
+            </div>
+          </div>
+          <div class="pt-4 mb-4 mb-lg-3 pb-lg-4">
+            <div class="row g-4">
+              <div class="col-auto">
+                <div class="avatar-lg">
+                  <img src="<?= base_url() ?>public/images/pupqc-logo.png" class="img-thumbnail rounded-circle" />
+                </div>
+              </div>
+              <!--end col-->
+              <div class="col">
+                <div class="p-2">
+                  <h3 class="text-white mb-3">Poltechnic University of the Philippines</h3>
+                  <h3 class="text-white">Quezon City Branch</h3>
+                  <div class="hstack text-white-50 gap-1">
+                    <div class="me-2">
+                      <i class="ri-road-map-line me-1 text-white-75 fs-16 align-middle"></i>
+                      <span class="ms-1">Don Fabian St., Commonwealth Quezon City</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!--end col-->
+            </div>
+            <!--end row-->
+          </div>
+
+          <div class="row">
+            <div class="col-lg-12">
+              <div>
+                <div class="row pt-4 text-muted">
+                  <div class="col-xxl-9">
+                    <div class="card shadow-none">
+                      <div class="card-body">
+                        <h3 class="mb-3 text-primary">Annual Reports</h3>
+                        <div class="card-body pb-0">
+                          <div class="row">
+                            <div class="col-lg-12">
+                              <ul class="nav nav-pills animation-nav nav-justified gap-2 mb-3" role="tablist">
+                                <li class="nav-item waves-effect waves-light">
+                                  <a class="nav-link active" data-bs-toggle="tab" href="#annual-2019" role="tab">
+                                    2019
+                                  </a>
+                                </li>
+                                <li class="nav-item waves-effect waves-light">
+                                  <a class="nav-link" data-bs-toggle="tab" href="#annual-2018" role="tab">
+                                    2018
+                                  </a>
+                                </li>
+                                <li class="nav-item waves-effect waves-light">
+                                  <a class="nav-link" data-bs-toggle="tab" href="#annual-2017" role="tab">
+                                    2017
+                                  </a>
+                                </li>
+                                <li class="nav-item waves-effect waves-light">
+                                  <a class="nav-link" data-bs-toggle="tab" href="#annual-2016" role="tab">
+                                    2016
+                                  </a>
+                                </li>
+                                <li class="nav-item waves-effect waves-light">
+                                  <a class="nav-link" data-bs-toggle="tab" href="#annual-2015" role="tab">
+                                    2015
+                                  </a>
+                                </li>
+                                <li class="nav-item waves-effect waves-light">
+                                  <a class="nav-link" data-bs-toggle="tab" href="#annual-2014" role="tab">
+                                    2014
+                                  </a>
+                                </li>
+                                <li class="nav-item waves-effect waves-light">
+                                  <a class="nav-link" data-bs-toggle="tab" href="#annual-2013" role="tab">
+                                    2013
+                                  </a>
+                                </li>
+                                <li class="nav-item waves-effect waves-light">
+                                  <a class="nav-link" data-bs-toggle="tab" href="#annual-2012" role="tab">
+                                    2012
+                                  </a>
+                                </li>
+                              </ul>
+                              <div class="tab-content text-muted">
+                                <div class="tab-pane active" id="annual-2019" role="tabpanel">
+                                  <div class="m-5">
+                                    <iframe src="https://drive.google.com/file/d/1xcsY2ZhADf0h3sMM9r7m3-xTU80Kzicq/preview" width="100%" height="1000px" allow="autoplay"></iframe>
+                                  </div>
+                                </div>
+                                <div class="tab-pane" id="annual-2018" role="tabpanel">
+                                  <div class="m-5">
+                                    <iframe src="https://drive.google.com/file/d/1AbEbD7VVWqtIVDhZb2dqoeYDw0Kd7Y4e/preview" width="100%" height="1000px" allow="autoplay"></iframe>
+                                  </div>
+                                </div>
+                                <div class="tab-pane" id="annual-2017" role="tabpanel">
+                                  <div class="m-5">
+                                    <iframe src="https://drive.google.com/file/d/1Ext1c45a0y8IIoEi25HDBBYGCTtkzgBw/preview" width="100%" height="1000px" allow="autoplay"></iframe>
+                                  </div>
+                                </div>
+                                <div class="tab-pane" id="annual-2016" role="tabpanel">
+                                  <div class="m-5">
+                                    <iframe src="https://drive.google.com/file/d/11LoJnfHhEwayMSv-I4qz3Kx24bol6HsI/preview" width="100%" height="1000px" allow="autoplay"></iframe>
+                                  </div>
+                                </div>
+                                <div class="tab-pane" id="annual-2015" role="tabpanel">
+                                  <div class="m-5">
+                                    <iframe src="https://drive.google.com/file/d/1rq5ma6dr_UcHPGqkNGYmxQhzNStN4zn5/preview" width="100%" height="1000px" allow="autoplay"></iframe>
+                                  </div>
+                                </div>
+                                <div class="tab-pane" id="annual-2014" role="tabpanel">
+                                  <div class="m-5">
+                                    <iframe src="https://drive.google.com/file/d/0B1BuDAuN0r8SeUNZVGVCT2s1Y3c/preview?resourcekey=0-BZ6B1JXIl5uuIL9k-5J5HA" width="100%" height="1000px" allow="autoplay"></iframe>
+                                  </div>
+                                </div>
+                                <div class="tab-pane" id="annual-2013" role="tabpanel">
+                                  <div class="m-5">
+                                    <iframe src="https://drive.google.com/file/d/0B1BuDAuN0r8Sdng0V0lDVnlfOGM/preview?resourcekey=0-wHMKJJsaGfjgqRDFH_xafA" width="100%" height="1000px" allow="autoplay"></iframe>
+                                  </div>
+                                </div>
+                                <div class="tab-pane" id="annual-2012" role="tabpanel">
+                                  <div class="m-5">
+                                    <iframe src="https://drive.google.com/file/d/0B1BuDAuN0r8SRVVZdXVXdTYyem8/preview?resourcekey=0-VDS7kcilMEeQwNqGXye5sg" width="100%" height="1000px" allow="autoplay"></iframe>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                          <!-- end row -->
+                        </div>
+                        <!-- end card body -->
+                      </div>
+                      <!--end card-body-->
+                    </div>
+                    <!-- end card -->
+                  </div>
+                  <!--end col-->
+                  <div class="col-xxl-3">
+                    <div class="card">
+                      <div class="card-body">
+                        <div class="d-flex align-items-center mb-4">
+                          <div class="flex-grow-1">
+                            <h5 class="card-title mb-0 text-primary">Contents</h5>
+                          </div>
+                        </div>
+                        <div>
+                          <ul class="list-group list-group-flush">
+                            <li class="list-group-item">
+                              <a class="text-muted" href="<?= base_url() ?>about">University Profile</a>
+                            </li>
+                            <li class="list-group-item text-muted">
+                              <a class="text-muted" href="<?= base_url() ?>about/vm">Vision and Mission</a>
+                            </li>
+                            <li class="list-group-item">
+                              <a class="text-muted" href="<?= base_url() ?>about/history">History</a>
+                            </li>
+                            <li class="list-group-item">
+                              <a class="text-muted" href="<?= base_url() ?>about/logo-and-symbols">Logo and Symbols</a>
+                            </li>
+                            <li class="list-group-item">
+                              <a class="text-muted" href="<?= base_url() ?>about/hymn">Hymn</a>
+                            </li>
+                            <li class="list-group-item">
+                              <a class="text-muted" href="<?= base_url() ?>about/annual-reports">Annual Reports</a>
+                            </li>
+                            <li class="list-group-item">
+                              <a class="text-muted" href="<?= base_url() ?>about/contact-us">Contact us</a>
+                            </li>
+                            <li class="list-group-item">
+                              <a class="text-muted" href="<?= base_url() ?>about/maps">Maps</a>
+                            </li>
+                            <li class="list-group-item">
+                              <a class="text-muted" href="<?= base_url() ?>about/code">University Code</a>
+                            </li>
+                            <li class="list-group-item">
+                              <a class="text-muted" href="<?= base_url() ?>about/memorandum-orders">Memorandum Orders</a>
+                            </li>
+                            <li class="list-group-item">
+                              <a class="text-muted" href="<?= base_url() ?>about/executive-orders">Executive Orders</a>
+                            </li>
+                            <li class="list-group-item">
+                              <a class="text-muted" href="<?= base_url() ?>about/codi-manual">CODI Manual</a>
+                            </li>
+                            <li class="list-group-item">
+                              <a class="text-muted" href="<?= base_url() ?>about/citizens-charter">Citizens Charter</a>
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                      <!-- end card body -->
+                    </div>
+                    <!--end card-->
+                  </div>
+                  <!--end col-->
+                </div>
+                <!--end row-->
+              </div>
+            </div>
+            <!--end col-->
+          </div>
+          <!--end row-->
+        </div>
+        <!-- container-fluid -->
+      </div>
+      <!-- End Page-content -->
+    </div>
+    <!-- end main content-->
+  </div>
+  <!-- END layout-wrapper -->
