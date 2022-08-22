f08313232efa76a2b099672b29452ce25819fb09
James Paul Tulod <paultulod@pm.me>
2022-08-22 21:20:53 +0800

    Add Landing page ajax - Contact Validation - Sweetalert eme

---
+++ public/js/ajax/home/landing.ajax.js
@@ -0,0 +1,27 @@
+$(function () {
+	$('#contact_form').on('submit', function (e) {
+		e.preventDefault()
+
+		if ($('#contact_form')[0].checkValidity()) {
+			Swal.fire({
+				html:
+					'<div class="mt-3">' +
+					'<lord-icon src="https://cdn.lordicon.com/lupuorrc.json" trigger="loop" colors="primary:#0ab39c,secondary:#405189" style="width:120px;height:120px"></lord-icon>' +
+					'<div class="mt-4 pt-2 fs-15">' +
+					'<h4>Get in Touch Successfully !</h4>' +
+					`<p class="text-muted mx-4 mb-0">Name: ${$('#name').val()}</p>` +
+					`<p class="text-muted mx-4 mb-0">Email: ${$('#email').val()}</p>` +
+					`<p class="text-muted mx-4 mb-0">Subject: ${$('#subject').val()}</p>` +
+					`<p class="text-muted mx-4 mb-0">Comments: ${$('#comments').val()}</p>` +
+					'</div>' +
+					'</div>',
+				showCancelButton: true,
+				showConfirmButton: false,
+				cancelButtonClass: 'btn btn-primary w-xs mb-1',
+				cancelButtonText: 'Back',
+				buttonsStyling: false,
+				showCloseButton: true,
+			})
+		}
+	})
+})
