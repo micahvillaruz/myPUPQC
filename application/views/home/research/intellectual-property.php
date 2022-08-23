cba0e24b4171b300a124d34847903f18aafbe205
Zsyrhill Soria <zsyrhillsoria@gmail.com>
2022-08-23 17:23:03 +0800

    Fixed IPMO UI

--- application/views/home/research/intellectual-property.php
+++ application/views/home/research/intellectual-property.php
@@ -1,66 +1,64 @@
 <body>
-    <!-- Begin page -->
-    <div id="layout-wrapper">
-        <!-- ============================================================== -->
-        <!-- Start right Content here -->
-        <!-- ============================================================== -->
-        <div class="main-content ms-0 bg-light">
-            <div class="page-content pt-4">
-                <div class="container-fluid">
-                    <div class="profile-foreground position-relative mx-n4 mt-n4">
-                        <div class="profile-wid-bg">
-                            <img src="<?= base_url() ?>public/images/pupqc-bg.jpg" alt="" class="profile-wid-img" />
-                        </div>
+  <!-- Begin page -->
+  <div id="layout-wrapper">
+    <!-- ============================================================== -->
+    <!-- Start right Content here -->
+    <!-- ============================================================== -->
+    <div class="main-content ms-0 bg-light">
+      <div class="page-content pt-4">
+        <div class="container-fluid">
+          <div class="profile-foreground position-relative mx-n4 mt-n4">
+            <div class="profile-wid-bg">
+              <img src="<?= base_url() ?>public/images/pupqc-bg.jpg" alt="" class="profile-wid-img" />
+            </div>
+          </div>
+          <div class="pt-4 mb-4 mb-lg-3 pb-lg-4">
+            <div class="row g-4">
+              <div class="col-auto">
+                <div class="avatar-lg">
+                  <img src="<?= base_url() ?>public/images/pupqc-logo.png" class="img-thumbnail rounded-circle" />
+                </div>
+              </div>
+              <!--end col-->
+              <div class="col">
+                <div class="p-2">
+                  <h3 class="text-white mb-3">Polytechnic University of the Philippines</h3>
+                  <h3 class="text-white">Quezon City Branch</h3>
+                  <div class="hstack text-white-50 gap-1">
+                    <div class="me-2">
+                      <i class="ri-road-map-line me-1 text-white-75 fs-16 align-middle"></i>
+                      <span class="ms-1">Don Fabian St., Commonwealth Quezon City</span>
                     </div>
-                    <div class="pt-4 mb-4 mb-lg-3 pb-lg-4">
-                        <div class="row g-4">
-                            <div class="col-auto">
-                                <div class="avatar-lg">
-                                    <img src="<?= base_url() ?>public/images/pupqc-logo.png" class="img-thumbnail rounded-circle" />
-                                </div>
-                            </div>
-                            <!--end col-->
-                            <div class="col">
-                                <div class="p-2">
-                                    <h3 class="text-white mb-3">Polytechnic University of the Philippines</h3>
-                                    <h3 class="text-white">Quezon City Branch</h3>
-                                    <div class="hstack text-white-50 gap-1">
-                                        <div class="me-2">
-                                            <i class="ri-road-map-line me-1 text-white-75 fs-16 align-middle"></i>
-                                            <span class="ms-1">Don Fabian St., Commonwealth Quezon City</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <!--end col-->
-                        </div>
-                        <!--end row-->
-                    </div>
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <div>
-                                <div class="row pt-4 text-muted">
-                                    <div class="col-xxl-9">
-                                        <div class="card">
-                                            <div class="card-body">
-                                                <h3 class="mb-3 text-primary">Intellectual Property Management Office (IPMO)</h3>
-                                                <div class="mx-3">
-                                                    <p>
-                                                        The Intellectual Property Management Office (IPMO) provides assistance to researchers to secure attractive returns in the long term from the bulk of valuable research outcomes and intellectual property. It also assists researchers in identifying partners within the academe, other businesses and investors to promote translation of research innovations into new products through advice and support for early-stage research and development. The IPMO provides a portfolio of new technologies created through University research for licensing or collaborative development.
-                                                    </p>
-                                                    <hr>
-                                                    <div class="my-3">
-                                                        <h5 class="text-primary fw-bold">IPMO Office and Center (Only in PUP Sta. Mesa)</h5>
-                                                        <ul>
-                                                            <li><span class="fw-medium text-primary">Innovations and Technology Support Office</span><br>
-                                                                The Innovations Technological and Support Office (ITSO) was created to reaffirm PUP's commitment of responding to the needs of time by propagating and protecting intellectual property rights. Intellectual properties are creations of human minds and just like any other form of property, intellectual property rights are duly recognized</li>
-                                                            <li><span class="fw-medium text-primary">Center for Technology Transfer and Enterprise Development</span><br>
-                                                                The Center for Technology Transfer and Enterprise Development (CTTED), is a dynamic center that facilitates and supports the development of start-up technologies based on University research. The Center also addresses the potential roles, considerations, and opportunities for university-affiliated inventors such as students, faculty members and administrative staff.</li>
-                                                        </ul>
-                                                    </div>
-                                                    <hr>
-                                                    <div class="my-3">
-                                                        <h5 class="text-primary fw-bold">Copyright Registration - Application Procedure & Forms</h5>
+                  </div>
+                </div>
+              </div>
+              <!--end col-->
+            </div>
+            <!--end row-->
+          </div>
+
+          <div class="row">
+            <div class="col-lg-12">
+              <div>
+                <div class="row pt-4 text-muted">
+                  <div class="col-xxl-9">
+                    <div class="card">
+                      <div class="card-body">
+                        <h3 class="mb-3 text-primary">Intellectual Property Management Office (IPMO)</h3>
+                        <div class="mx-3">
+                          <p>
+                            The Intellectual Property Management Office (IPMO) provides assistance to researchers to secure attractive returns in the long term from the bulk of valuable research outcomes and intellectual property. It also assists researchers in identifying partners within the academe, other businesses and investors to promote translation of research innovations into new products through advice and support for early-stage research and development. The IPMO provides a portfolio of new technologies created through University research for licensing or collaborative development.
+                          </p>
+                          <hr>
+                          <div class="my-3">
+                            <h5 class="text-primary fw-bold">IPMO Office and Center (Only in PUP Sta. Mesa)</h5>
+                            <ul>
+                              <li><span class="fw-medium text-primary">Innovations and Technology Support Office</span><br>
+                                The Innovations Technological and Support Office (ITSO) was created to reaffirm PUP's commitment of responding to the needs of time by propagating and protecting intellectual property rights. Intellectual properties are creations of human minds and just like any other form of property, intellectual property rights are duly recognized</li>
+                              <li><span class="fw-medium text-primary">Center for Technology Transfer and Enterprise Development</span><br>
+                                The Center for Technology Transfer and Enterprise Development (CTTED), is a dynamic center that facilitates and supports the development of start-up technologies based on University research. The Center also addresses the potential roles, considerations, and opportunities for university-affiliated inventors such as students, faculty members and administrative staff.</li>
+                            </ul><hr>
+                            <h5 class="text-primary fw-bold">Copyright Registration - Application Procedure & Forms</h5>
                                                         <p>As approved in the 141st Regular Meeting of the University Board of Regents and pursuant to the PUP Memorandum Order No. 031, series of 2013, all academic works (theses, dissertations, feasibility studies, design prototypes, computer programs and software, audiovisual and cinematographic works, literary and creative works, etc.) submitted in partial fulfillment of the requirements of undergraduate and graduate courses in the University shall be applied for Copyright Registration with the Polytechnic University of the Philippines as copyright co-owner. The following is the procedure for copyright registration application:</p>
                                                         <p><span class="fw-medium">Step 1:</span>
                                                             Secure the Application Form and Affidavit of Copyright Co-ownership from the Intellectual Property Management Office (IPMO) , or the Innovation and Technology Commercialization Office (ITSO) and Center for Technology Transfer and Enterprise Development for Branches and Satellite Campuses at S-407, or download it from the PUP website at <a href="http://www.pup.edu.ph/research/ipmo" class="text-primary">http://www.pup.edu.ph/research/ipmo.</a> Note that the application for copyright registration of works in science, engineering and information technology shall be handled by ITSO. All the rest shall be coursed through the IPMO.</p>
@@ -90,69 +88,66 @@
                                                         </p>
                                                         <p>Other Downloads: </p>
                                                         <ul>
-                                                            <li><a href="https://1drv.ms/w/s!AqZm000jSfvFkgU_ppeqXDB8alyW?e=qG5HKc" target="_blank">Updated Recordal Template (docx)</a></li>
-                                                            <li><a href="https://1drv.ms/w/s!AqZm000jSfvFkgTy6mkuMT10Vs2M?e=i8tF10" target="_blank">Certificate of Copyright Application (docx)</a></li>
+                                                            <li><a href="http://www.buzzgig.com/downloads/files/SCBWI%20Joint%20Author%20Agreement.pdf" target="_blank">Joint Authorship Agreement Template (PDF)</a></li>
+                                                            <li><a href="https://1drv.ms/w/s!AqZm000jSfvFkgU_ppeqXDB8alyW?e=qG5HKc" target="_blank">Updated Recordal Template (DOCX)</a></li>
+                                                            <li><a href="https://1drv.ms/w/s!AqZm000jSfvFkgTy6mkuMT10Vs2M?e=i8tF10" target="_blank">Certificate of Copyright Application (DOCX)</a></li>
                                                         </ul>
-                                                    </div>
-                                                    <hr>
-                                                </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <!--end card-body-->
-                                    </div>
-                                    <!-- end card -->
-                                </div>
-                                <!--end col-->
-                                <div class="col-xxl-3">
-                                    <div class="card">
-                                        <div class="card-body">
-                                            <div class="d-flex align-items-center mb-4">
-                                                <div class="flex-grow-1">
-                                                    <h5 class="card-title mb-0 text-primary">Contents</h5>
-                                                </div>
-                                            </div>
-                                            <div>
-                                                <ul class="list-group list-group-flush">
-                                                    <li class="list-group-item">
-                                                        <a class="text-muted" href="#!">Research Management Office</a>
-                                                    </li>
-                                                    <li class="list-group-item text-muted">
-                                                        <a class="text-muted" href="#!">Objectives</a>
-                                                    </li>
-                                                    <li class="list-group-item">
-                                                        <a class="text-muted" href="#!">Logo</a>
-                                                    </li>
-                                                    <li class="list-group-item">
-                                                        <a class="text-muted" href="#!">Services and Regular Capacity <br> Building Programs / Projects</a>
-                                                    </li>
-                                                    <li class="list-group-item">
-                                                        <a class="text-muted" href="#!">Officials and Staffs</a>
-                                                    </li>
-                                                    <li class="list-group-item">
-                                                        <a class="text-muted" href="#!">Contact Information</a>
-                                                    </li>
-                                                </ul>
-                                            </div>
-                                        </div>
-                                        <!-- end card body -->
-                                    </div>
-                                    <!--end card-->
-                                </div>
-                                <!--end col-->
-                            </div>
-                            <!--end row-->
+                          </div>
                         </div>
+                      </div>
+                      <!--end card-body-->
                     </div>
-                    <!--end col-->
+                    <!-- end card -->
+                  </div>
+                  <!--end col-->
+                  <div class="col-xxl-3">
+                    <div class="card">
+                      <div class="card-body">
+                        <div class="d-flex align-items-center mb-4">
+                          <div class="flex-grow-1">
+                            <h5 class="card-title mb-0 text-primary">Contents</h5>
+                          </div>
+                        </div>
+                        <div>
+                          <ul class="list-group list-group-flush">
+                            <li class="list-group-item">
+                              <a class="text-muted" href="#!">Research Management Office</a>
+                            </li>
+                            <li class="list-group-item text-muted">
+                              <a class="text-muted" href="#!">Objectives</a>
+                            </li>
+                            <li class="list-group-item">
+                              <a class="text-muted" href="#!">Logo</a>
+                            </li>
+                            <li class="list-group-item">
+                              <a class="text-muted" href="#!">Services and Regular Capacity <br> Building Programs / Projects</a>
+                            </li>
+                            <li class="list-group-item">
+                              <a class="text-muted" href="#!">Officials and Staffs</a>
+                            </li>
+                            <li class="list-group-item">
+                              <a class="text-muted" href="#!">Contact Information</a>
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                      <!-- end card body -->
+                    </div>
+                    <!--end card-->
+                  </div>
+                  <!--end col-->
                 </div>
                 <!--end row-->
+              </div>
             </div>
-            <!-- container-fluid -->
+            <!--end col-->
+          </div>
+          <!--end row-->
         </div>
-        <!-- End Page-content -->
+        <!-- container-fluid -->
+      </div>
+      <!-- End Page-content -->
     </div>
     <!-- end main content-->
-    </div>
-    <!-- END layout-wrapper -->
-</body>
+  </div>
+  <!-- END layout-wrapper -->
