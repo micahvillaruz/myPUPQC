5ccf00e596c9cb9cc955f9383c0d2c51682fd441
Zsyrhill Soria <zsyrhillsoria@gmail.com>
2022-08-23 15:45:03 +0800

    Refactored Student Council Page

--- application/views/home/student/council.php
+++ application/views/home/student/council.php
@@ -47,36 +47,122 @@
                         <h3 class="mb-3 text-primary">Student Council</h3>
                         <div class="mx-3">
                           <div class="mb-3 d-flex justify-content-center">
-                            <img src="<?= base_url() ?>public/images/about/student/ssclogo.jpg" alt="" class="img-thumbnail">
+                            <img src="<?= base_url() ?>public/images/about/student/ssclogo.jpg" alt="" class="img-thumbnail w-25">
                           </div>
                           <p>
-                          The Supreme Student Council, also known as SSC, is the highest policy-making and student-governing body of PUP Quezon City Branch that upholds a nationalist, scientific, and mass-oriented system of education.
-                          As we welcome the new Academic Year 2021-2022 in Polytechnic University of the Philippines Quezon City Branch, the new and well-represented student council is now ready to stand and fight for the students' rights, interests, and welfare at all costs. A student council that is committed to facing the challenges amidst these trying times and affirmed to have an HONEST, COMMITTED, and DATA-DRIVEN governance which will all be useful to uphold not just academic excellence but also bring forth sociopolitical consciousness among PUPQC students.
-                          The new set of SSC-Executive Board Officers comprise of student-leaders from different academic programs, students who have diverse personalities, raised in different cultures within and outside the university, students who have answered the call of duty to lead, and have chosen to be the voice of our fellow Iskolars ng Bayan.
+                            The Supreme Student Council, also known as SSC, is the highest policy-making and student-governing body of PUP Quezon City Branch that upholds a nationalist, scientific, and mass-oriented system of education.
+                            As we welcome the new Academic Year 2021-2022 in Polytechnic University of the Philippines Quezon City Branch, the new and well-represented student council is now ready to stand and fight for the students' rights, interests, and welfare at all costs. A student council that is committed to facing the challenges amidst these trying times and affirmed to have an HONEST, COMMITTED, and DATA-DRIVEN governance which will all be useful to uphold not just academic excellence but also bring forth sociopolitical consciousness among PUPQC students.
+                            The new set of SSC-Executive Board Officers comprise of student-leaders from different academic programs, students who have diverse personalities, raised in different cultures within and outside the university, students who have answered the call of duty to lead, and have chosen to be the voice of our fellow Iskolars ng Bayan.
                           </p>
                           <p>
-                          Polytechnic University of the Philippines Quezon City Supreme Student Council: Honest, Committed, Data-driven Governance.
+                            Polytechnic University of the Philippines Quezon City Supreme Student Council: Honest, Committed, Data-driven Governance.
                           </p>
                           <p>
                             The current members of the PUP QC Supreme Student Council are as follows:
-                          <ul>
-                            <li><h5>President</h5> Jennylyn M. Balaoro</li>
-                            <li><h5>Vice President</h5> Kristine Joy M. Baldovizo</li>
-                            <li><h5>Deputy Secretary</h5> Johnric P. Delos Reyes</li>
-                            <li><h5>Secretary General</h5> Erica C. Cantos</li>
-                            <li><h5>Auditor</h5> Kate S. Culile</li>
-                            <li><h5>Legal Counseling Officer</h5> Shiena Mae Joan O. Francia</li>
-                            <li><h5>Research & Development Officer</h5> Jane Samer B. Pajela</li>
-                            <li><h5>Budget & Finance Officer</h5> Sophia Veinne B. Cerbo</li>
-                            <li><h5>Internal Affair Officer</h5> Jhazzmin Nera</li>
-                            <li><h5>External Affair Officer</h5> Angelo J. Legaspi</li>
-                            <li><h5>Technical Commitee</h5> Krisha T. Maldonado</li>
-                            <li><h5>Technical Commitee</h5> Carl Marcus D. Aquino</li>
-                            <li><h5>Technical Commitee</h5> Jocy W. Zambrona</li>
-                            <li><h5>Technical Commitee</h5> Adrian Owen G. Balanquit</li>
-                            <li><h5>Public Affair Officer</h5> Ken Erika Godeloson</li>
-                           
-                          </ul>
+                          <table class="table mx-auto justify-content-center text-center mb-1 w-75 ">
+                            <thead class="table-light">
+                              <tr>
+                                <th>
+                                  <h5>Position</h5>
+                                </th>
+                                <th>
+                                  <h5>Name</h5>
+                                </th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              <tr>
+                                <td>
+                                  <h5>President</h5>
+                                </td>
+                                <td>Jennylyn M. Balaoro</td>
+                              </tr>
+                              <tr>
+                                <td>
+                                  <h5> Vice President</h5>
+                                </td>
+                                <td>Kristine Joy M. Baldovizo</td>
+                              </tr>
+                              <tr>
+                                <td>
+                                  <h5>Deputy Secretary</h5>
+                                </td>
+                                <td>Johnric P. Delos Reyes</td>
+                              </tr>
+                              <tr>
+                                <td>
+                                  <h5>Secretary General</h5>
+                                </td>
+                                <td>Erica C. Cantos</td>
+                              </tr>
+                              <tr>
+                              <td>
+                                  <h5>Auditor</h5>
+                                </td>
+                                <td>Kate S. Culile</td>
+                              </tr>
+                              <tr>
+                              <td>
+                                  <h5>Legal Counseling Officer</h5>
+                                </td>
+                                <td>Shiena Mae Joan O. Francia</td>
+                              </tr>
+                              <tr>
+                              <td>
+                              <h5>Research & Development Officer</h5>
+                                </td>
+                                <td>Jane Samer B. Pajela</td>
+                              </tr>
+                              <tr>
+                              <td>
+                              <h5>Budget & Finance Officer</h5>
+                                </td>
+                                <td>Sophia Veinne B. Cerbo</td>
+                              </tr>
+                              <tr>
+                              <td>
+                              <h5>Internal Affair Officer</h5>
+                                </td>
+                                <td>Jhazzmin Nera</td>
+                              </tr>
+                              <tr>
+                              <td>
+                              <h5>External Affair Officer</h5>
+                                </td>
+                                <td>Angelo J. Legaspi</td> 
+                              </tr>
+                              <tr>
+                              <td>
+                              <h5>Technical Commitee</h5>
+                                </td>
+                                <td>Krisha T. Maldonado</td> 
+                              </tr>
+                              <tr>
+                                <td>
+                              <h5>Technical Commitee</h5>
+                                </td>
+                                <td>Carl Marcus D. Aquino</td>  
+                              </tr>
+                              <tr>
+                              <td>
+                              <h5>Technical Commitee</h5>
+                                </td>
+                                <td>Jocy W. Zambrona</td>  
+                              </tr>
+                              <tr>
+                              <td>
+                              <h5>Technical Commitee</h5>
+                                </td>
+                                <td>Adrian Owen G. Balanquit</td>
+                              </tr>
+                              <tr>
+                              <td>
+                              <h5>Public Affair Officer</h5>
+                                </td>
+                                <td>Ken Erika Godeloson</td>
+                              </tr>
+                            </tbody>
+                          </table>
                           </p>
                           <div class="mb-3 d-flex justify-content-center">
                             <img src="<?= base_url() ?>public/images/about/student/studentcouncil.jpg" alt="" class="img-fluid">
@@ -85,7 +171,7 @@
                             <b><i>Honest. Committed. Data-driven.</i></b>
                           </p>
                           <p>
-                          As the highest student government in PUP Quezon City, the Supreme Student Council (SSC) is at the forefront of advancing student rights and welfare. The council upholds the principles of being honest, committed, and data-driven in creating a range of programs or initiatives that do not merely contribute greatly to the welfare of the PUPQC Isko at Iska, but also give a significant impact on the overall well-being of the society at large.
+                            As the highest student government in PUP Quezon City, the Supreme Student Council (SSC) is at the forefront of advancing student rights and welfare. The council upholds the principles of being honest, committed, and data-driven in creating a range of programs or initiatives that do not merely contribute greatly to the welfare of the PUPQC Isko at Iska, but also give a significant impact on the overall well-being of the society at large.
                           <ul>
                             <li>Transparency and Efficiency<br>
                               Responsibly manage school-related activities that may contribute to the holistic development of all stakeholders in instruction, research, and extension.
