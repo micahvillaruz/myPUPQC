77fd0d76a7a0094093482102e69d993df60c153e
Micah Villaruz <mmvillaruz@iskolarngbayan.pup.edu.ph>
2022-08-25 01:42:52 +0800

    Update contact information page

--- application/views/home/about/contact-us.php
+++ application/views/home/about/contact-us.php
@@ -46,26 +46,176 @@
                       <div class="card-body">
                         <h3 class="mb-4 text-primary">Contact Information</h3>
                         <div class="mx-3">
-                          <div class="mb-4">
-                            <h4 class="text-primary">Email</h4>
-                            <p>quezoncity@pup.edu.ph</p>
+                          <div class="card">
+                            <div class="card-body p-0">
+                              <div class="alert alert-primary rounded-top alert-solid alert-label-icon border-0 rounded-0 m-0 d-flex align-items-center" role="alert">
+                                <i class="ri-facebook-circle-fill label-icon"></i>
+                                <div class="flex-grow-1 text-truncate">
+                                  Official Facebook Pages
+                                </div>
+                              </div>
+
+                              <div class="row align-items-end">
+                                <div class="col-sm-8">
+                                  <div class="mt-3 px-3 mb-3">
+                                    <h5 class="list-title fs-15 mb-1">PUPQC BRANCH 'OFFICIAL'</h5>
+                                    <a href="https://www.facebook.com/ThePUPQCBranchOfficial/" class="link-primary fs-12 fst-italic">https://www.facebook.com/ThePUPQCBranchOfficial/</a>
+                                  </div>
+                                  <div class="px-3 mb-2">
+                                    <h5 class="list-title fs-15 mb-1">PUPQUEZON CITY BRANCH-STUDENT SERVICES (ANNOUNCEMENTS)</h5>
+                                    <a href="https://www.facebook.com/groups/303222520571343" class="link-primary fs-12 fst-italic">https://www.facebook.com/groups/303222520571343</a>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                            <!-- end card-body-->
                           </div>
-                          <div class="mb-4">
-                            <h4 class="text-primary">Postal Mail</h4>
-                            <p>PUP Quezon City Branch <br> Don Fabian St., Commonwealth <br> Quezon City Philippines</p>
+                          <div class="card">
+                            <div class="card-body p-0">
+                              <div class="alert alert-primary rounded-top alert-solid alert-label-icon border-0 rounded-0 m-0 d-flex align-items-center" role="alert">
+                                <i class="ri-phone-fill label-icon"></i>
+                                <div class="flex-grow-1 text-truncate">
+                                  Landline
+                                </div>
+                              </div>
+
+                              <div class="row align-items-end">
+                                <div class="col-sm-8">
+                                  <div class="mt-3 px-3 mb-3">
+                                    <h5 class="list-title fs-15 mb-1">ADMISSION AND REGISTRATION OFFICE</h5>
+                                    <p>(8) 287-8204</p>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                            <!-- end card-body-->
                           </div>
-                          <div class="mb-4">
-                            <h4 class="text-primary">Telephone</h4>
-                            <span class="d-block">(632) 8952-7818</span>
-                            <span class="d-block">(632) 8287-8204</span>
+                          <div class="card">
+                            <div class="card-body p-0">
+                              <div class="alert alert-primary rounded-top alert-solid alert-label-icon border-0 rounded-0 m-0 d-flex align-items-center" role="alert">
+                                <i class="mdi mdi-mailbox label-icon"></i>
+                                <div class="flex-grow-1 text-truncate">
+                                  Postal Mail
+                                </div>
+                              </div>
+
+                              <div class="row align-items-end">
+                                <div class="col-sm-8">
+                                  <div class="mt-3 px-3 mb-3">
+                                    <p class="fs-14 text-dark">
+                                      Polytechnic University of the Philippines Quezon City Branch <br>
+                                      Don Fabian Street, Barangay Commonwealth <br>
+                                      Quezon City, Philippines
+                                    </p>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                            <!--end card-body-->
+                          </div>
+                          <div class="card">
+                            <div class="card-body p-0">
+                              <div class="alert alert-primary rounded-top alert-solid alert-label-icon border-0 rounded-0 m-0 d-flex align-items-center" role="alert">
+                                <i class="mdi mdi-email-multiple label-icon"></i>
+                                <div class="flex-grow-1 text-truncate">
+                                  Email
+                                </div>
+                              </div>
+
+                              <div class="row align-items-end">
+                                <div class="col-sm-8">
+                                  <div class="mt-3 px-3 mb-3">
+                                    <h5 class="list-title fs-15 mb-1">DIRECTOR OFFICE</h5>
+                                    <a href="mailto:quezoncity@pup.edu.ph" class="link-primary fs-12">
+                                      quezoncity@pup.edu.ph</a> <br>
+                                    <a href="mailto:pupcommonwealth@pup.edu.ph" class="link-primary fs-12">pupcommonwealth@pup.edu.ph</a>
+                                  </div>
+                                  <div class="px-3 mb-3">
+                                    <h5 class="list-title fs-15 mb-1">ACADEMIC OFFICE</h5>
+                                    <a href="mailto:academic.qc@pup.edu.ph" class="link-primary fs-12">
+                                      academic.qc@pup.edu.ph</a>
+                                  </div>
+                                  <div class="px-3 mb-3">
+                                    <h5 class="list-title fs-15 mb-1">STUDENT AFFAIRS AND SERVICES OFFICE</h5>
+                                    <a href="mailto:studentservices.qc@pup.edu.ph" class="link-primary fs-12">studentservices.qc@pup.edu.ph</a>
+                                  </div>
+                                  <div class="px-3 mb-3">
+                                    <h5 class="list-title fs-15 mb-1">REGISTRAR OFFICE</h5>
+                                    <a href="mailto:registrar.commonwealth@pup.edu.ph" class="link-primary fs-12">registrar.commonwealth@pup.edu.ph</a>
+                                  </div>
+                                  <div class="mt-3 px-3 mb-3">
+                                    <h5 class="list-title fs-15 mb-1">ADMISSION OFFICE</h5>
+                                    <a href="mailto:pupqc.admission@pup.edu.ph" class="link-primary fs-12">
+                                      pupqc.admission@pup.edu.ph</a> <br>
+                                    <a href="mailto:admission.commonwealth@pup.edu.ph" class="link-primary fs-12">admission.commonwealth@pup.edu.ph</a>
+                                  </div>
+                                  <div class="px-3 mb-3">
+                                    <h5 class="list-title fs-15 mb-1">CASH AND DISBURSING OFFICE</h5>
+                                    <a href="mailto:cashier.qc@pup.edu.ph" class="link-primary fs-12">
+                                      cashier.qc@pup.edu.ph</a>
+                                  </div>
+                                  <div class="px-3 mb-3">
+                                    <h5 class="list-title fs-15 mb-1">ACCOUNTING OFFICE</h5>
+                                    <a href="mailto:accounting.qc@pup.edu.ph" class="link-primary fs-12">
+                                      accounting.qc@pup.edu.ph</a>
+                                  </div>
+                                  <div class="px-3 mb-3">
+                                    <h5 class="list-title fs-15 mb-1">SCHOLARSHIP OFFICE</h5>
+                                    <a href="mailto:scholarship.qc@pup.edu.ph" class="link-primary fs-12">
+                                      scholarship.qc@pup.edu.ph</a>
+                                  </div>
+                                  <div class="px-3 mb-3">
+                                    <h5 class="list-title fs-15 mb-1">GUIDANCE AND PLACEMENT OFFICE</h5>
+                                    <a href="mailto:guidance_placement.qc@pup.edu.ph" class="link-primary fs-12">guidance_placement.qc@pup.edu.ph</a>
+                                  </div>
+                                  <div class="px-3 mb-3">
+                                    <h5 class="list-title fs-15 mb-1">ADMINISTRATIVE OFFICE</h5>
+                                    <a href="mailto:administrative.qc@pup.edu.ph" class="link-primary fs-12">administrative.qc@pup.edu.ph</a>
+                                  </div>
+                                  <div class="px-3 mb-3">
+                                    <h5 class="list-title fs-15 mb-1">QUALITY ASSURANCE CENTER AND OJT OFFICE</h5>
+                                    <a href="mailto:qac_ojt.qc@pup.edu.ph" class="link-primary fs-12">qac_ojt.qc@pup.edu.ph</a>
+                                  </div>
+                                  <div class="px-3 mb-3">
+                                    <h5 class="list-title fs-15 mb-1">RESEARCH OFFICE</h5>
+                                    <a href="mailto:research.qc@pup.edu.ph" class="link-primary fs-12">research.qc@pup.edu.ph</a>
+                                  </div>
+                                  <div class="px-3 mb-3">
+                                    <h5 class="list-title fs-15 mb-1">LIBRARY (RESOURCE LEARNING CENTER)</h5>
+                                    <a href="mailto:library.qc@pup.edu.ph" class="link-primary fs-12">library.qc@pup.edu.ph</a>
+                                  </div>
+                                  <div class="px-3 mb-3">
+                                    <h5 class="list-title fs-15 mb-1">IT LABORATORY OFFICE</h5>
+                                    <a href="mailto:itlab.qc@pup.edu.ph" class="link-primary fs-12">itlab.qc@pup.edu.ph</a>
+                                  </div>
+                                  <div class="px-3 mb-3">
+                                    <h5 class="list-title fs-15 mb-1">QUALITY MANAGEMENT OFFICE</h5>
+                                    <a href="mailto:qms.qc@pup.edu.ph" class="link-primary fs-12">qms.qc@pup.edu.ph</a>
+                                  </div>
+                                  <div class="px-3 mb-3">
+                                    <h5 class="list-title fs-15 mb-1">MEDICAL CLINIC</h5>
+                                    <a href="mailto:medical.qc@pup.edu.ph" class="link-primary fs-12">medical.qc@pup.edu.ph</a>
+                                  </div>
+                                  <div class="px-3 mb-3">
+                                    <h5 class="list-title fs-15 mb-1">DENTAL CLINIC</h5>
+                                    <a href="mailto:dental.qc@pup.edu.ph" class="link-primary fs-12">dental.qc@pup.edu.ph</a>
+                                  </div>
+                                  <div class="px-3 mb-3">
+                                    <h5 class="list-title fs-15 mb-1">SECURITY OFFICE</h5>
+                                    <a href="mailto:security.qc@pup.edu.ph" class="link-primary fs-12">security.qc@pup.edu.ph</a>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                            <!-- end card-body-->
                           </div>
                         </div>
                       </div>
-                      <!--end card-body-->
+                      <!-- end card body -->
                     </div>
-                    <!-- end card -->
+                    <!--end card -->
                   </div>
-                  <!--end col-->
+                  <!-- end col -->
                   <div class="col-xxl-3">
                     <div class="card">
                       <div class="card-body">
@@ -77,43 +227,25 @@
                         <div>
                           <ul class="list-group list-group-flush">
                             <li class="list-group-item">
-                              <a class="text-muted" href="<?= base_url() ?>about">University Profile</a>
+                              <a class="text-muted" href="<?= base_url() ?>about">PUP Quezon City Branch</a>
                             </li>
                             <li class="list-group-item text-muted">
-                              <a class="text-muted" href="<?= base_url() ?>about/vm">Vision and Mission</a>
-                            </li>
-                            <li class="list-group-item">
                               <a class="text-muted" href="<?= base_url() ?>about/history">History</a>
                             </li>
                             <li class="list-group-item">
-                              <a class="text-muted" href="<?= base_url() ?>about/logo-and-symbols">Logo and Symbols</a>
+                              <a class="text-muted" href="<?= base_url() ?>facilities">Facilities</a>
                             </li>
                             <li class="list-group-item">
-                              <a class="text-muted" href="<?= base_url() ?>about/hymn">Hymn</a>
+                              <a class="text-muted" href="<?= base_url() ?>offices">Offices</a>
                             </li>
                             <li class="list-group-item">
-                              <a class="text-muted" href="<?= base_url() ?>about/annual-reports">Annual Reports</a>
+                              <a class="text-muted" href="<?= base_url() ?>personnel">Officials and Staff</a>
                             </li>
                             <li class="list-group-item">
-                              <a class="text-muted" href="<?= base_url() ?>about/contact-us">Contact us</a>
+                              <a class="text-muted" href="<?= base_url() ?>faculty">Faculty</a>
                             </li>
                             <li class="list-group-item">
-                              <a class="text-muted" href="<?= base_url() ?>about/maps">Maps</a>
-                            </li>
-                            <li class="list-group-item">
-                              <a class="text-muted" href="<?= base_url() ?>about/code">University Code</a>
-                            </li>
-                            <li class="list-group-item">
-                              <a class="text-muted" href="<?= base_url() ?>about/memorandum-orders">Memorandum Orders</a>
-                            </li>
-                            <li class="list-group-item">
-                              <a class="text-muted" href="<?= base_url() ?>about/executive-orders">Executive Orders</a>
-                            </li>
-                            <li class="list-group-item">
-                              <a class="text-muted" href="<?= base_url() ?>about/codi-manual">CODI Manual</a>
-                            </li>
-                            <li class="list-group-item">
-                              <a class="text-muted" href="<?= base_url() ?>about/citizens-charter">Citizens Charter</a>
+                              <a class="text-muted" href="<?= base_url() ?>about/contact-us">Contact Information</a>
                             </li>
                           </ul>
                         </div>
