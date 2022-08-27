dc5bf09fc4fd791f1339f39e920f02052fa26c43
James Paul Tulod <paultulod@pm.me>
2022-08-27 20:46:17 +0800

    Update student sidebar and topbar - Remove unnecessary eme - Fix icons

--- application/views/partials/student/sidebar.php
+++ application/views/partials/student/sidebar.php
@@ -2,22 +2,13 @@
     <div class="app-menu navbar-menu">
       <!-- LOGO -->
       <div class="navbar-brand-box">
-        <!-- Dark Logo-->
-        <a href="index.html" class="logo logo-dark">
-          <span class="logo-sm">
-            <img src="<?= base_url() ?>public/images/logo-sm.png" alt="" height="22" />
-          </span>
-          <span class="logo-lg">
-            <img src="<?= base_url() ?>public/images/mypupqc-logo.png" alt="" height="30" />
-          </span>
-        </a>
         <!-- Light Logo-->
         <a href="index.html" class="logo logo-light">
           <span class="logo-sm">
-            <img src="<?= base_url() ?>public/images/logo-sm.png" alt="" height="22" />
+            <img src="<?= base_url() ?>public/images/logo-sm.png" alt="" height="75" />
           </span>
           <span class="logo-lg">
-            <img src="<?= base_url() ?>public/images/mypupqc-logo.png" alt="" height="30" />
+            <img src="<?= base_url() ?>public/images/logo-light.png" alt="" height="40" />
           </span>
         </a>
         <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover" id="vertical-hover">
--- application/views/partials/student/topbar.php
+++ application/views/partials/student/topbar.php
@@ -63,18 +63,6 @@
               </div>
             </div>
 
-            <div class="ms-1 header-item d-none d-sm-flex">
-              <button type="button" class="btn btn-icon btn-topbar btn-ghost-secondary rounded-circle" data-toggle="fullscreen">
-                <i class="bx bx-fullscreen fs-22"></i>
-              </button>
-            </div>
-
-            <div class="ms-1 header-item d-none d-sm-flex">
-              <button type="button" class="btn btn-icon btn-topbar btn-ghost-secondary rounded-circle light-dark-mode">
-                <i class="bx bx-moon fs-22"></i>
-              </button>
-            </div>
-
             <div class="dropdown topbar-head-dropdown ms-1 header-item">
               <button type="button" class="btn btn-icon btn-topbar btn-ghost-secondary rounded-circle" id="page-header-notifications-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <i class="bx bx-bell fs-22"></i>
@@ -354,7 +342,7 @@
                   <img class="rounded-circle header-profile-user" src="<?= base_url() ?>public/images/users/avatar-1.jpg" alt="Header Avatar" />
                   <span class="text-start ms-xl-2">
                     <span class="d-none d-xl-inline-block ms-1 fw-medium user-name-text">Anna Adame</span>
-                    <span class="d-none d-xl-block ms-1 fs-12 text-muted user-name-sub-text">Founder</span>
+                    <span class="d-none d-xl-block ms-1 fs-12 text-muted user-name-sub-text">Student</span>
                   </span>
                 </span>
               </button>
@@ -363,19 +351,11 @@
                 <h6 class="dropdown-header">Welcome Anna!</h6>
                 <a class="dropdown-item" href="pages-profile.html"><i class="mdi mdi-account-circle text-muted fs-16 align-middle me-1"></i>
                   <span class="align-middle">Profile</span></a>
-                <a class="dropdown-item" href="apps-chat.html"><i class="mdi mdi-message-text-outline text-muted fs-16 align-middle me-1"></i>
-                  <span class="align-middle">Messages</span></a>
-                <a class="dropdown-item" href="apps-tasks-kanban.html"><i class="mdi mdi-calendar-check-outline text-muted fs-16 align-middle me-1"></i>
-                  <span class="align-middle">Taskboard</span></a>
                 <a class="dropdown-item" href="pages-faqs.html"><i class="mdi mdi-lifebuoy text-muted fs-16 align-middle me-1"></i>
                   <span class="align-middle">Help</span></a>
                 <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="pages-profile.html"><i class="mdi mdi-wallet text-muted fs-16 align-middle me-1"></i>
-                  <span class="align-middle">Balance : <b>$5971.67</b></span></a>
                 <a class="dropdown-item" href="pages-profile-settings.html"><span class="badge bg-soft-success text-success mt-1 float-end">New</span><i class="mdi mdi-cog-outline text-muted fs-16 align-middle me-1"></i>
                   <span class="align-middle">Settings</span></a>
-                <a class="dropdown-item" href="auth-lockscreen-basic.html"><i class="mdi mdi-lock text-muted fs-16 align-middle me-1"></i>
-                  <span class="align-middle">Lock screen</span></a>
                 <a class="dropdown-item" href="auth-logout-basic.html"><i class="mdi mdi-logout text-muted fs-16 align-middle me-1"></i>
                   <span class="align-middle" data-key="t-logout">Logout</span></a>
               </div>
