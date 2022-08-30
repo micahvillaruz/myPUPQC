ee0d4776ba6ef0086ac5c1eaf50a8c260399038d
Miguel Carlos Arevalo <micoarevalo.10@gmail.com>
2022-08-30 18:43:28 +0800

    Staff(did not change modal-id)

--- application/views/admin/user-management/components/pup-staffs-modal.php
+++ application/views/admin/user-management/components/pup-staffs-modal.php
@@ -1,17 +1,17 @@
-<!-- Add Student modal -->
+<!-- Add Faculty/Staff modal -->
 <div id="addStaffModal" class="modal fade" tabindex="-1" aria-hidden="true" style="display: none">
   <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content border-0 overflow-hidden">
       <div class="modal-header p-3">
-        <h4 class="card-title mb-0">Add Student Form</h4>
+        <h4 class="card-title mb-0">Add Faculty/Staff Form</h4>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
 
       <div class="modal-body">
         <form>
           <div class="mb-3">
-            <label for="studentNo" class="form-label">Faculty/Staff No.</label>
-            <input type="text" class="form-control" id="studentNo" placeholder="Enter Faculty/Staff No." />
+            <label for="staffNo" class="form-label">Faculty/Staff No.</label>
+            <input type="text" class="form-control" id="staffNo" placeholder="Enter Faculty/Staff No." />
           </div>
           <div class="mb-3">
             <label for="fullName" class="form-label">Name</label>
@@ -81,13 +81,13 @@
   <!-- /.modal-dialog -->
 </div>
 
-<!-- View Student modal -->
+<!-- View Faculty/Staff modal -->
 <div class="modal fade" id="viewStudentModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
   <div class="modal-dialog modal-dialog-scrollable">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalScrollableTitle">
-          Student Information
+          Faculty/Staff Information
         </h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
@@ -98,7 +98,7 @@
             <li class="list-group-item">
               <div class="d-flex align-items-center">
                 <div class="col-4">
-                  <h5 class="fs-13 mb-1 fw-medium"><a href="#" class="link text-dark">Student No</a></h5>
+                  <h5 class="fs-13 mb-1 fw-medium"><a href="#" class="link text-dark">Faculty/Staff No.</a></h5>
                 </div>
                 <div class="col-8">
                   <div class="d-flex fs-13 fw-medium">2020-18543-CM-0</div>
@@ -112,7 +112,7 @@
                   <h5 class="fs-13 mb-1 fw-medium"><a href="#" class="link text-dark">Name</a></h5>
                 </div>
                 <div class="col-8">
-                  <div class="d-flex fs-13 fw-medium">Mary Rucker</div>
+                  <div class="d-flex fs-13 fw-medium">Rodolfo Y. Aquino, MBA</div>
                 </div>
               </div>
             </li>
@@ -123,7 +123,7 @@
                   <h5 class="fs-13 mb-1">Email Address</h5>
                 </div>
                 <div class="col-8">
-                  <div class="d-flex fs-13 fw-medium">eykalutinx2@iskolarngbayan.pup.edu.ph</div>
+                  <div class="d-flex fs-13 fw-medium">@iskolarngbayan.pup.edu.ph</div>
                 </div>
               </div>
             </li>
@@ -131,10 +131,10 @@
             <li class="list-group-item">
               <div class="d-flex align-items-center">
                 <div class="col-4">
-                  <h5 class="fs-13 mb-1">Program</h5>
+                  <h5 class="fs-13 mb-1">Faculty Type</h5>
                 </div>
                 <div class="col-8">
-                  <div class="d-flex fs-13 fw-medium">BPAPFM</div>
+                  <div class="d-flex fs-13 fw-medium">Full-Time</div>
                 </div>
               </div>
             </li>
@@ -142,10 +142,10 @@
             <li class="list-group-item">
               <div class="d-flex align-items-center">
                 <div class="col-4">
-                  <h5 class="fs-13 mb-1">Year and Section</h5>
+                  <h5 class="fs-13 mb-1">Designation</h5>
                 </div>
                 <div class="col-8">
-                  <div class="d-flex fs-13 fw-medium">2-2</div>
+                  <div class="d-flex fs-13 fw-medium">Emeng Faculty ganda lang</div>
                 </div>
               </div>
             </li>
@@ -153,11 +153,11 @@
             <li class="list-group-item">
               <div class="d-flex align-items-center">
                 <div class="col-4">
-                  <h5 class="fs-13 mb-1">Date Enrolled</h5>
+                  <h5 class="fs-13 mb-1">Role</h5>
                 </div>
                 <div class="col-8">
                   <div class="fw-medium">
-                    <span>14 Aug, 2021</span>
+                    <span>Faculty</span>
                   </div>
                 </div>
               </div>
@@ -166,10 +166,10 @@
             <li class="list-group-item">
               <div class="d-flex align-items-center">
                 <div class="col-4">
-                  <h5 class="fs-13 mb-1 fw-medium"><a href="#" class="link text-dark">Enrollment Status</a></h5>
+                  <h5 class="fs-13 mb-1 fw-medium"><a href="#" class="link text-dark">Academic Rank</a></h5>
                 </div>
                 <div class="col-8">
-                  <span class="fs-12 badge rounded-pill bg-success">Enrolled</span>
+                  <span class="fs-12 badge rounded-pill bg-success">Professor</span>
                 </div>
               </div>
             </li>
@@ -177,7 +177,7 @@
             <li class="list-group-item">
               <div class="d-flex align-items-center">
                 <div class="col-4">
-                  <h5 class="fs-13 mb-1 fw-medium"><a href="#" class="link text-dark">Status</a></h5>
+                  <h5 class="fs-13 mb-1 fw-medium"><a href="#" class="link text-dark"> Employment Status</a></h5>
                 </div>
                 <div class="col-8">
                   <span class="fs-12 badge rounded-pill bg-success">Active</span>
@@ -200,20 +200,20 @@
   <!-- /.modal-dialog -->
 </div>
 
-<!-- Edit Student modal -->
+<!-- Edit Staff modal -->
 <div id="editStudentModal" class="modal fade" tabindex="-1" aria-hidden="true" style="display: none">
   <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content border-0 overflow-hidden">
       <div class="modal-header p-3">
-        <h4 class="card-title mb-0">Edit Student Form</h4>
+        <h4 class="card-title mb-0">Edit Faculty/Staff Form</h4>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
 
       <div class="modal-body">
-        <form>
+      <form>
           <div class="mb-3">
-            <label for="studentNo" class="form-label">Student No.</label>
-            <input type="text" class="form-control" id="studentNo" placeholder="Enter student no" />
+            <label for="staffNo" class="form-label">Faculty/Staff No.</label>
+            <input type="text" class="form-control" id="staffNo" placeholder="Enter Faculty/Staff No." />
           </div>
           <div class="mb-3">
             <label for="fullName" class="form-label">Name</label>
@@ -222,42 +222,46 @@
           <div class="mb-3">
             <label for="emailInput" class="form-label">Email Address</label>
             <div class="input-group">
-              <input type="email" class="form-control" id="emailInput" placeholder="Enter email" />
+              <input class="form-control" id="emailInput" placeholder="Enter email" />
               <span class="input-group-text" id="basic-addon2">@iskolarngbayan.pup.edu.ph</span>
             </div>
           </div>
           <div class="mb-3">
-            <label for="program" class="form-label">Program</label>
-            <select class="form-select mb-3" name="state">
-              <option value="BBTLEDHE">BBTLEDHE</option>
-              <option value="BSBAHRM">BSBAHRM</option>
-              <option value="BSBA-MM">BSBA-MM</option>
-              <option value="BSENTREP">BSENTREP</option>
-              <option value="BSIT">BSIT</option>
-              <option value="BPAPFM">BPAPFM</option>
-              <option value="DOMTMOM">DOMTMOM</option>
+            <label for="program" class="form-label">Faculty Type</label>
+            <select id="program" class="form-select mb-3">
+              <option value="Full-Time">Full-Time</option>
+              <option value="Part-Time">Part-Time</option>
             </select>
           </div>
           <div class="mb-3">
-            <label for="yearSection" class="form-label">Year and Section</label>
-            <input type="text" class="form-control" id="yearSection" placeholder="Enter year and section" />
+            <label for="program" class="form-label">Designation</label>
+            <select id="program" class="form-select mb-3">
+              <option value="ResearchProfessor">Research Professor</option>
+              <option value="FacultyExtensionist">Faculty Extensionist</option>
+            </select>
           </div>
           <div class="mb-3">
-            <label for="enrolledDate" class="form-label">Date Enrolled</label>
-            <input id="enrolledDate" type="text" class="form-control" data-provider="flatpickr" data-date-format="d M, Y" placeholder="Pick date">
+            <label for="program" class="form-label">Role</label>
+            <select id="program" class="form-select mb-3">
+              <option value="Admin">Admin</option>
+              <option value="Faculty">Faculty</option>
+              <option value="AcademicHead">Academic Head</option>
+              <option value="Checker">Checker</option>
+            </select>
           </div>
           <div class="mb-3">
-            <label for="enrollStatus" class="form-label">Enrollment Status</label>
+            <label for="program" class="form-label">Academic Rank</label>
+            <select id="program" class="form-select mb-3">
+              <option value="Professor">Professor</option>
+              <option value="AssistantProf">Assistant Professor</option>
+              <option value="AssocProf">Associate Professor</option>
+              <option value="Instructor">Instructor</option>
+            </select>
+          </div>
+          <div class="mb-3">
+            <label for="enrollStatus" class="form-label">Employment Status</label>
             <select class="form-select mb-3" id="enrollStatus">
-              <option selected>Select enrollment status</option>
-              <option value="1">Enrolled</option>
-              <option value="0">Not Enrolled</option>
-            </select>
-          </div>
-          <div class="mb-3">
-            <label for="studentStatus" class="form-label">Status</label>
-            <select class="form-select mb-3" id="studentStatus">
-              <option selected>Select status</option>
+              <option selected>Select employment status</option>
               <option value="1">Active</option>
               <option value="0">Inactive</option>
             </select>
@@ -269,7 +273,7 @@
               and Privacy Policy</label>
           </div>
           <div class="text-end">
-            <button type="submit" class="btn btn-primary">Edit Student</button>
+            <button type="submit" class="btn btn-primary">Add Professor</button>
           </div>
         </form>
       </div>
