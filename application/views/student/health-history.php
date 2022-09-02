da734231f8111821f857ba5f207c440d56f1196c
Micah Villaruz <mmvillaruz@iskolarngbayan.pup.edu.ph>
2022-09-02 16:19:14 +0800

    Add tab component

---
+++ application/views/student/health-history.php
@@ -0,0 +1,304 @@
+<div class="row text-muted">
+  <div class="col-lg-12">
+    <div>
+      <div class="card">
+        <div class="card-body">
+          <!-- Nav tabs -->
+          <ul class="nav nav-pills animation-nav nav-justified gap-2 mb-3" role="tablist">
+            <li class="nav-item waves-effect waves-light">
+              <a class="nav-link active" data-bs-toggle="tab" href="#pupqc" role="tab">
+                PUP Quezon City
+              </a>
+            </li>
+            <li class="nav-item waves-effect waves-light">
+              <a class="nav-link" data-bs-toggle="tab" href="#pupmain" role="tab">
+                PUP Sta. Mesa
+              </a>
+            </li>
+          </ul>
+          <div class="tab-content text-muted">
+            <div class="tab-pane active" id="pupqc" role="tabpanel">
+              <div class="ms-2 mt-5">
+                <h3 class="mb-3 text-primary">Vision (Bisyon)</h3>
+                <div class="mx-5">
+                  <div class="row">
+                    <div class="col-4">
+                      PUP QC, a committed and comprehensive institution of learning that provides a culture of excellence.
+                    </div>
+                    <div class="col-4">
+                      Ang PUP QC, isang nakatuon at komprehensibong institusyon ng pag-aaral na nagbibigay ng kultura ng kahusayan.
+                    </div>
+                    <div class="col-4">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, magni. Sunt quibusdam maxime doloremque harum quia, natus quisquam magni eum consequuntur dolorum? Tempora accusantium natus error iure totam sequi voluptatibus.
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="ms-2 mt-4">
+                <h3 class="mb-3 text-primary">Mission (Misyon)</h3>
+                <div class="mx-5">
+                  <div class="row">
+                    <div class="col-6">
+                      PUP QC responsive to the needs of its stakeholders is committed to promote cultural and academic excellence guided with ethical and moral leadership in pursuit of quality education.
+                    </div>
+                    <div class="col-6">
+                      Ang PUP QC na tumutugon sa mga pangangailangan ng mga kasapi nito ay nakatuon sa pagtataguyod ng kahusayan sa kultura at akademiko na ginagabayan ng etikal at moral na pamumuno sa paghahangad ng kalidad na edukasyon.
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="ms-2 mt-4">
+                <h3 class="mb-3 text-primary">Goals</h3>
+                <div class="mx-5">
+                  <ul>
+                    <li>
+                      Effective curriculum that promotes arts, science, and research based education.
+                    </li>
+                    <li>
+                      Harmonious relationship between the administration, faculty, non-teaching personnel and students, by considering the culture of academic excellence and academic freedom in the institution.
+                    </li>
+                    <li>
+                      Competent university personnels and student leaders, to sustain quality standard education.
+                    </li>
+                    <li>
+                      Graduates that are globally competitive in their chosen fields and responsive to the community needs.
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div class="ms-2 mt-4">
+                <h3 class="mb-3 text-primary">Objectives</h3>
+                <div class="mx-5">
+                  <ul>
+                    <li>
+                      Provide quality education that is responsive to the needs of the society.
+                    </li>
+                    <li>
+                      Preserve the commitment and pursue common goals.
+                    </li>
+                    <li>
+                      Provide its students and faculty with arts, science and research based education.
+                    </li>
+                    <li>
+                      Promote culture of academic excellence among the faculty, students and non-teaching personnel by maintaining academic freedom responsibility.
+                    </li>
+                    <li>
+                      Sustain a reliable world class products in the future.
+                    </li>
+                    <li>
+                      Make a PUP QC a prime learning institution in the locality.
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div class="tab-pane" id="pupmain" role="tabpanel">
+              <div class="ms-2 mt-5">
+                <h3 class="mb-3 text-primary">Vision (Bisyon)</h3>
+                <div class="mx-5 text-center">
+                  <h2>PUP: The National Polytechnic University</h2>
+                  <p>PUP: Pambansang Politeknikong Unibersidad</p>
+                </div>
+              </div>
+              <div class="ms-2 mt-4">
+                <h3 class="mb-3 text-primary">Mission (Misyon)</h3>
+                <div class="mx-5">
+                  <div class="row">
+                    <div class="col-6">
+                      <p>
+                        Ensuring inclusive and equitable quality education and promoting lifelong learning opportunities through a re-engineered polytechnic university by committing to:
+                      </p>
+                    </div>
+                    <div class="col-6">
+                      <p>
+                        Tinitiyak na makapaghain ng edukasyon na may kalidad para sa lahat at magsulong ng panghabambuhay na pagkatuto sa pamamagitan nang patuloy na paglinang ng politeknikong unibersidad na may paninidigan sa:
+                      </p>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-6">
+                      <ul>
+                        <li>provide democratized access to educational opportunities for the holistic development of individuals with global perspective</li>
+                        <li>offer industry-oriented curricula that produce highly-skilled professionals with managerial and technical capabilities and a strong sense of public service for nation building</li>
+                        <li>embed a culture of research and innovation</li>
+                        <li>continuously develop faculty and employees with the highest level of professionalism</li>
+                        <li>engage public and private institutions and other stakeholders for the attainment of social development goal</li>
+                        <li>establish a strong presence and impact in the international academic community</li>
+                      </ul>
+                    </div>
+                    <div class="col-6">
+                      <ul>
+                        <li>pagbigay ng pantay na pang-akademikong oportunidad para sa holistikong pag-unlad ng indibidwal na may pandaigdigang perspektiba</li>
+                        <li>paghain ng akademikong programa na tumutugon sa pangangailangan ng industriya na magluluwal ng mga propesyunal na may kahusayan sa pamamahala at kasanayang teknikal gayundin may matatag na kamalayang mapaglingkod para sa pagbuo ng bansa</li>
+                        <li>paglangkap ng kultura ng pananaliksik at inobasyon</li>
+                        <li>patuloy na pagpapaunlad ng kaguruan at kawani sa pinakamataas na antas ng propesyunalisasyon</li>
+                        <li>paghikayat sa pampubliko at pribadong institusyon at iba pang pinaglilingkuran para sa pagkakamit ng hangaring panlipunang pag-unlad</li>
+                        <li>paglikha nang matatag na pag-iral at impluwensya sa internasyunal na komunidad pang-akademiko</li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="ms-2 mt-4">
+                <h3 class="mb-3 text-primary">The PUP Philosophy (Pilosopiya ng PUP)</h3>
+                <div class="mx-5">
+                  <div class="row">
+                    <div class="col-6">
+                      <p>
+                        As a state university, the Polytechnic University of the Philippines believes that:
+                      </p>
+                    </div>
+                    <div class="col-6">
+                      <p>
+                        Bilang pambansang unibersidad, naniniwala ang Politeknikong Unibersidad ng Pilipinas na:
+                      </p>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-6">
+                      <ul>
+                        <li>Education is an instrument for the development of the citizenry and for the enhancement of nation building; and</li>
+                        <li>That meaningful growth and transformation of the country are best achieved in an atmosphere of brotherhood, peace, freedom, justice and nationalist-oriented education imbued with the spirit of humanist internationalism.</li>
+                      </ul>
+                    </div>
+                    <div class="col-6">
+                      <ul>
+                        <li>Ang edukasyon ay instrumento para sa pagpapaunlad ng mamamayan at pagpapahusay ng pagbuo ng bansa at</li>
+                        <li>Ang makabuluhang pag-unlad at pagbabago sa bansa ay matagumpay na makakamtan sa esensya ng kapatiran, kapayapaan, kalayaan, katarungan at makabayang oryentasyon ng edukasyon na may pagbuo ng diwa ng pagkamakatao at internasyunalismo.</li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="ms-2 mt-4">
+                <h3 class="mb-3 text-primary">Ten Pillars (Sampung Haligi)</h3>
+                <div class="mx-5">
+                  <div class="row">
+                    <div class="col-6">
+                      <p class="mb-1">
+                        Pillar 1: Dynamic, Transformational, and Responsible Leadership
+                      </p>
+                      <p class="mb-1">
+                        Pillar 2: Responsive and Innovative Curricula and Instruction
+                      </p>
+                      <p class="mb-1">
+                        Pillar 3: Enabling and Productive Learning Environment
+                      </p>
+                      <p class="mb-1">
+                        Pillar 4: Holistic Student Development and Engagement
+                      </p>
+                      <p class="mb-1">
+                        Pillar 5: Empowered Faculty Members and Employees
+                      </p>
+                      <p class="mb-1">
+                        Pillar 6: Vigorous Research Production and Utilization
+                      </p>
+                      <p class="mb-1">
+                        Pillar 7: Global Academic Standards and Excellence
+                      </p>
+                      <p class="mb-1">
+                        Pillar 8: Synergistic, Productive, Strategic Networks and Partnerships
+                      </p>
+                      <p class="mb-1">
+                        Pillar 9: Active and Sustained Stakeholdersâ€™ Engagement
+                      </p>
+                      <p class="mb-1">
+                        Pillar 10: Sustainable Social Development Programs and Projects
+                      </p>
+                    </div>
+                    <div class="col-6">
+                      <p class="mb-1">
+                        Haligi 1: Dinamiko, Mapagbago at Responsableng Pamumuno
+                      </p>
+                      <p class="mb-1">
+                        Haligi 2: Tumutugon at Makabagong Kurikula at Pagtuturo
+                      </p>
+                      <p class="mb-1">
+                        Haligi 3: Nakatutulong at Produktibong Kapaligirang Pampagkatuto
+                      </p>
+                      <p class="mb-1">
+                        Haligi 4: Holistiko at Nakahihikayat na Pagpapaunlad Pang-mag-aaral
+                      </p>
+                      <p class="mb-1">
+                        Haligi 5: Pinahusay na Dalubguro at Kawani
+                      </p>
+                      <p class="mb-1">
+                        Haligi 6: Masikhay na Produksyon at Pakikinabang ng Pananaliksik
+                      </p>
+                      <p class="mb-1">
+                        Haligi 7: Pandaigdigang Pamantayang Akademiko at Kahusayan
+                      </p>
+                      <p class="mb-1">
+                        Haligi 8: Kolaborasyon, Produktibo, Estratehikong Ugnayan at Pagtutuwang
+                      </p>
+                      <p class="mb-1">
+                        Haligi 9: Aktibo at Napapanatiling Ugnayan sa Pinaglilingkuran
+                      </p>
+                      <p class="mb-1">
+                        Haligi 10: Patuluyang Programa at Proyektong Pagpapaunlad Panlipunan
+                      </p>
+                    </div>
+                  </div>
+                  <div class="mt-3">
+                    <iframe width="100%" height="650" src="https://www.youtube.com/embed/i7qQVvI8fOI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                  </div>
+                </div>
+              </div>
+              <div class="ms-2 mt-4">
+                <h3 class="mb-3 text-primary">Shared Values and Principles (Bahaginang Pagpapahalaga at Prinsipyo)</h3>
+                <div class="mx-5">
+                  <div class="row">
+                    <div class="col-6">
+                      <ul>
+                        <li>Integrity and Accountability</li>
+                        <li>Nationalism</li>
+                        <li>Spirituality</li>
+                        <li>Passion for Learning and Innovation</li>
+                        <li>Inclusivity</li>
+                        <li>Respect for Human Rights and The Environment</li>
+                        <li>Excellence</li>
+                        <li>Democracy</li>
+                      </ul>
+                    </div>
+                    <div class="col-6">
+                      <ul>
+                        <li>Integridad at Pananagutan</li>
+                        <li>Makabayan</li>
+                        <li>Espiritwalidad</li>
+                        <li>Masidhing Hangarin para sa Pagkatuto at Inobasyon</li>
+                        <li>Mapagbuo</li>
+                        <li>Paggalang sa Karapatang Pantao at Kalikasan</li>
+                        <li>Kahusayan</li>
+                        <li>Kalayaan</li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div class="mt-3">
+                    <iframe width="100%" height="650" src="https://www.youtube.com/embed/Ik9XpA-nsPs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                  </div>
+                </div>
+              </div>
+              <div class="ms-2 mt-5">
+                <h3 class="mb-3 text-primary">Tagline</h3>
+                <div class="mx-5 text-center">
+                  <h2>The Country's 1st Polytechnic U</h2>
+                </div>
+              </div>
+              <div class="ms-2 mt-5">
+                <h3 class="mb-3 text-primary">Mandate</h3>
+                <p>
+                  <a href="https://www.pup.edu.ph/about/pd1341">Presidential Decree No.1341</a>
+                  mandated the Polytechnic University of the Philippines to expand the program offerings of the University to include courses in polytechnic areas and has also given the University the authority to expand diametrically through the establishment of branches, consortia and linkages.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!--end card-body-->
+      </div>
+      <!-- end card -->
+    </div>
+  </div>
+  <!--end col-->
+</div>
+<!--end row-->
