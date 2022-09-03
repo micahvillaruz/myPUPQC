041b477421d1962ee1acd277429c023371538700
James Paul Tulod <paultulod@pm.me>
2022-09-04 01:04:34 +0800

    Change navbar links

--- application/views/partials/home/home-header.php
+++ application/views/partials/home/home-header.php
@@ -11,146 +11,147 @@
           <i class="mdi mdi-menu"></i>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mx-auto mt-2 mt-lg-0 dropdown" id="navbar-nav">
+        <div class="collapse navbar-collapse align-middle" id="navbarSupportedContent">
+          <ul class="navbar-nav mx-auto mt-2 mt-lg-0 text-primary" id="navbar-nav">
             <li class="nav-item">
-              <a class="nav-link active" href="<?= base_url() ?>">Home</a>
+              <a class="nav-link" aria-expanded="false" href="<?= base_url() ?>"><i class="bx bxs-home me-2"></i><span>Home</span></a>
             </li>
             <li class="nav-item">
-              <div class="dropdown">
-                <a class="nav-link dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-                  About PUP
-                </a>
-                <ul class="list-group-flush dropdown-menu" aria-labelledby="dropdownMenuButton">
-                  <li class="list-group-item dropdown-item">
-                    <a class="" href="<?= base_url() ?>about">University Profile</a>
+              <a class="nav-link" href="#">
+                <i class="bx bxs-school me-2"></i><span>About PUP</span>
+              </a>
+              <div class="collapse menu-dropdown">
+                <ul class="nav nav-sm flex-column">
+                  <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url() ?>about">University Profile</a>
                   </li>
-                  <li class="list-group-item dropdown-item">
-                    <a class="" href="<?= base_url() ?>about/vm">Vision and Mission</a>
+                  <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url() ?>about/vm">Vision and Mission</a>
                   </li>
-                  <li class="list-group-item dropdown-item">
-                    <a class="" href="<?= base_url() ?>about/history">History</a>
+                  <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url() ?>about/history">History</a>
                   </li>
-                  <li class="list-group-item dropdown-item">
-                    <a class="" href="<?= base_url() ?>about/logo-and-symbols">Logo and Symbols</a>
+                  <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url() ?>about/logo-and-symbols">Logo and Symbols</a>
                   </li>
-                  <li class="list-group-item dropdown-item">
-                    <a class="" href="<?= base_url() ?>about/hymn">Hymn</a>
+                  <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url() ?>about/hymn">Hymn</a>
                   </li>
-                  <li class="list-group-item dropdown-item">
-                    <a class="" href="<?= base_url() ?>about/annual-reports">Annual Reports</a>
+                  <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url() ?>about/annual-reports">Annual Reports</a>
                   </li>
-                  <li class="list-group-item dropdown-item">
-                    <a class="" href="<?= base_url() ?>about/contact-us">Contact us</a>
+                  <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url() ?>about/contact-us">Contact us</a>
                   </li>
-                  <li class="list-group-item dropdown-item">
-                    <a class="" href="<?= base_url() ?>about/maps">Maps</a>
+                  <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url() ?>about/maps">Maps</a>
                   </li>
-                  <li class="list-group-item dropdown-item">
-                    <a class="" href="<?= base_url() ?>about/code">University Code</a>
+                  <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url() ?>about/code">University Code</a>
                   </li>
-                  <li class="list-group-item dropdown-item">
-                    <a class="" href="<?= base_url() ?>about/memorandum-orders">Memorandum Orders</a>
+                  <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url() ?>about/memorandum-orders">Memorandum Orders</a>
                   </li>
-                  <li class="list-group-item dropdown-item">
-                    <a class="" href="<?= base_url() ?>about/executive-orders">Executive Orders</a>
+                  <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url() ?>about/executive-orders">Executive Orders</a>
                   </li>
-                  <li class="list-group-item dropdown-item">
-                    <a class="" href="<?= base_url() ?>about/codi-manual">CODI Manual</a>
+                  <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url() ?>about/codi-manual">CODI Manual</a>
                   </li>
-                  <li class="list-group-item dropdown-item">
-                    <a class="" href="<?= base_url() ?>about/citizens-charter">Citizens Charter</a>
+                  <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url() ?>about/citizens-charter">Citizens Charter</a>
                   </li>
                 </ul>
               </div>
             </li>
             <li class="nav-item">
-              <div class="dropdown">
-                <a class="nav-link dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-                  Academic
-                </a>
-                <ul class="list-group-flush dropdown-menu" aria-labelledby="dropdownMenuButton">
-                  <li class="list-group-item dropdown-item">
-                    <a class="" href="<?= base_url() ?>academic/programs">Academic Programs</a>
+              <a class="nav-link" href="#">
+                <i class="bx bxs-book me-2"></i><span>Academic</span>
+              </a>
+              <div class="collapse menu-dropdown">
+                <ul class="nav nav-sm flex-column">
+                  <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url() ?>academic/programs">Academic Programs</a>
                   </li>
-                  <li class="list-group-item dropdown-item">
-                    <a class="" href="<?= base_url() ?>academic/ous">Open University System</a>
+                  <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url() ?>academic/ous">Open University System</a>
                   </li>
-                  <li class="list-group-item dropdown-item">
-                    <a class="" href="https://www.pup.edu.ph/nallrc/" target="_blank">Library Services</a>
-                    <!-- <a class="" href="<?= base_url() ?>academic/library">Library Services</a> -->
+                  <li class="nav-item">
+                    <a class="nav-link" href="https://www.pup.edu.ph/nallrc/" target="_blank">Library Services</a>
                   </li>
-                  <li class="list-group-item dropdown-item">
-                    <a class="" href="<?= base_url() ?>academic/ovpaa">OVPAA Advisories</a>
-                  </li>
-                </ul>
-              </div>
-            </li>
-            <li class="nav-item">
-              <div class="dropdown">
-                <a class="nav-link dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-                  Students
-                </a>
-                <ul class="list-group-flush dropdown-menu" aria-labelledby="dropdownMenuButton">
-                  <li class="list-group-item dropdown-item">
-                    <a class="" href="<?= base_url() ?>#student-services">Student Services</a>
-                  </li>
-                  <li class="list-group-item dropdown-item">
-                    <a class="" href="<?= base_url() ?>student/organizations">Student Organizations</a>
-                  </li>
-                  <li class="list-group-item dropdown-item">
-                    <a class="" href="<?= base_url() ?>student/publications">Student Publications</a>
-                  </li>
-                  <li class="list-group-item dropdown-item">
-                    <a class="" href="<?= base_url() ?>student/council">Student Council</a>
-                  </li>
-                  <li class="list-group-item dropdown-item">
-                    <a class="" href="<?= base_url() ?>student/handbook">Student Handbook</a>
-                  </li>
-                  <li class="list-group-item dropdown-item">
-                    <a class="" href="https://sisstudents.pup.edu.ph/">PUP Student Information System</a>
-                  </li>
-                  <li class="list-group-item dropdown-item">
-                    <a class="" href="https://odrs.pup.edu.ph/">PUP Online Document Request System</a>
-                  </li>
-                  <li class="list-group-item dropdown-item">
-                    <a class="" href="https://apps.pup.edu.ph/books/">PUP Bookstore</a>
-                  </li>
-                  <li class="list-group-item dropdown-item">
-                    <a class="" href="<?= base_url() ?>student/downloads">Downloadable Forms</a>
+                  <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url() ?>academic/ovpaa">OVPAA Advisories</a>
                   </li>
                 </ul>
               </div>
             </li>
             <li class="nav-item">
-              <div class="dropdown">
-                <a class="nav-link dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-                  Research
-                </a>
-                <ul class="list-group-flush dropdown-menu" aria-labelledby="dropdownMenuButton">
-                  <li class="list-group-item dropdown-item">
-                    <a class="" href="<?= base_url() ?>research/researches">Researches</a>
+              <a class="nav-link" href="#">
+                <i class="bx bxs-graduation me-2"></i><span>Students</span>
+              </a>
+              <div class="collapse menu-dropdown">
+                <ul class="nav nav-sm flex-column">
+                  <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url() ?>#student-services">Student Services</a>
                   </li>
-                  <li class="list-group-item dropdown-item">
-                    <a class="" href="<?= base_url() ?>research/extensions">Extensions</a>
+                  <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url() ?>student/organizations">Student Organizations</a>
                   </li>
-                  <li class="list-group-item dropdown-item">
-                    <a class="" href="<?= base_url() ?>research/research-ethics">Research Ethics</a>
+                  <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url() ?>student/publications">Student Publications</a>
                   </li>
-                  <li class="list-group-item dropdown-item">
-                    <a class="" href="<?= base_url() ?>research/intellectual-property">Intellectual Property</a>
+                  <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url() ?>student/council">Student Council</a>
                   </li>
-                  <li class="list-group-item dropdown-item">
-                    <a class="" href="https://apps.pup.edu.ph/ojs/">PUP Online Journal System</a>
+                  <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url() ?>student/handbook">Student Handbook</a>
                   </li>
-                  <li class="list-group-item dropdown-item">
-                    <a class="" href="https://drive.google.com/open?id=0B1BuDAuN0r8SMFpBYWNGckhUWkE">University Thesis and Dissertation Manual</a>
+                  <li class="nav-item">
+                    <a class="nav-link" href="https://sisstudents.pup.edu.ph/">PUP Student Information System</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="https://odrs.pup.edu.ph/">PUP Online Document Request System</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="https://apps.pup.edu.ph/books/">PUP Bookstore</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url() ?>student/downloads">Downloadable Forms</a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <i class="bx bxs-search me-2"></i><span>Research</span>
+              </a>
+              <div class="collapse menu-dropdown">
+                <ul class="nav nav-sm flex-column">
+                  <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url() ?>about">University Profile</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url() ?>research/researches">Researches</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url() ?>research/extensions">Extensions</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url() ?>research/research-ethics">Research Ethics</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url() ?>research/intellectual-property">Intellectual Property</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="https://apps.pup.edu.ph/ojs/">PUP Online Journal System</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="https://drive.google.com/open?id=0B1BuDAuN0r8SMFpBYWNGckhUWkE">University Thesis and Dissertation Manual</a>
                   </li>
                 </ul>
               </div>
             </li>
           </ul>
-
           <div class="">
             <a href="<?= base_url() ?>signin" class="btn btn-primary">Log In</a>
           </div>
---
+++ application/views/partials/main-home.php
@@ -0,0 +1,2 @@
+<!DOCTYPE html>
+<html lang="en" data-layout="horizontal" data-topbar="light" data-sidebar="dark" data-sidebar-image="none" data-layout-width="fluid">
