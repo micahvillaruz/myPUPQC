f5fde02e2e1801f6cde2a85459d7ded38a5090dd
Zsyrhill Soria <zsyrhillsoria@gmail.com>
2022-09-05 15:53:52 +0800

    added evrsers pages

---
+++ application/views/student/evrsers/new-reservation.php
@@ -0,0 +1,91 @@
+<div class="row">
+    <div class="col-lg-12">
+        <div class="card">
+            <div class="card-header">
+                <h5 class="card-title mb-0 text-primary">New Reservation</h5>
+            </div>
+            <div class="card-body">
+                <form action="#">
+                    <div class="row g-3">
+                        <div class="col-lg-6">
+                            <div class="form-floating">
+                                <input type="text" class="form-control" id="fullnamefloatingInput" placeholder="Student Full Name">
+                                <label for="fullnamefloatingInput">Full Name</label>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-floating">
+                                <input type="text" class="form-control" id="orgNamefloatingInput" placeholder="Student Organization">
+                                <label for="orgNamefloatingInput">Organization Name</label>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="form-floating">
+                                <input type="text" class="form-control" id="eventTitlefloatingInput" placeholder="Student Full Name">
+                                <label for="eventTitlefloatingInput">Event Title</label>
+                            </div>
+                        </div>
+                        <div class="col-lg-8">
+                            <div class="form-floating">
+                                <input type="text" class="form-control" id="eventDetailsfloatingInput" placeholder="Student Organization">
+                                <label for="eventDetailsfloatingInput">Event Details</label>
+                            </div>
+                        </div>
+                        <div class="col-lg-3">
+                            <div class="form-floating">
+                                <input type="number" class="form-control" id="numberfloatingInput" placeholder="Number of Attendees">
+                                <label for="numberfloatingInput">Number of Attendees</label>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="form-floating">
+                                <input type="date" class="form-control" id="reserveDatefloatingInput" placeholder="Enter your password">
+                                <label for="reserveDatefloatingInput">Reserve Date</label>
+                            </div>
+                        </div>
+                        <div class="col-lg-2">
+                            <div class="form-floating">
+                                <input type="text" class="form-control" id="timeFromfloatingInput" placeholder="">
+                                <label for="timeFromfloatingInput">Time From</label>
+                            </div>
+                        </div>
+                        <div class="col-lg-2">
+                            <div class="form-floating">
+                                <input type="text" class="form-control" id="timeTofloatingInput" placeholder="">
+                                <label for="timeTofloatingInput">Time To</label>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="form-floating">
+                                <input type="text" class="form-control" id="cityfloatingInput" placeholder="Enter your city">
+                                <label for="cityfloatingInput">City</label>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="form-floating">
+                                <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
+                                    <option selected>USA</option>
+                                    <option value="1">Brazil</option>
+                                    <option value="2">France</option>
+                                    <option value="3">Germany</option>
+                                </select>
+                                <label for="floatingSelect">Country</label>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="form-floating">
+                                <input type="number" class="form-control" id="zipfloatingInput" placeholder="Enter your zipcode">
+                                <label for="zipfloatingInput">Zipcode</label>
+                            </div>
+                        </div>
+                        <div class="col-lg-12">
+                            <div class="text-end">
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
