fbdf409715cab2c302db3805fdf8146f9919a7e3
Zsyrhill Soria <zsyrhillsoria@gmail.com>
2022-09-05 17:18:46 +0800

    updated evrsers reservation

---
+++ application/views/student/evrsers/view-reservations.php
@@ -0,0 +1,111 @@
+<div class="row">
+    <div class="col-lg-12">
+        <div class="card">
+            <div class="card-header d-flex align-items-center">
+                <h5 class="card-title mb-0 flex-grow-1">Venue Reservations</h5>
+                <div>
+                    <!-- <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addStaffModal">Add Faculty/Staff</button> -->
+                </div>
+            </div>
+            <div class="card-body">
+                <table id="example" class="table table-bordered dt-responsive nowrap table-striped align-middle text-center" style="width: 100%">
+                    <thead>
+                        <tr>
+                            <th>Doctor ID.</th>
+                            <th>Doctor for consultation</th>
+                            <th>Patient</th>
+                            <th>Service</th>
+                            <th>Date</th>
+                            <th>Time</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>00014414</td>
+                            <td>Dr. Melissa Sarappudin</td>
+                            <td>Shein</td>
+                            <td>Covid</td>
+                            <td>09-01-2022</td>
+                            <td>1:00 - 2:00PM</td>
+                            <td><span class="badge rounded-pill bg-success">Accept</span></td>
+                            <td>
+                                <div class="dropdown d-inline-block">
+                                    <button type="button" class="btn btn-info btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#viewMedicalModal"><i class="ri-eye-fill"></i></button>
+                                    <button type="button" class="btn btn-warning btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#editMedicalModal"><i class="ri-edit-2-fill"></i></button>
+                                    <button type="button" class="btn btn-danger btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#deleteMedicalModal"><i class="ri-delete-bin-5-fill"></i></button>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>00014414</td>
+                            <td>Dr. Melissa Sarappudin</td>
+                            <td>Shein</td>
+                            <td>Cancer</td>
+                            <td>09-02-2022</td>
+                            <td>2:00 - 3:00PM</td>
+                            <td><span class="badge rounded-pill bg-danger">Denied</span></td>
+                            <td>
+                                <div class="dropdown d-inline-block">
+                                    <button type="button" class="btn btn-info btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#viewMedicalModal"><i class="ri-eye-fill"></i></button>
+                                    <button type="button" class="btn btn-warning btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#editMedicalModal"><i class="ri-edit-2-fill"></i></button>
+                                    <button type="button" class="btn btn-danger btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#deleteMedicalModal"><i class="ri-delete-bin-5-fill"></i></button>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>00014414</td>
+                            <td>Dr. Melissa Sarappudin</td>
+                            <td>Shein</td>
+                            <td>Fever</td>
+                            <td>09-03-2022</td>
+                            <td>3:00 - 4:00PM</td>
+                            <td><span class="badge rounded-pill bg-secondary">Pending</span></td>
+                            <td>
+                                <div class="dropdown d-inline-block">
+                                    <button type="button" class="btn btn-info btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#viewMedicalModal"><i class="ri-eye-fill"></i></button>
+                                    <button type="button" class="btn btn-warning btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#editMedicalModal"><i class="ri-edit-2-fill"></i></button>
+                                    <button type="button" class="btn btn-danger btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#deleteMedicalModal"><i class="ri-delete-bin-5-fill"></i></button>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>00014414</td>
+                            <td>Dr. Melissa Sarappudin</td>
+                            <td>Shein</td>
+                            <td>Diarrhea</td>
+                            <td>09-04-2022</td>
+                            <td>4:00 - 5:00PM</td>
+                            <td><span class="badge rounded-pill bg-success">Accept</span></td>
+                            <td>
+                                <div class="dropdown d-inline-block">
+                                    <button type="button" class="btn btn-info btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#viewMedicalModal"><i class="ri-eye-fill"></i></button>
+                                    <button type="button" class="btn btn-warning btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#editMedicalModal"><i class="ri-edit-2-fill"></i></button>
+                                    <button type="button" class="btn btn-danger btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#deleteMedicalModal"><i class="ri-delete-bin-5-fill"></i></button>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>00014414</td>
+                            <td>Dr. Melissa Sarappudin</td>
+                            <td>Shein</td>
+                            <td>Allergy</td>
+                            <td>09-05-2022</td>
+                            <td>10:00 AM - 11:00 AM</td>
+                            <td><span class="badge rounded-pill bg-danger">Denied</span></td>
+                            <td>
+                                <div class="dropdown d-inline-block">
+                                    <button type="button" class="btn btn-info btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#viewMedicalModal"><i class="ri-eye-fill"></i></button>
+                                    <button type="button" class="btn btn-warning btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#editMedicalModal"><i class="ri-edit-2-fill"></i></button>
+                                    <button type="button" class="btn btn-danger btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#deleteMedicalModal"><i class="ri-delete-bin-5-fill"></i></button>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    <!--end col-->
+</div>
