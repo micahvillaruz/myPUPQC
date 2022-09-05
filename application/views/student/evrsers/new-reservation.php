a93c4c111462b33ffcf385f1a1bcda00adef6bef
Zsyrhill Soria <zsyrhillsoria@gmail.com>
2022-09-05 16:21:05 +0800

    updated create reservation

--- application/views/student/evrsers/new-reservation.php
+++ application/views/student/evrsers/new-reservation.php
@@ -37,7 +37,7 @@
                                 <label for="numberfloatingInput">Number of Attendees</label>
                             </div>
                         </div>
-                        <div class="col-lg-4">
+                        <div class="col-lg-5">
                             <div class="form-floating">
                                 <input type="date" class="form-control" id="reserveDatefloatingInput" placeholder="Enter your password">
                                 <label for="reserveDatefloatingInput">Reserve Date</label>
@@ -45,37 +45,56 @@
                         </div>
                         <div class="col-lg-2">
                             <div class="form-floating">
-                                <input type="text" class="form-control" id="timeFromfloatingInput" placeholder="">
+                                <select class="form-select" id="timeFromfloatingInput" placeholder="">
+                                    <option selected disabled>- Select Time -</option>
+                                    <option>8:00 AM</option>
+                                    <option>9:00 AM</option>
+                                    <option>10:00 AM</option>
+                                    <option>11:00 AM</option>
+                                    <option>1:00 PM</option>
+                                    <option>2:00 PM</option>
+                                    <option>3:00 PM</option>
+                                    <option>4:00 PM</option>
+                                    <option>5:00 PM</option>
+                                    <option>6:00 PM</option>
+                                    <option>7:00 PM</option>
+                                    <option>8:00 PM</option>
+                                    <option>9:00 PM</option>
+                                </select>
                                 <label for="timeFromfloatingInput">Time From</label>
                             </div>
                         </div>
                         <div class="col-lg-2">
                             <div class="form-floating">
-                                <input type="text" class="form-control" id="timeTofloatingInput" placeholder="">
+                                <select class="form-select" id="timeTofloatingInput" placeholder="">
+                                <option selected disabled>- Select Time -</option>
+                                    <option>8:00 AM</option>
+                                    <option>9:00 AM</option>
+                                    <option>10:00 AM</option>
+                                    <option>11:00 AM</option>
+                                    <option>1:00 PM</option>
+                                    <option>2:00 PM</option>
+                                    <option>3:00 PM</option>
+                                    <option>4:00 PM</option>
+                                    <option>5:00 PM</option>
+                                    <option>6:00 PM</option>
+                                    <option>7:00 PM</option>
+                                    <option>8:00 PM</option>
+                                    <option>9:00 PM</option>
+                                </select>
                                 <label for="timeTofloatingInput">Time To</label>
                             </div>
                         </div>
                         <div class="col-lg-4">
-                            <div class="form-floating">
-                                <input type="text" class="form-control" id="cityfloatingInput" placeholder="Enter your city">
-                                <label for="cityfloatingInput">City</label>
+                            <div class="form-control">
+                                <label for="attachfloatingInput">Relevant Attachments</label>
+                                <input type="file" class="form-control" id="attachfloatingInput" placeholder="Insert attachments">
                             </div>
                         </div>
-                        <div class="col-lg-4">
+                        <div class="col-lg-8">
                             <div class="form-floating">
-                                <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
-                                    <option selected>USA</option>
-                                    <option value="1">Brazil</option>
-                                    <option value="2">France</option>
-                                    <option value="3">Germany</option>
-                                </select>
-                                <label for="floatingSelect">Country</label>
-                            </div>
-                        </div>
-                        <div class="col-lg-4">
-                            <div class="form-floating">
-                                <input type="number" class="form-control" id="zipfloatingInput" placeholder="Enter your zipcode">
-                                <label for="zipfloatingInput">Zipcode</label>
+                                <input type="text" class="form-control" id="remarksfloatingInput" placeholder="Please enter reservation details" disabled>
+                                <label for="remarksfloatingInput">Admin Remarks</label>
                             </div>
                         </div>
                         <div class="col-lg-12">
