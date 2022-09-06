d3c9fdb096f8aea2298ec480dcdfe669d13604d5
Micah Villaruz <mmvillaruz@iskolarngbayan.pup.edu.ph>
2022-09-07 01:03:21 +0800

    Add admin edit profile ajax

--- application/views/admin/scripts/settings-scripts.php
+++ application/views/admin/scripts/settings-scripts.php
@@ -6,6 +6,17 @@
 
 		<script src="<?= base_url() ?>public/js/pages/select2.init.js"></script>
 
+		<script src="<?= base_url() ?>public/js/pages/form-validation.init.js"></script>
+
+		<!-- Sweet Alerts js -->
+		<script src="<?= base_url() ?>public/libs/sweetalert2/sweetalert2.min.js"></script>
+
+		<!-- Sweet alert init js-->
+		<script src="<?= base_url() ?>public/js/pages/sweetalerts.init.js"></script>
+
+		<!-- Settings ajax -->
+		<script src="<?= base_url() ?>public/js/ajax/admin/settings.ajax.js"></script>
+
 		<!-- App js -->
 		<script src="<?= base_url() ?>public/js/app.js"></script>
 		</body>
---
+++ public/js/ajax/admin/settings.ajax.js
@@ -0,0 +1,96 @@
+$(function () {
+	editProfile()
+
+	$('#profileSettingsForm').on('submit', function (e) {
+		e.preventDefault() // prevent page refresh
+		// pass data to API for updating of student's info
+		editProfileAJAX()
+	})
+})
+
+// Edit Admin Details
+editProfile = () => {
+	$.ajax({
+		url: apiURL + `super_admin/info`,
+		type: 'GET',
+		headers: AJAX_HEADERS,
+		success: (result) => {
+			if (result) {
+				// Get data from result
+				const data = result.data
+				// $('#edit_image').val(data.image)
+
+				$('#first_name').val(data.first_name)
+				$('#middle_name').val(data.middle_name)
+				$('#last_name').val(data.last_name)
+				$('#extension_name').val(data.extension_name)
+				const birth_date = data.birth_date
+				let birthDateFormatted = `${moment(birth_date).format('LL')}`
+				$('#birth_date').val(birthDateFormatted)
+				$('#gender').val(data.gender)
+				$('#contact_number').val(data.contact_number)
+				$('#house_street').val(data.house_street)
+				$('#barangay').val(data.barangay)
+				$('#municipality').val(data.municipality)
+				$('#province').val(data.province)
+				$('#region').val(data.region)
+			}
+		},
+	}).fail(() => console.error('There was an error in retrieving admin data'))
+}
+
+// Edit Admin AJAX
+editProfileAJAX = () => {
+	// Get data from form
+	if ($('#profileSettingsForm')[0].checkValidity()) {
+		// no validation error
+		const form = new FormData($('#profileSettingsForm')[0])
+
+		data = {
+			image: null,
+			first_name: form.get('first_name'),
+			middle_name: form.get('middle_name'),
+			last_name: form.get('last_name'),
+			extension_name: form.get('extension_name'),
+			contact_number: form.get('contact_number'),
+			birth_date: form.get('birth_date'),
+			gender: form.get('gender'),
+			house_street: form.get('house_street'),
+			barangay: form.get('barangay'),
+			municipality: form.get('municipality'),
+			province: form.get('province'),
+			region: form.get('region'),
+		}
+
+		$.ajax({
+			url: apiURL + `super_admin/info`,
+			type: 'PUT',
+			data: data,
+			dataType: 'json',
+			headers: AJAX_HEADERS,
+			success: (result) => {
+				if (result) {
+					Swal.fire({
+						html: '<div class="mt-3"><lord-icon src="https://cdn.lordicon.com/lupuorrc.json" trigger="loop" colors="primary:#0ab39c,secondary:#405189" style="width:120px;height:120px"></lord-icon><div class="mt-4 pt-2 fs-15"><h4>Well done !</h4><p class="text-muted mx-4 mb-0">You have successfully updated your information!</p></div></div>',
+						showCancelButton: !0,
+						showConfirmButton: !1,
+						cancelButtonClass: 'btn btn-primary w-xs mb-1',
+						cancelButtonText: 'Ok',
+						buttonsStyling: !1,
+						showCloseButton: !0,
+					})
+				}
+			},
+		}).fail(() => {
+			Swal.fire({
+				html: '<div class="mt-3"><lord-icon src="https://cdn.lordicon.com/tdrtiskw.json" trigger="loop" colors="primary:#f06548,secondary:#f7b84b" style="width:120px;height:120px"></lord-icon><div class="mt-4 pt-2 fs-15"><h4>Something went Wrong !</h4><p class="text-muted mx-4 mb-0">There was an error while updating your details. Please try again.</p></div></div>',
+				showCancelButton: !0,
+				showConfirmButton: !1,
+				cancelButtonClass: 'btn btn-primary w-xs mb-1',
+				cancelButtonText: 'Dismiss',
+				buttonsStyling: !1,
+				showCloseButton: !0,
+			})
+		})
+	}
+}
