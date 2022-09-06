ac4cd3ea80964414a20ccda3d4bacaccce5af1a5
Micah Villaruz <mmvillaruz@iskolarngbayan.pup.edu.ph>
2022-09-06 19:32:10 +0800

    Add Helpdesk Link

--- application/views/partials/student/sidebar.php
+++ application/views/partials/student/sidebar.php
@@ -27,8 +27,7 @@
             <span>Dashboard</span>
           </a>
         </li>
-        <!-- end Dashboard Menu -->
-        <li class="nav-item">
+        <!--         <li class="nav-item">
           <a class="nav-link menu-link">
             <i class="ri-calendar-todo-fill"></i>
             <span>Calendar</span>
@@ -40,8 +39,7 @@
             <i class="ri-customer-service-2-fill"></i>
             <span>Announcements</span>
           </a>
-        </li>
-        <!-- end Dashboard Menu -->
+        </li> -->
 
         <li class="menu-title">
           <i class="ri-more-fill"></i>
@@ -62,7 +60,7 @@
           </a>
         </li>
 
-        <li class="nav-item">
+        <!--         <li class="nav-item">
           <a class="nav-link menu-link">
             <i class="ri-hand-coin-line"></i>
             <span>Scholarships</span>
@@ -115,11 +113,11 @@
               </li>
             </ul>
           </div>
-        </li>
+        </li> -->
 
         <li class="nav-item">
-          <a class="nav-link menu-link">
-            <i class="ri-question-answer-line"></i>
+          <a class="nav-link menu-link" href="https://osssac.pup.edu.ph/">
+            <i class=" ri-question-answer-line"></i>
             <span>HelpDesk</span>
           </a>
         </li>
