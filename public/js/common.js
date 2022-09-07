0e87489b5f934c066857d97132cee47f9d038e32
James Paul Tulod <paultulod@pm.me>
2022-09-07 18:17:01 +0800

    Update common js

--- public/js/common.js
+++ public/js/common.js
@@ -1,5 +1,12 @@
-const baseURL = 'http://localhost/myPUPQC/'
-const apiURL = 'https://mypupqc-api.azurewebsites.net/mypupqc/v1/'
+// * prod = production, dev = development
+const webEnv = 'dev'
+const apiEnv = 'prod'
+
+const baseURL = webEnv === 'prod' ? 'https://www.mypupqc.live/' : 'http://localhost/myPUPQC/'
+const apiURL =
+	apiEnv === 'prod'
+		? 'https://mypupqc-api.azurewebsites.net/mypupqc/v1/'
+		: 'http://localhost:3600/mypupqc/v1/'
 // Token
 const TOKEN = localStorage.getItem('TOKEN')
 
@@ -8,3 +15,6 @@ const AJAX_HEADERS = {
 	Accept: 'application/json',
 	Authorization: `Bearer ${TOKEN}`,
 }
+
+console.log(webEnv)
+console.log(apiEnv)
