68060011bde6fe6f3f0fb7e0e8af883c4cd7a981
Micah Villaruz <mmvillaruz@iskolarngbayan.pup.edu.ph>
2022-09-13 21:09:40 +0800

    Connect requests to API

--- application/views/student/odrs/requests.php
+++ application/views/student/odrs/requests.php
@@ -5,7 +5,7 @@
         <h5 class="card-title mb-0">Requests</h5>
       </div>
       <div class="card-body">
-        <table id="model-datatables" class="display dt-responsive table align-middle" style="width: 100%">
+        <table id="requests-datatable" class="display dt-responsive table align-middle" style="width: 100%">
           <thead class="bg-light">
             <tr>
               <th>Control Number</th>
@@ -17,63 +17,6 @@
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>
-                <span class="text-primary fw-medium">20220903-0088</span>
-              </td>
-              <td>
-                <div class="d-flex align-items-center">
-                  <i class="ri-calendar-todo-fill text-primary"></i>
-                  <span class="ms-2">28 Aug, 2022<small class="text-muted ms-1">08:00 AM</small></span>
-                </div>
-              </td>
-              <td>
-                <div class="d-block badge badge-soft-warning align-items-center">
-                  <i class="me-2 mdi mdi-cash-fast fs-13"></i>
-                  <span class="text-uppercase">Pending</span>
-                </div>
-              </td>
-              <td>
-                <table>
-                  <tbody>
-                    <tr>
-                      <td>
-                        <span class="fw-medium badge bg-dark me-1">Clearance: </span>
-                      </td>
-                      <td>Undergraduate Student</td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <span class="fw-medium badge bg-dark me-1">Course: </span>
-                      </td>
-                      <td>BACHELOR OF SCIENCE IN INFORMATION TECHNOLOGY</td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <span class="fw-medium badge bg-dark me-1">Purpose: </span>
-                      </td>
-                      <td>Transfer to another school</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </td>
-              <td>
-                <div class="mt-2 d-grid badge badge-soft-danger position-relative">
-                  <i class="m-2  mdi mdi-nfc-search-variant fs-13"></i>
-                  <span class="pb-2 text-uppercase">For Clearance</span>
-                  <button type="button" class="btn btn-sm btn-secondary bg-gradient waves-effect waves-light rounded-circle position-absolute top-0 start-100 translate-middle" data-bs-toggle="modal" data-bs-target="#viewProcessStatusFlow">?</button>
-                </div>
-              </td>
-              <td>
-                <div class="vstack gap-2">
-                  <button type="button" class="btn btn-sm btn-info text-start waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#viewRequestDetails"><i class="mdi mdi-eye label-icon align-middle me-2"></i> View Details</button>
-                  <a type="button" class="btn btn-sm btn-success text-start waves-effect waves-light" href="<?= base_url() ?>Public/file/Request-Form.pdf" download="Request-Form"><i class="mdi mdi-download label-icon align-middle me-2"></i> Download Request Form</a>
-                </div>
-                <div class="mt-4 d-grid">
-                  <button type="button" class="btn btn-sm btn-light waves-effect waves-light" onclick="cancelRequest()"><i class="ri-close-fill label-icon align-middle me-2"></i> Cancel</button>
-                </div>
-              </td>
-            </tr>
           </tbody>
         </table>
       </div>
--- public/js/ajax/student/odrs/requests.ajax.js
+++ public/js/ajax/student/odrs/requests.ajax.js
@@ -1,3 +1,7 @@
+$(function () {
+	loadRequestsTable()
+})
+
 // Cancel Request
 cancelRequest = () => {
 	Swal.fire({
@@ -40,7 +44,7 @@ cancelRequest = () => {
 						'<lord-icon src="https://cdn.lordicon.com/tdrtiskw.json" trigger="loop" colors="primary:#f06548,secondary:#f7b84b" style="width:120px;height:120px"></lord-icon>' +
 						'<div class="mt-4 pt-2 fs-15">' +
 						'<h4>Something went Wrong!</h4>' +
-						'<p class="text-muted mx-4 mb-0">There was an error while updating the status of this request. Please try again.</p>' +
+						'<p class="text-muted mx-4 mb-0">There was an error while cancelling this request. Please try again.</p>' +
 						'</div>' +
 						'</div>',
 					showCancelButton: !0,
@@ -53,4 +57,193 @@ cancelRequest = () => {
 			})
 		}
 	})
+}
+
+// Load Requests Table
+loadRequestsTable = () => {
+	const dt = $('#requests-datatable')
+
+	if (dt.length) {
+		dt.DataTable({
+			bDestroy: true,
+			ajax: {
+				url: apiURL + 'odrs/student/view_requests',
+				type: 'GET',
+				ContentType: 'application/x-www-form-urlencoded',
+				headers: AJAX_HEADERS,
+			},
+			columns: [
+				// Control Number
+				{
+					data: null,
+					render: (data) => {
+						return `<span class="text-primary fw-medium">${data.control_no}</span>`
+					},
+				},
+
+				// Date Filed
+				{
+					data: null,
+					render: (data) => {
+						const date = moment(data.date_of_filing).format('MMM. D, YYYY')
+						const time = moment(data.date_of_filing).format('hh:mm A')
+
+						return `
+              <div class="d-flex align-items-center">
+                <i class="ri-calendar-todo-fill text-primary"></i>
+                <span class="ms-2">${date}<small class="text-muted ms-1">${time}</small></span>
+              </div>
+            `
+					},
+				},
+
+				// Payment Status
+				{
+					data: null,
+					render: (data) => {
+						if (data.payment_status === 'Pending') {
+							return `
+							<div class="d-block badge badge-soft-warning align-items-center">
+								<i class="me-2 mdi mdi-cash-fast fs-13"></i>
+								<span class="text-uppercase">${data.payment_status}</span>
+							</div>
+							`
+						} else if (data.payment_status === 'Paid') {
+							return `
+							<div class="d-block badge badge-soft-success align-items-center">
+								<i class="me-2 mdi mdi-cash-check fs-13"></i>
+								<span class="text-uppercase">${data.payment_status}</span>
+							</div>
+              `
+						}
+					},
+				},
+
+				// Details
+				{
+					data: null,
+					render: (data) => {
+						const educationStatus = data.user_assigned_to_request.education_profile.education_status
+						const course = data.user_assigned_to_request.education_profile.course_when_admitted
+						const purpose = data.purpose_of_request
+						return `
+							<table>
+								<tbody>
+									<tr>
+										<td>
+											<span class="fw-medium badge bg-dark me-1">Clearance: </span>
+										</td>
+										<td>${educationStatus} Student</td>
+									</tr>
+									<tr>
+										<td>
+											<span class="fw-medium badge bg-dark me-1">Course: </span>
+										</td>
+										<td><span class="text-uppercase">${course}</span></td>
+									</tr>
+									<tr>
+										<td>
+											<span class="fw-medium badge bg-dark me-1">Purpose: </span>
+										</td>
+										<td>${purpose}</td>
+									</tr>
+								</tbody>
+							</table>
+						`
+					},
+				},
+
+				// Request Status
+				{
+					data: null,
+					render: (data) => {
+						const requestStatus = data.status_of_request
+
+						if (requestStatus === 'Pending for Clearance') {
+							return `
+								<div class="mt-2 d-grid fw-bolder badge badge-soft-warning position-relative">
+									<i class="m-2 mdi mdi-clock fs-13"></i>
+									<span class="pb-2 text-uppercase">${requestStatus}</span>
+									<button type="button" class="btn btn-sm btn-secondary bg-gradient waves-effect waves-light rounded-circle position-absolute top-0 start-100 translate-middle" data-bs-toggle="modal" data-bs-target="#viewProcessStatusFlow">?</button>
+								</div>
+								`
+						} else if (requestStatus === 'For Clearance') {
+							return `
+								<div class="mt-2 d-grid badge badge-soft-danger position-relative">
+									<i class="m-2  mdi mdi-nfc-search-variant fs-13"></i>
+									<span class="pb-2 text-uppercase">${requestStatus}</span>
+									<button type="button" class="btn btn-sm btn-secondary bg-gradient waves-effect waves-light rounded-circle position-absolute top-0 start-100 translate-middle" data-bs-toggle="modal" data-bs-target="#viewProcessStatusFlow">?</button>
+								</div>
+              `
+						} else if (requestStatus === 'For Evaluation/Processing') {
+							return `
+								<div class="mt-2 d-grid badge badge-soft-info position-relative">
+									<i class="m-2 mdi mdi-file-sign fs-13"></i>
+									<span class="pb-2 text-uppercase">${requestStatus}</span>
+									<button type="button" class="btn btn-sm btn-secondary bg-gradient waves-effect waves-light rounded-circle position-absolute top-0 start-100 translate-middle" data-bs-toggle="modal" data-bs-target="#viewProcessStatusFlow">?</button>
+								</div>
+              `
+						} else if (requestStatus === 'Ready for Pickup') {
+							return `
+								<div class="mt-2 d-grid badge badge-soft-dark position-relative">
+									<i class="m-2 ri-user-received-2-line fs-13"></i>
+									<span class="pb-2 text-uppercase">${requestStatus}</span>
+									<button type="button" class="btn btn-sm btn-secondary bg-gradient waves-effect waves-light rounded-circle position-absolute top-0 start-100 translate-middle" data-bs-toggle="modal" data-bs-target="#viewProcessStatusFlow">?</button>
+								</div>
+              `
+						} else if (requestStatus === 'Released') {
+							return `
+								<div class="mt-2 d-grid fw-bolder badge badge-soft-success position-relative">
+									<i class="m-2 ri-checkbox-circle-line fs-13"></i>
+									<span class="pb-2 text-uppercase">${requestStatus}</span>
+									<button type="button" class="btn btn-sm btn-secondary bg-gradient waves-effect waves-light rounded-circle position-absolute top-0 start-100 translate-middle" data-bs-toggle="modal" data-bs-target="#viewProcessStatusFlow">?</button>
+								</div>
+              `
+						} else if (requestStatus === 'Cancelled') {
+							return `
+								<div class="mt-2 d-grid fw-bolder badge badge-soft-danger position-relative">
+									<i class="m-2 mdi mdi-cancel fs-13"></i>
+									<span class="pb-2 text-uppercase">${requestStatus}</span>
+									<button type="button" class="btn btn-sm btn-secondary bg-gradient waves-effect waves-light rounded-circle position-absolute top-0 start-100 translate-middle" data-bs-toggle="modal" data-bs-target="#viewProcessStatusFlow">?</button>
+								</div>
+              `
+						} else if (requestStatus === 'Deleted') {
+							return `
+							<div class="mt-2 d-grid fw-bolder badge badge-soft-dark position-relative">
+								<i class="m-2 mdi mdi-trash-can-outline fs-13"></i>
+								<span class="pb-2 text-uppercase">${requestStatus}</span>
+								<button type="button" class="btn btn-sm btn-secondary bg-gradient waves-effect waves-light rounded-circle position-absolute top-0 start-100 translate-middle" data-bs-toggle="modal" data-bs-target="#viewProcessStatusFlow">?</button>
+							</div>
+						`
+						}
+					},
+				},
+
+				// Action
+				{
+					data: null,
+					render: (data) => {
+						const requestStatus = data.status_of_request
+
+						if (requestStatus === 'Pending For Clearance' || 'For Clearance') {
+							return `
+								<div class="vstack gap-2">
+									<button type="button" class="btn btn-sm btn-info text-start waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#viewRequestDetails"><i class="mdi mdi-eye label-icon align-middle me-2"></i> View Details</button>
+									<a type="button" class="btn btn-sm btn-success text-start waves-effect waves-light" href="<?= base_url() ?>Public/file/Request-Form.pdf" download="Request-Form"><i class="mdi mdi-download label-icon align-middle me-2"></i> Download Request Form</a>
+								</div>
+								<div class="mt-4 d-grid">
+									<button type="button" class="btn btn-sm btn-light waves-effect waves-light" onclick="cancelRequest()"><i class="ri-close-fill label-icon align-middle me-2"></i> Cancel</button>
+								</div>
+							`
+						} else {
+							return `
+								<button type="button" class="btn btn-sm btn-info text-start waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#viewRequestDetails"><i class="mdi mdi-eye label-icon align-middle me-2"></i> View Details</button>
+							`
+						}
+					},
+				},
+			],
+			order: [[0, 'asc']],
+		})
+	}
 }
