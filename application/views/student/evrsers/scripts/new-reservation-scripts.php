d29ff970f09074c9c50fbd74a415b20e1ea320f1
Zsyrhill Soria <zsyrhillsoria@gmail.com>
2022-09-14 19:49:17 +0800

    Updated reserve

--- application/views/student/evrsers/scripts/new-reservation-scripts.php
+++ application/views/student/evrsers/scripts/new-reservation-scripts.php
@@ -35,12 +35,17 @@
 <!-- validation init -->
 <script src="<?= base_url() ?>public/js/pages/form-validation.init.js"></script>
 
+<!-- Sweet Alerts js -->
+<script src="<?= base_url() ?>public/libs/sweetalert2/sweetalert2.min.js"></script>
+
+<!-- Sweet alert init js-->
+<script src="<?= base_url() ?>public/js/pages/sweetalerts.init.js"></script>
+
 <!-- input spin init -->
 <script src="<?= base_url() ?>public/js/pages/form-input-spin.init.js"></script>
 
 <!--select2 cdn-->
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-
 <script src="<?= base_url() ?>public/js/pages/select2.init.js"></script>
 
 <!-- App js -->
