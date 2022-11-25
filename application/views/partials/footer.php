4612f9b1af7027957b784e3bacd199dd0e3fcebb
James Paul Tulod <paultulod@pm.me>
2022-11-25 18:18:00 +0800

    Update Footer color
    Co-authored-by: Micah Villaruz <micahmvillaruz@gmail.com>

--- application/views/partials/footer.php
+++ application/views/partials/footer.php
@@ -1,11 +1,11 @@
-<footer class="footer">
+<footer class="footer text-dark">
   <div class="container-fluid">
     <div class="row">
       <div class="col-sm-6">
         <script>
           document.write(new Date().getFullYear())
         </script>
-        © myPUPQC.
+        © myPUPQC. | <a href="<?= base_url() ?>terms/">Terms of Use</a> | <a href="<?= base_url() ?>privacy/">Privacy Statement</a>
       </div>
       <div class="col-sm-6">
         <div class="text-sm-end d-none d-sm-block">Design & Develop by BSIT 4-1 Group 3</div>
