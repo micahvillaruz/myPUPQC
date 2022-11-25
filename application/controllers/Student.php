e2197729d87192d5f1c97749939c4495269bf913
James Paul Tulod <paultulod@pm.me>
2022-11-25 18:22:22 +0800

    Add ODRTS Footer 👣

--- application/controllers/Student.php
+++ application/controllers/Student.php
@@ -67,7 +67,7 @@ class Student extends CI_Controller
     $this->load->view('partials/student/sidebar');
     $this->load->view('partials/page-title', ["page_title" => "ODRS", "title" => "New Request"]);
     $this->load->view('student/odrs/new-request');
-    $this->load->view('partials/footer');
+    $this->load->view('student/odrs/footer');
     $this->load->view('student/odrs/components/new-request-modal');
     $this->load->view('partials/foot-scripts');
     $this->load->view('student/odrs/scripts/new-request-scripts');
@@ -82,7 +82,7 @@ class Student extends CI_Controller
     $this->load->view('partials/student/sidebar');
     $this->load->view('partials/page-title', ["page_title" => "ODRS", "title" => "Requests"]);
     $this->load->view('student/odrs/requests');
-    $this->load->view('partials/footer');
+    $this->load->view('student/odrs/footer');
     $this->load->view('student/odrs/components/requests-modal');
     $this->load->view('partials/foot-scripts');
     $this->load->view('student/odrs/scripts/requests-scripts');
@@ -97,7 +97,7 @@ class Student extends CI_Controller
     $this->load->view('partials/student/sidebar');
     $this->load->view('partials/page-title', ["page_title" => "ODRS", "title" => "History"]);
     $this->load->view('student/odrs/history');
-    $this->load->view('partials/footer');
+    $this->load->view('student/odrs/footer');
     $this->load->view('student/odrs/components/history-modal');
     $this->load->view('partials/foot-scripts');
     $this->load->view('student/odrs/scripts/history-scripts');
---
+++ application/views/student/odrs/footer.php
@@ -0,0 +1,26 @@
+<footer class="footer text-dark">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-sm-3">
+        <script>
+          document.write(new Date().getFullYear())
+        </script>
+        © myPUPQC. | <a href="<?= base_url() ?>terms/">Terms of Use</a> | <a href="<?= base_url() ?>privacy/">Privacy Statement</a>
+
+      </div>
+      <div class="col-sm-9">
+        <div class="text-sm-end d-none d-sm-block">Contact us at: <span class="text-primary">8287-8204</span> or visit us at: <span class="text-primary">Ground Flr., Rothlener Building, Records Section, PUP Quezon City Branch, Don Fabian St., Commonwealth, Quezon City</span></div>
+      </div>
+    </div>
+  </div>
+</footer>
+</div>
+<!-- end main content-->
+</div>
+<!-- END layout-wrapper -->
+
+<!--start back-to-top-->
+<button onclick="topFunction()" class="btn btn-danger btn-icon" id="back-to-top">
+  <i class="ri-arrow-up-line"></i>
+</button>
+<!--end back-to-top-->
