086890362ca24a3d29d175dd6d68bb747c5373e3
Micah Villaruz <micahmvillaruz@gmail.com>
2022-11-26 00:30:32 +0800

    Connect PUP Staff document AJAX - Load documents table
    Co-authored-by: James Paul Tulod <paultulod@pm.me>

--- public/js/ajax/pup-staff/odrs/documents.ajax.js
+++ public/js/ajax/pup-staff/odrs/documents.ajax.js
@@ -1,5 +1,5 @@
 $(function () {
-	//loadDocumentsTable()
+	loadDocumentsTable()
 })
 
 // Load Documents Table
@@ -11,11 +11,61 @@ loadDocumentsTable = () => {
 	dt.DataTable({
 		bDestroy: true,
 		ajax: {
-			url: apiURL + 'odrs/student/view_documents',
+			url: apiURL + 'odrs/pup_staff/view_documents',
 			type: 'GET',
 			headers: AJAX_HEADERS,
 		},
-		columns: [],
+		columns: [
+			{
+				data: null,
+				render: (data) => {
+					return `<a href="#!">${data.document_name}</a><br><small>${
+						data.document_requirements == null ? '' : data.document_requirements
+					}</small>`
+				},
+			},
+			{
+				data: null,
+				render: (data) => {
+					if (data.document_type === 'Transcript of Records') {
+						return `<div class="d-block badge badge-soft-dark align-items-center">
+						<i class="me-2 bx bxs-institution fs-13"></i>
+						<span class="text-uppercase">CAV (CHED/DFA/WES/CES)</span>
+					</div>`
+					} else if (data.document_type === 'Certifications') {
+						return `<div class="d-block badge badge-soft-success align-items-center">
+						<i class="me-2 mdi mdi-certificate fs-13"></i>
+						<span class="text-uppercase">Certifications</span>
+					</div>`
+					} else if (data.document_type === 'Unclaimed') {
+						return `<div class="d-block badge badge-soft-danger align-items-center">
+						<i class="me-2 mdi mdi-package-variant fs-13"></i>
+						<span class="text-uppercase">Unclaimed</span>
+					</div>`
+					} else if (data.document_type === 'CAV') {
+						return `<div class="d-block badge badge-soft-primary align-items-center">
+						<i class="me-2 mdi mdi-format-annotation-plus fs-13"></i>
+						<span class="text-uppercase">Transcript of Records</span>
+					</div>`
+					} else {
+						return ''
+					}
+				},
+			},
+			{
+				data: null,
+				render: (data) => {
+					return `<div class="d-flex gap-1 justify-content-center">
+					<button type="button" class="btn btn-info btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#viewDocumentModal">
+						<i class="ri-eye-fill fs-5"></i>
+					</button>
+					<button type="button" class="btn btn-warning btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#updateDocumentModal">
+						<i class="ri-edit-2-fill fs-5"></i>
+					</button>
+				</div>`
+				},
+			},
+		],
 		order: [[0, 'asc']],
 	})
 }
