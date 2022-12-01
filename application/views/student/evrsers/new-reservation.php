a35a3d8cfc14cc80068a052c0ed81863918b3f21
Zsyrhill Soria <zsyrhillsoria@gmail.com>
2022-12-01 14:07:47 +0800

    implemented UI changes in evrsers reservation

--- application/views/student/evrsers/new-reservation.php
+++ application/views/student/evrsers/new-reservation.php
@@ -95,20 +95,61 @@
                                 <label for="timeTofloatingInput">Time To</label>
                             </div>
                         </div>
-                        <div class="col-lg-4">
+                        <div class="col-lg-6">
                             <div class="form-control">
                                 <label for="attachfloatingInput">Relevant Attachments</label>
-                                <input type="file" class="form-control" id="attachfloatingInput" placeholder="Insert attachments" required>
+                                <!-- Default Modals -->
+                                <button type="button" class="btn rounded-pill btn-info btn-sm" data-bs-toggle="modal" data-bs-target="#myModal"><i class="bx bx-question-mark"></i></button>
+                                <div id="myModal" class="modal fade" tabindex="-1" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="myModalLabel">Event Request Attachments</h5>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"> </button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <h5 class="fs-15">
+                                                    1. Event Request
+                                                </h5>
+                                                <p class="text-muted">This should contain the information regarding the event and completed signatories from the following: <strong>Org President, SSC President, Adviser, Property Custodian, OSAS Head, Acad Head, & the Director.</strong></p>
+                                                <h5 class="fs-15">
+                                                    2. Concept Paper
+                                                </h5>
+                                                <p class="text-muted">The specific details regarding the event should be found here. This includes the venue, platform, number of participants included in the said event.</p>
+                                                <h5 class="fs-15">
+                                                    3. Other Files
+                                                </h5>
+                                                <p class="text-muted">Anything relevant to the creation of the event request should then be included here. <i>(Note that this could help your application to be approved faster.)</i></p>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
+                                            </div>
+
+                                        </div><!-- /.modal-content -->
+                                    </div><!-- /.modal-dialog -->
+                                </div><!-- /.modal -->
+                                <div class="input-group mt-2">
+                                    <input type="file" class="form-control" id="inputGroupFile02">
+                                    <label class="input-group-text" for="inputGroupFile02">Event Request</label>
+                                </div>
+                                <div class="input-group mt-2">
+                                    <input type="file" class="form-control" id="inputGroupFile02">
+                                    <label class="input-group-text" for="inputGroupFile02">Concept Paper</label>
+                                </div>
+                                <div class="input-group mt-2">
+                                    <input type="file" class="form-control" id="inputGroupFile02">
+                                    <label class="input-group-text" for="inputGroupFile02">Others</label>
+                                </div>
                             </div>
                         </div>
-                        <div class="col-lg-8">
+                        <div class="col-lg-6">
                             <div class="form-floating">
                                 <input type="text" class="form-control d-flex align-items-stretch" id="remarksfloatingInput" placeholder="Please enter reservation details" disabled value="Please wait for your reservation to be approved.">
                                 <label for="remarksfloatingInput">Admin Remarks</label>
                             </div>
                         </div>
                         <div class="col-lg-12">
-                                <button type="submit" class="btn btn-primary btn-md waves-effect waves-light btn-md float-end fs-6" onclick="addNewReservation()">Submit</button>
+                            <button type="submit" class="btn btn-primary btn-md waves-effect waves-light btn-md float-end fs-6" onclick="addNewReservation()">Submit</button>
                         </div>
                     </div>
                 </form>
