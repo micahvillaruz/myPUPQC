6b6f9f6e9c32d167d802989ad192931e80f01119
Zsyrhill Soria <zsyrhillsoria@gmail.com>
2022-12-06 23:10:49 +0800

    Edit Admin reservation management

--- application/views/admin/evrsers/manage-reservations.php
+++ application/views/admin/evrsers/manage-reservations.php
@@ -8,10 +8,10 @@
                 </div>
             </div>
             <div class="card-body">
-                <table id="example" class="table table-bordered dt-responsive nowrap table-striped align-middle text-center" style="width: 100%">
+                <table id="reservations-datatable" class="table table-bordered table-responsive dt-responsive nowrap table-striped align-middle text-center" style="width: 100%">
                     <thead>
                         <tr>
-                            <th>Reservation ID</th>
+                            <th>Reservation Control #</th>
                             <th>Event Title</th>
                             <th>Venue</th>
                             <th>Event Details</th>
@@ -22,145 +22,6 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>RES-00002</td>
-                            <td>Ligma Webinar</td>
-                            <td>Court</td>
-                            <td>Sponsored by Jhobart M. Xavier-Oxford III.</td>
-                            <td>09-01-2022</td>
-                            <td>1:00 - 2:00PM</td>
-                            <td><span class="badge rounded-pill bg-success">Approved</span></td>
-                            <td>
-                                <div class="dropdown d-inline-block">
-                                    <button type="button" class="btn btn-info btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#viewMedicalModal"><i class="ri-eye-fill"></i></button>
-                                    <button type="button" class="btn btn-warning btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#editMedicalModal"><i class="ri-edit-2-fill"></i></button>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>RES-00003</td>
-                            <td>Mental Health Webinar</td>
-                            <td>Court</td>
-                            <td>Sponsored by Jhobart M. Xavier-Oxford III.</td>
-                            <td>09-02-2022</td>
-                            <td>2:00 - 3:00PM</td>
-                            <td><span class="badge rounded-pill bg-danger">Denied</span></td>
-                            <td>
-                                <div class="dropdown d-inline-block">
-                                    <button type="button" class="btn btn-info btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#viewMedicalModal"><i class="ri-eye-fill"></i></button>
-                                    <button type="button" class="btn btn-warning btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#editMedicalModal"><i class="ri-edit-2-fill"></i></button>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>RES-00004</td>
-                            <td>Joe M. Ma Webinar</td>
-                            <td>Chapel</td>
-                            <td>Sponsored by Jhobart M. Xavier-Oxford III.</td>
-                            <td>09-03-2022</td>
-                            <td>3:00 - 4:00PM</td>
-                            <td><span class="badge rounded-pill bg-secondary">Pending</span></td>
-                            <td>
-                                <div class="dropdown d-inline-block">
-                                    <button type="button" class="btn btn-primary btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#viewMedicalModal"><i class="ri-checkbox-multiple-fill"></i></button>
-                                    <button type="button" class="btn btn-info btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#viewMedicalModal"><i class="ri-eye-fill"></i></button>
-                                    <button type="button" class="btn btn-warning btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#editMedicalModal"><i class="ri-edit-2-fill"></i></button>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>RES-00005</td>
-                            <td>CommITS General Assembly</td>
-                            <td>Chapel</td>
-                            <td>Sponsored by Jhobart M. Xavier-Oxford III.</td>
-                            <td>09-04-2022</td>
-                            <td>4:00 - 5:00PM</td>
-                            <td><span class="badge rounded-pill bg-success">Approved</span></td>
-                            <td>
-                                <div class="dropdown d-inline-block">
-                                    <button type="button" class="btn btn-info btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#viewMedicalModal"><i class="ri-eye-fill"></i></button>
-                                    <button type="button" class="btn btn-warning btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#editMedicalModal"><i class="ri-edit-2-fill"></i></button>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>RES-00006</td>
-                            <td>Blockchain Project Expo</td>
-                            <td>Court</td>
-                            <td>Sponsored by Jhobart M. Xavier-Oxford III.</td>
-                            <td>09-05-2022</td>
-                            <td>10:00 AM - 11:00 AM</td>
-                            <td><span class="badge rounded-pill bg-danger">Denied</span></td>
-                            <td>
-                                <div class="dropdown d-inline-block">
-                                    <button type="button" class="btn btn-info btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#viewMedicalModal"><i class="ri-eye-fill"></i></button>
-                                    <button type="button" class="btn btn-warning btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#editMedicalModal"><i class="ri-edit-2-fill"></i></button>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>RES-00007</td>
-                            <td>CommITS General Assembly</td>
-                            <td>Chapel</td>
-                            <td>Sponsored by Willie B. Revillame.</td>
-                            <td>09-15-2022</td>
-                            <td>4:00 - 5:00PM</td>
-                            <td><span class="badge rounded-pill bg-secondary">Pending</span></td>
-                            <td>
-                                <div class="dropdown d-inline-block">
-                                    <button type="button" class="btn btn-primary btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#viewMedicalModal"><i class="ri-checkbox-multiple-fill"></i></button>
-                                    <button type="button" class="btn btn-info btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#viewMedicalModal"><i class="ri-eye-fill"></i></button>
-                                    <button type="button" class="btn btn-warning btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#editMedicalModal"><i class="ri-edit-2-fill"></i></button>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>RES-00008</td>
-                            <td>Blockchain Project Expo</td>
-                            <td>Court</td>
-                            <td>Sponsored by Jhobart M. Xavier-Oxford III.</td>
-                            <td>09-05-2022</td>
-                            <td>10:00 AM - 11:00 AM</td>
-                            <td><span class="badge rounded-pill bg-secondary">Pending</span></td>
-                            <td>
-                                <div class="dropdown d-inline-block">
-                                    <button type="button" class="btn btn-primary btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#viewMedicalModal"><i class="ri-checkbox-multiple-fill"></i></button>
-                                    <button type="button" class="btn btn-info btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#viewMedicalModal"><i class="ri-eye-fill"></i></button>
-                                    <button type="button" class="btn btn-warning btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#editMedicalModal"><i class="ri-edit-2-fill"></i></button>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>RES-00009</td>
-                            <td>CommITS General Assembly</td>
-                            <td>Chapel</td>
-                            <td>Sponsored by Jhobart M. Xavier-Oxford III.</td>
-                            <td>09-04-2022</td>
-                            <td>4:00 - 5:00PM</td>
-                            <td><span class="badge rounded-pill bg-secondary">Pending</span></td>
-                            <td>
-                                <div class="dropdown d-inline-block">
-                                    <button type="button" class="btn btn-primary btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#viewMedicalModal"><i class="ri-checkbox-multiple-fill"></i></button>
-                                    <button type="button" class="btn btn-info btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#viewMedicalModal"><i class="ri-eye-fill"></i></button>
-                                    <button type="button" class="btn btn-warning btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#editMedicalModal"><i class="ri-edit-2-fill"></i></button>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>RES-00010</td>
-                            <td>Blockchain Project Expo</td>
-                            <td>Court</td>
-                            <td>Sponsored by Jhobart M. Xavier-Oxford III.</td>
-                            <td>09-05-2022</td>
-                            <td>10:00 AM - 11:00 AM</td>
-                            <td><span class="badge rounded-pill bg-danger">Denied</span></td>
-                            <td>
-                                <div class="dropdown d-inline-block">
-                                    <button type="button" class="btn btn-info btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#viewMedicalModal"><i class="ri-eye-fill"></i></button>
-                                    <button type="button" class="btn btn-warning btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#editMedicalModal"><i class="ri-edit-2-fill"></i></button>
-                                </div>
-                            </td>
-                        </tr>
                     </tbody>
                 </table>
             </div>
