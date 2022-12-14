b36a1984d6fe7b326d69e45e59a8c8cbf3abb039
Zsyrhill Soria <zsyrhillsoria@gmail.com>
2022-12-14 09:34:03 +0800

    Edited view reservation modal size and alignment

--- application/views/student/evrsers/components/reservation-modal.php
+++ application/views/student/evrsers/components/reservation-modal.php
@@ -54,7 +54,7 @@
 
 <!-- View Reservation modal -->
 <div class="modal fade" id="viewReservationModal" tabindex="-1" role="dialog" aria-labelledby="reservation-details-label" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-scrollable">
+  <div class="modal-dialog modal-lg modal-dialog-centered">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="reservation-details-label">
@@ -63,127 +63,129 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        <div data-simplebar class="mx-n3">
-          <ul class="list list-group list-group-flush mb-0">
-            <!-- end list item -->
-            <li class="list-group-item">
-              <div class="d-flex align-items-center">
-                <div class="col-4">
-                  <h5 class="fs-13 mb-1 fw-medium"><a href="#" class="link text-dark">Reservation Control #:</a></h5>
-                </div>
-                <div class="col-8">
-                  <div class="d-flex fs-13 fw-medium" id="reserve_number"></div>
-                </div>
-              </div>
-            </li>
-            <!-- end list item -->
-            <li class="list-group-item">
-              <div class="d-flex align-items-center">
-                <div class="col-4">
-                  <h5 class="fs-13 mb-1">Organizaton</h5>
-                </div>
-                <div class="col-8">
-                  <div class="d-flex fs-13 fw-medium" id="organization"></div>
-                </div>
-              </div>
-            </li>
-            <!-- end list item -->
-            <li class="list-group-item">
-              <div class="d-flex align-items-center">
-                <div class="col-4">
-                  <h5 class="fs-13 mb-1 fw-medium"><a href="#" class="link text-dark">Event Title</a></h5>
-                </div>
-                <div class="col-8">
-                  <div class="d-flex fs-13 fw-medium" id="event_title"></div>
-                </div>
-              </div>
-            </li>
-            <!-- end list item -->
-            <li class="list-group-item">
-              <div class="d-flex align-items-center">
-                <div class="col-4">
-                  <h5 class="fs-13 mb-1">Venue</h5>
-                </div>
-                <div class="col-8">
-                  <div class="d-flex fs-13 fw-medium" id="facility_name"></div>
-                </div>
-              </div>
-            </li>
-            <!-- end list item -->
-            <li class="list-group-item">
-              <div class="d-flex align-items-center">
-                <div class="col-4">
-                  <h5 class="fs-13 mb-1">Event Details</h5>
-                </div>
-                <div class="col-8">
-                  <div class="d-flex fs-13 fw-medium" id="event_details"></div>
-                </div>
-              </div>
-            </li>
-            <!-- end list item -->
-            <li class="list-group-item">
-              <div class="d-flex align-items-center">
-                <div class="col-4">
-                  <h5 class="fs-13 mb-1">Date</h5>
-                </div>
-                <div class="col-8">
-                  <div class="d-flex fs-13 fw-medium" id="reserve_date"></div>
-                </div>
-              </div>
-            </li>
-            <!-- end list item -->
-            <li class="list-group-item">
-              <div class="d-flex align-items-center">
-                <div class="col-4">
-                  <h5 class="fs-13 mb-1">Time</h5>
-                </div>
-                <div class="col-8">
-                  <div class="fw-medium">
-                    <span id="time"></span>
+        <div>
+          <div data-simplebar class="mx-n3">
+            <ul class="list list-group list-group-flush mb-0">
+              <!-- end list item -->
+              <li class="list-group-item">
+                <div class="d-flex align-items-center">
+                  <div class="col-4">
+                    <h5 class="fs-13 mb-1 fw-medium"><a href="#" class="link text-dark">Reservation Control #:</a></h5>
+                  </div>
+                  <div class="col-8">
+                    <div class="d-flex fs-13 fw-medium" id="reserve_number"></div>
                   </div>
                 </div>
-              </div>
-            </li>
-            <!-- end list item -->
-            <li class="list-group-item">
-              <div class="d-flex align-items-center">
-                <div class="col-4">
-                  <h5 class="fs-13 mb-1">Attachments</h5>
-                </div>
-                <div class="col-8">
-                  <div class="fw-medium">
-                    <span id="attachment1"></span>
-                    <span id="attachment2"></span>
-                    <span id="attachment3"></span>
+              </li>
+              <!-- end list item -->
+              <li class="list-group-item">
+                <div class="d-flex align-items-center">
+                  <div class="col-4">
+                    <h5 class="fs-13 mb-1">Organizaton</h5>
+                  </div>
+                  <div class="col-8">
+                    <div class="d-flex fs-13 fw-medium" id="organization"></div>
                   </div>
                 </div>
-              </div>
-            </li>
-            <!-- end list item -->
-            <li class="list-group-item">
-              <div class="d-flex align-items-center">
-                <div class="col-4">
-                  <h5 class="fs-13 mb-1">Remarks</h5>
-                </div>
-                <div class="col-8">
-                  <div class="fw-medium">
-                    <span id="remarks"></span>
+              </li>
+              <!-- end list item -->
+              <li class="list-group-item">
+                <div class="d-flex align-items-center">
+                  <div class="col-4">
+                    <h5 class="fs-13 mb-1 fw-medium"><a href="#" class="link text-dark">Event Title</a></h5>
+                  </div>
+                  <div class="col-8">
+                    <div class="d-flex fs-13 fw-medium" id="event_title"></div>
                   </div>
                 </div>
-              </div>
-            </li>
-            <!-- end list item -->
-            <li class="list-group-item">
-              <div class="d-flex align-items-center">
-                <div class="col-4">
-                  <h5 class="fs-13 mb-1 fw-medium"><a href="#" class="link text-dark">Status</a></h5>
+              </li>
+              <!-- end list item -->
+              <li class="list-group-item">
+                <div class="d-flex align-items-center">
+                  <div class="col-4">
+                    <h5 class="fs-13 mb-1">Venue</h5>
+                  </div>
+                  <div class="col-8">
+                    <div class="d-flex fs-13 fw-medium" id="facility_name"></div>
+                  </div>
                 </div>
-                <div class="col-8" id="reservation_status"></div>
-              </div>
-            </li>
-            <!-- end list item -->
-          </ul>
-          <!-- end ul list -->
+              </li>
+              <!-- end list item -->
+              <li class="list-group-item">
+                <div class="d-flex align-items-center">
+                  <div class="col-4">
+                    <h5 class="fs-13 mb-1">Event Details</h5>
+                  </div>
+                  <div class="col-8">
+                    <div class="d-flex fs-13 fw-medium" id="event_details"></div>
+                  </div>
+                </div>
+              </li>
+              <!-- end list item -->
+              <li class="list-group-item">
+                <div class="d-flex align-items-center">
+                  <div class="col-4">
+                    <h5 class="fs-13 mb-1">Date</h5>
+                  </div>
+                  <div class="col-8">
+                    <div class="d-flex fs-13 fw-medium" id="reserve_date"></div>
+                  </div>
+                </div>
+              </li>
+              <!-- end list item -->
+              <li class="list-group-item">
+                <div class="d-flex align-items-center">
+                  <div class="col-4">
+                    <h5 class="fs-13 mb-1">Time</h5>
+                  </div>
+                  <div class="col-8">
+                    <div class="fw-medium">
+                      <span id="time"></span>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <!-- end list item -->
+              <li class="list-group-item">
+                <div class="d-flex align-items-center">
+                  <div class="col-4">
+                    <h5 class="fs-13 mb-1">Attachments</h5>
+                  </div>
+                  <div class="col-8">
+                    <div class="fw-medium fs-6">
+                      <span id="attachment1"></span>
+                      <span id="attachment2"></span>
+                      <span id="attachment3"></span>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <!-- end list item -->
+              <li class="list-group-item">
+                <div class="d-flex align-items-center">
+                  <div class="col-4">
+                    <h5 class="fs-13 mb-1">Remarks</h5>
+                  </div>
+                  <div class="col-8">
+                    <div class="fw-medium">
+                      <span id="remarks"></span>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <!-- end list item -->
+              <li class="list-group-item">
+                <div class="d-flex align-items-center">
+                  <div class="col-4">
+                    <h5 class="fs-13 mb-1 fw-medium"><a href="#" class="link text-dark">Status</a></h5>
+                  </div>
+                  <div class="col-8" id="reservation_status"></div>
+                </div>
+              </li>
+              <!-- end list item -->
+            </ul>
+            <!-- end ul list -->
+          </div>
         </div>
       </div>
       <div class="modal-footer">
@@ -300,8 +302,8 @@
             </div>
             <div class="col-md col-lg-8">
               <div class="mb-2">
-              <label for="edit-attachments">Edit Attachments</label>
-              <button type="button" class="btn rounded-pill btn-info btn-sm" data-bs-toggle="modal" data-bs-target="#attachment-info"><i class="bx bx-question-mark"></i></button>
+                <label for="edit-attachments">Edit Attachments</label>
+                <button type="button" class="btn rounded-pill btn-info btn-sm" data-bs-toggle="modal" data-bs-target="#attachment-info"><i class="bx bx-question-mark"></i></button>
               </div>
               <div class="input-group mb-2" id="edit-attachments">
                 <input type="file" class="form-control" id="edit-inputGroupFile01">
