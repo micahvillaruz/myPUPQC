c018dd4987569f54d4279b837abdcd016ae6991d
Zsyrhill Soria <zsyrhillsoria@gmail.com>
2022-12-14 08:47:45 +0800

    Fixed view all reservations ajax

--- application/views/student/evrsers/view-reservations.php
+++ application/views/student/evrsers/view-reservations.php
@@ -6,9 +6,6 @@
         <div class="card">
             <div class="card-header d-flex align-items-center">
                 <h5 class="card-title mb-0 flex-grow-1">Venue Reservations</h5>
-                <div>
-                    <!-- <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addStaffModal">Add Faculty/Staff</button> -->
-                </div>
             </div>
             <div class="card-body">
                 <table id="reservations-datatable" class="table table-bordered table-responsive dt-responsive nowrap table-striped align-middle text-center" style="width: 100%">
