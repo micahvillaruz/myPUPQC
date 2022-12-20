6b86530d8a3000c921534a7acf2075b37e7d732c
Micah Villaruz <micahmvillaruz@gmail.com>
2022-12-20 22:41:57 +0800

    Connect Documents to API

--- application/views/admin/odrs/components/documents-modal.php
+++ application/views/admin/odrs/components/documents-modal.php
@@ -3,28 +3,19 @@
   <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content border-0 overflow-hidden" style="background: url('<?= base_url() ?>public/images/chat-bg-pattern.png') rgb(255, 255, 255);">
       <div class="modal-body login-modal p-5">
-        <h5 class="text-white text-center fs-20" id="view_document_name">
-          CAV-CHED (Commission on Higher Education)
-        </h5>
+        <h5 class="text-white text-center fs-20" id="document_name"></h5>
       </div>
       <div class="modal-body px-5 pt-5">
         <span class="badge badge-soft-dark fs-15 mb-3"> <i class="ri-folder-4-line me-2"></i> Type</span>
-        <mark class="mb-4 mx-3 fw-bold d-block text-center" id="view_document_type">
-          CAV (CHED/DFA/WES/CES)
-        </mark>
+        <mark class="mb-4 mx-3 fw-bold d-block text-center" id="document_type"></mark>
         <span class="badge badge-soft-dark fs-15 mb-3"> <i class="ri-file-list-3-line me-2"></i> Details</span>
-        <p class="mb-4 mx-3" id="view_document_details">
-          The Certification, Authentication, and Verification (CAV) refers to the official and formal processes and acts of checking, reviewing and certifying to the genuineness and veracity of available academic records of a learner. The documents will be prepared only by the Registrar in a sealed envelope, and the client will forward it to CHED.
-        </p>
+        <p class="mb-4 mx-3" id="document_details"></p>
         <span class="badge badge-soft-dark fs-15 mb-3"> <i class="ri-suitcase-line me-2"></i> Requirements</span>
-        <ul id="view_document_requirements" class="mx-3">
-          <li>Photocopy of TOR (must capture the whole page including the signatories)</li>
-          <li>Photocopy of Diploma</li>
-          <li>Letter format for CHED</li>
+        <ul id="document_requirements" class="mx-3">
         </ul>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-danger btn-animation waves-effect waves-light fs-13" data-text="Close" data-bs-dismiss="modal"><span>Close</span></button>
+        <button type="button" class="btn btn-danger waves-effect waves-light fs-13" data-text="Close" data-bs-dismiss="modal"><span>Close</span></button>
       </div>
     </div>
     <!-- /.modal-content -->
--- public/js/ajax/admin/odrs/documents.ajax.js
+++ public/js/ajax/admin/odrs/documents.ajax.js
@@ -1,30 +1,123 @@
-// Delete Document
-deleteDocument = () => {
-	$.ajaxSetup({
-		headers: {
-			Accept: 'application/json',
-			Authorization: 'Bearer ' + TOKEN,
-			ContentType: 'application/x-www-form-urlencoded',
+$(function () {
+	loadDocumentsTable()
+})
+
+// Load Documents Table
+loadDocumentsTable = () => {
+	const dt = $('#documents-datatable')
+
+	if (!dt.length) return
+
+	dt.DataTable({
+		bDestroy: true,
+		ajax: {
+			url: apiURL + 'odrs/super_admin/view_documents',
+			type: 'GET',
+			headers: AJAX_HEADERS,
+		},
+		columns: [
+			{
+				data: null,
+				width: '70%',
+				render: (data) => {
+					return `<a href="#!" class="fw-medium">${data.document_name}</a><br><small>${
+						data.document_details == null ? '' : data.document_details
+					}</small>`
+				},
+			},
+			{
+				data: null,
+				width: '20%',
+				render: (data) => {
+					if (data.document_type === 'CAV') {
+						return `<div class="d-block badge badge-soft-dark align-items-center">
+						<i class="me-2 bx bxs-institution fs-13"></i>
+						<span class="text-uppercase">${data.document_type} (CHED/DFA/WES/CES)</span>
+					</div>`
+					} else if (data.document_type === 'Certifications') {
+						return `<div class="d-block badge badge-soft-success align-items-center">
+						<i class="me-2 mdi mdi-certificate fs-13"></i>
+						<span class="text-uppercase">${data.document_type}</span>
+					</div>`
+					} else if (data.document_type === 'Unclaimed') {
+						return `<div class="d-block badge badge-soft-danger align-items-center">
+						<i class="me-2 mdi mdi-package-variant fs-13"></i>
+						<span class="text-uppercase">${data.document_type}</span>
+					</div>`
+					} else if (data.document_type === 'Transcript of Records') {
+						return `<div class="d-block badge badge-soft-primary align-items-center">
+						<i class="me-2 mdi mdi-format-annotation-plus fs-13"></i>
+						<span class="text-uppercase">${data.document_type}</span>
+					</div>`
+					}
+				},
+			},
+			{
+				data: null,
+				width: '10%',
+				render: (data) => {
+					return `<div class="d-flex gap-2 justify-content-center">
+					<button type="button" class="btn btn-info btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#viewDocumentModal" onclick="loadDocumentInfo('${data.document_id}')">
+						<i class="ri-eye-fill fs-5"></i>
+					</button>
+					<button type="button" class="btn btn-danger btn-icon waves-effect waves-light" onclick="deleteDocument('${data.document_id}')"><i class="bx bx-trash fs-4"></i></button>
+				</div>`
+				},
+			},
+		],
+		order: [[0, 'asc']],
+	})
+}
+
+// Get Document Info
+loadDocumentInfo = (document_id) => {
+	$.ajax({
+		type: 'GET',
+		url: `${apiURL}odrs/super_admin/view_document/${document_id}`,
+		headers: AJAX_HEADERS,
+		success: (result) => {
+			const data = result.data
+
+			$('#document_name').html(data.document_name)
+
+			$('#document_type').html(
+				data.document_type === 'CAV' ? 'CAV (CHED/DFA/WES/CES)' : data.document_type,
+			)
+			$('#document_details').html(data.document_details)
+
+			$('#document_requirements').empty()
+			if (data.document_requirements.length === 0) {
+				$('#document_requirements').html('<i>No requirements are needed.</i>')
+			} else {
+				data.document_requirements.forEach((item) => {
+					$('#document_requirements').append(`
+						<li>${item.doc_req_name}</li>
+					`)
+				})
+			}
 		},
 	})
+}
 
+// Delete Document
+deleteDocument = (document_id) => {
 	Swal.fire({
 		html: `
-                <div class="mt-3">
-                    <lord-icon src="https://cdn.lordicon.com/exkbusmy.json" trigger="loop" colors="outline:#121331,primary:#646e78,secondary:#f06548,tertiary:#ebe6ef" style="width:100px;height:100px"></lord-icon>
-                    <div class="mt-4 fs-13">
-                        <h4 class="mb-3 fw-semibold">Delete this Document?</h4>
-                        <div class="alert alert-warning alert-border-left alert-dismissible fade show" style="padding-right: 16px;" role="alert">
-                            <div class="mb-2 d-flex align-items-center justify-content-center">
-                                <i class="ri-alert-line me-2 align-middle fs-6"></i> <strong>WARNING</strong>
-                            </div>
-                            <span>
-                                Deleting this document means that the document will not be visible to the Student and the PUP staff. <br><span class="fw-medium">You also cannot restore a deleted document</span>.
-                            </span>
-                        </div>
-                    </div>
-                </div>
-            `,
+      <div class="mt-3">
+          <lord-icon src="https://cdn.lordicon.com/exkbusmy.json" trigger="loop" colors="outline:#121331,primary:#646e78,secondary:#f06548,tertiary:#ebe6ef" style="width:100px;height:100px"></lord-icon>
+          <div class="mt-4 fs-13">
+              <h4 class="mb-3 fw-semibold">Delete this Document?</h4>
+              <div class="alert alert-warning alert-border-left alert-dismissible fade show" style="padding-right: 16px;" role="alert">
+                  <div class="mb-2 d-flex align-items-center justify-content-center">
+                      <i class="ri-alert-line me-2 align-middle fs-6"></i> <strong>WARNING</strong>
+                  </div>
+                  <span>
+                      Deleting this document means that the document will not be visible to the Student and the PUP staff. <br><span class="fw-medium">You also cannot restore a deleted document</span>.
+                  </span>
+              </div>
+          </div>
+      </div>
+    `,
 		showCancelButton: true,
 		confirmButtonClass: 'btn btn-danger fw-medium waves-effect waves-light me-2',
 		confirmButtonText: 'Yes, Delete It!',
@@ -34,6 +127,10 @@ deleteDocument = () => {
 	}).then(function (result) {
 		if (result.value) {
 			$.ajax({
+				url: `${apiURL}odrs/super_admin/delete_document/${document_id}`,
+				type: 'DELETE',
+				dataType: 'json',
+				headers: AJAX_HEADERS,
 				success: (result) => {
 					if (result) {
 						Swal.fire({
@@ -51,6 +148,9 @@ deleteDocument = () => {
 							cancelButtonText: 'Dismiss',
 							buttonsStyling: !1,
 							showCloseButton: !0,
+						}).then(function () {
+							// Reload Documents Datatable
+							loadDocumentsTable()
 						})
 					}
 				},
