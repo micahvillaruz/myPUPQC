a7c53cfc9da8b3a3f8983eea32ffe4e69f7953d3
Zsyrhill Soria <zsyrhillsoria@gmail.com>
2022-12-21 14:57:45 +0800

    Managed organizer-management scripts

--- application/views/pup-staff/evrsers/scripts/organizer-management-scripts.php
+++ application/views/pup-staff/evrsers/scripts/organizer-management-scripts.php
@@ -1,15 +1,4 @@
-<!-- filepond js -->
-<script src="<?= base_url() ?>public/libs/filepond/filepond.min.js"></script>
-<script src="<?= base_url() ?>public/libs/filepond-plugin-image-preview/filepond-plugin-image-preview.min.js"></script>
-<script src="<?= base_url() ?>public/libs/filepond-plugin-file-validate-size/filepond-plugin-file-validate-size.min.js"></script>
-<script src="<?= base_url() ?>public/libs/filepond-plugin-image-exif-orientation/filepond-plugin-image-exif-orientation.min.js"></script>
-<script src="<?= base_url() ?>public/libs/filepond-plugin-file-encode/filepond-plugin-file-encode.min.js"></script>
-
-<!--nft create init js-->
-<script src="<?= base_url() ?>public/js/pages/apps-nft-create.init.js"></script>
-
-<!-- prismjs plugin -->
-<script src="<?= base_url() ?>public/libs/prismjs/prism.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
 <!--datatable js-->
 <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
@@ -24,32 +13,12 @@
 
 <script src="<?= base_url() ?>public/js/pages/datatables.init.js"></script>
 
-<!-- multi.js -->
-<script src="<?= base_url() ?>public/libs/multi.js/multi.min.js"></script>
-<!-- autocomplete js -->
-<script src="<?= base_url() ?>public/libs/@tarekraafat/autocomplete.js/autoComplete.min.js"></script>
-
-<!-- init js -->
-<script src="<?= base_url() ?>public/js/pages/form-advanced.init.js"></script>
-
-<!-- validation init -->
-<script src="<?= base_url() ?>public/js/pages/form-validation.init.js"></script>
-
-<!-- Sweet Alerts js -->
-<script src="<?= base_url() ?>public/libs/sweetalert2/sweetalert2.min.js"></script>
-
-<!-- Sweet alert init js-->
-<script src="<?= base_url() ?>public/js/pages/sweetalerts.init.js"></script>
-
-<!-- input spin init -->
-<script src="<?= base_url() ?>public/js/pages/form-input-spin.init.js"></script>
-
-<!--select2 cdn-->
-<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-<script src="<?= base_url() ?>public/js/pages/select2.init.js"></script>
-
 <!-- App js -->
 <script src="<?= base_url() ?>public/js/app.js"></script>
+
+<!-- PUP STAFF ajax -->
+<script src="<?= base_url() ?>public/js/ajax/pup-staff/evrsers/organizer.ajax.js"></script>
+
 </body>
 
 </html>
