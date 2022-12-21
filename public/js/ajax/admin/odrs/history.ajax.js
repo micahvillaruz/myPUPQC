df2ddaec9694977d8dae53fe03c318cc05f0df5e
Micah Villaruz <micahmvillaruz@gmail.com>
2022-12-21 21:26:45 +0800

    Connect history datatable to API

--- public/js/ajax/admin/odrs/history.ajax.js
+++ public/js/ajax/admin/odrs/history.ajax.js
@@ -1,3 +1,456 @@
+$(function () {
+	loadReleasedRequests()
+	loadCancelledRequests()
+	loadDeletedRequests()
+
+	$('a[data-bs-toggle="tab"]').on('shown.bs.tab', function (e) {
+		$('#released-requests').DataTable().columns.adjust().responsive.recalc()
+		$('#cancelled-requests').DataTable().columns.adjust().responsive.recalc()
+		$('#deleted-requests').DataTable().columns.adjust().responsive.recalc()
+	})
+})
+
+// Load Released Requests Table
+loadReleasedRequests = () => {
+	const dt = $('#released-requests')
+
+	if (dt.length) {
+		dt.DataTable({
+			bDestroy: true,
+			scrollX: true,
+			ajax: {
+				url: `${apiURL}odrs/super_admin/requests_history/Released`,
+				type: 'GET',
+				headers: AJAX_HEADERS,
+			},
+			columns: [
+				// Control Number
+				{
+					data: null,
+					render: (data) => {
+						return `<span class="text-primary fw-medium">${data.control_no}</span>`
+					},
+				},
+
+				// Student
+				{
+					data: null,
+					render: (data) => {
+						const gender = data.user_assigned_to_request.user_profiles[0].gender
+						const fullName = data.user_assigned_to_request.user_profiles[0].full_name
+
+						if (gender == 'Male') {
+							return `
+							<div class="d-flex align-items-center fw-medium">
+								<img src="${baseURL}public/images/profile/flat-faces-icons-circle-man-6.png" class="avatar-xs rounded-circle me-2">
+								<div>
+									<span class="d-block fw-medium">${fullName}</span>
+									<i class="mdi mdi-gender-male text-info"></i>
+									<small>${gender}</small>
+								</div>
+							</div>
+						`
+						} else {
+							return `
+							<div class="d-flex align-items-center fw-medium">
+								<img src="${baseURL}public/images/profile/flat-faces-icons-circle-woman-1.png" class="avatar-xs rounded-circle me-2">
+								<div>
+									<span class="d-block fw-medium">${fullName}</span>
+									<i class="mdi mdi-gender-female text-danger"></i>
+									<small>${gender}</small>
+								</div>
+							</div>
+						`
+						}
+					},
+				},
+
+				// Date Filed
+				{
+					data: null,
+					render: (data) => {
+						const date = moment(data.pending_for_clearance).format('DD, MMM. YYYY')
+						const time = moment(data.pending_for_clearance).format('hh:mm A')
+
+						return `
+							<div class="d-flex align-items-center">
+								<i class="ri-calendar-todo-fill text-primary"></i>
+								<span class="ms-2">${date}<small class="text-muted ms-1">${time}</small></span>
+							</div>
+						`
+					},
+				},
+
+				// Payment Status
+				{
+					data: null,
+					render: (data) => {
+						return `
+							<div class="d-flex badge badge-soft-success align-items-center justify-content-center">
+								<i class="me-2 mdi mdi-cash-check fs-15"></i>
+								<span class="text-uppercase">${data.payment_status}</span>
+							</div>
+							<span class="mt-1 d-block badge badge-soft-dark badge-border">OR No. ${data.or_no} </span>
+						`
+					},
+				},
+
+				// Details
+				{
+					data: null,
+					width: '30%',
+					render: (data) => {
+						const course = data.user_assigned_to_request.education_profile.course_when_admitted
+						const purpose = data.purpose_of_request
+						return `
+							<table>
+								<tbody>
+									<tr>
+										<td>
+											<span class="fw-medium badge bg-primary">Course: </span>
+										</td>
+										<td><span class="text-uppercase">${course}</span></td>
+									</tr>
+									<tr>
+										<td>
+											<span class="fw-medium badge bg-primary me-3">Purpose: </span>
+										</td>
+										<td>${purpose}</td>
+									</tr>
+								</tbody>
+							</table>
+						`
+					},
+				},
+
+				// Date Released
+				{
+					data: null,
+					render: (data) => {
+						const date = moment(data.released).format('DD, MMM. YYYY')
+						const time = moment(data.released).format('hh:mm A')
+
+						return `
+							<div class="d-flex align-items-center">
+								<i class="ri-calendar-todo-fill text-primary"></i>
+								<span class="ms-2">${date}<small class="text-muted ms-1">${time}</small></span>
+							</div>
+						`
+					},
+				},
+
+				// Action
+				{
+					data: null,
+					width: '5%',
+					class: 'text-center',
+					render: (data) => {
+						return `
+							<div class="d-flex gap-2 justify-content-center">
+								<button type="button" class="btn btn-info btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#viewRequestDetails"><i class="ri-eye-fill"></i></button>
+								<button type="button" class="btn btn-danger btn-icon waves-effect waves-light" onclick="deleteRequest()"><i class="bx bx-trash fs-4"></i></button>
+							</div>
+						`
+					},
+				},
+			],
+			order: [[0, 'desc']],
+		})
+	}
+}
+
+// Load Cancelled Requests Table
+loadCancelledRequests = () => {
+	const dt = $('#cancelled-requests')
+
+	if (dt.length) {
+		dt.DataTable({
+			bDestroy: true,
+			scrollX: true,
+			ajax: {
+				url: `${apiURL}odrs/super_admin/requests_history/Cancelled`,
+				type: 'GET',
+				headers: AJAX_HEADERS,
+			},
+			columns: [
+				// Control Number
+				{
+					data: null,
+					render: (data) => {
+						return `<span class="text-primary fw-medium">${data.control_no}</span>`
+					},
+				},
+
+				// Student
+				{
+					data: null,
+					render: (data) => {
+						const gender = data.user_assigned_to_request.user_profiles[0].gender
+						const fullName = data.user_assigned_to_request.user_profiles[0].full_name
+
+						if (gender == 'Male') {
+							return `
+							<div class="d-flex align-items-center fw-medium">
+								<img src="${baseURL}public/images/profile/flat-faces-icons-circle-man-6.png" class="avatar-xs rounded-circle me-2">
+								<div>
+									<span class="d-block fw-medium">${fullName}</span>
+									<i class="mdi mdi-gender-male text-info"></i>
+									<small>${gender}</small>
+								</div>
+							</div>
+						`
+						} else {
+							return `
+							<div class="d-flex align-items-center fw-medium">
+								<img src="${baseURL}public/images/profile/flat-faces-icons-circle-woman-1.png" class="avatar-xs rounded-circle me-2">
+								<div>
+									<span class="d-block fw-medium">${fullName}</span>
+									<i class="mdi mdi-gender-female text-danger"></i>
+									<small>${gender}</small>
+								</div>
+							</div>
+						`
+						}
+					},
+				},
+
+				// Date Filed
+				{
+					data: null,
+					render: (data) => {
+						const date = moment(data.pending_for_clearance).format('DD, MMM. YYYY')
+						const time = moment(data.pending_for_clearance).format('hh:mm A')
+
+						return `
+							<div class="d-flex align-items-center">
+								<i class="ri-calendar-todo-fill text-primary"></i>
+								<span class="ms-2">${date}<small class="text-muted ms-1">${time}</small></span>
+							</div>
+						`
+					},
+				},
+
+				// Details
+				{
+					data: null,
+					width: '30%',
+					render: (data) => {
+						const course = data.user_assigned_to_request.education_profile.course_when_admitted
+						const purpose = data.purpose_of_request
+						return `
+							<table>
+								<tbody>
+									<tr>
+										<td>
+											<span class="fw-medium badge bg-primary">Course: </span>
+										</td>
+										<td><span class="text-uppercase">${course}</span></td>
+									</tr>
+									<tr>
+										<td>
+											<span class="fw-medium badge bg-primary me-3">Purpose: </span>
+										</td>
+										<td>${purpose}</td>
+									</tr>
+								</tbody>
+							</table>
+						`
+					},
+				},
+
+				// Request Status
+				{
+					data: null,
+					width: '10%',
+					render: (data) => {
+						return `
+							<div class="mt-2 d-grid fw-bolder badge badge-soft-danger position-relative">
+								<i class="m-2 mdi mdi-cancel fs-13"></i>
+								<span class="pb-2 text-uppercase">${data.status_of_request}</span>
+								<button type="button" class="btn btn-sm btn-secondary bg-gradient waves-effect waves-light rounded-circle position-absolute top-0 start-100 translate-middle" data-bs-toggle="modal" data-bs-target="#viewProcessStatusFlow">?</button>
+							</div>
+						`
+					},
+				},
+
+				// Date Cancelled
+				{
+					data: null,
+					render: (data) => {
+						const date = moment(data.cancelled).format('DD, MMM. YYYY')
+						const time = moment(data.cancelled).format('hh:mm A')
+
+						return `
+							<div class="d-flex align-items-center">
+								<i class="ri-calendar-todo-fill text-primary"></i>
+								<span class="ms-2">${date}<small class="text-muted ms-1">${time}</small></span>
+							</div>
+						`
+					},
+				},
+
+				// Action
+				{
+					data: null,
+					width: '5%',
+					class: 'text-center',
+					render: (data) => {
+						return `
+							<div class="d-flex gap-2 justify-content-center">
+								<button type="button" class="btn btn-info btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#viewRequestDetails"><i class="ri-eye-fill"></i></button>
+								<button type="button" class="btn btn-danger btn-icon waves-effect waves-light" onclick="deleteRequest()"><i class="bx bx-trash fs-4"></i></button>
+							</div>
+						`
+					},
+				},
+			],
+			order: [[0, 'desc']],
+		})
+	}
+}
+
+// Load Deleted Requests Table
+loadDeletedRequests = () => {
+	const dt = $('#deleted-requests')
+
+	if (dt.length) {
+		dt.DataTable({
+			bDestroy: true,
+			ajax: {
+				url: `${apiURL}odrs/super_admin/requests_history/Deleted`,
+				type: 'GET',
+				headers: AJAX_HEADERS,
+			},
+			columns: [
+				// Control Number
+				{
+					data: null,
+					render: (data) => {
+						return `<span class="text-primary fw-medium">${data.control_no}</span>`
+					},
+				},
+
+				// Student
+				{
+					data: null,
+					render: (data) => {
+						const gender = data.user_assigned_to_request.user_profiles[0].gender
+						const fullName = data.user_assigned_to_request.user_profiles[0].full_name
+
+						if (gender == 'Male') {
+							return `
+								<div class="d-flex align-items-center fw-medium">
+									<img src="${baseURL}public/images/profile/flat-faces-icons-circle-man-6.png" class="avatar-xs rounded-circle me-2">
+									<div>
+										<span class="d-block fw-medium">${fullName}</span>
+										<i class="mdi mdi-gender-male text-info"></i>
+										<small>${gender}</small>
+									</div>
+								</div>
+							`
+						} else {
+							return `
+								<div class="d-flex align-items-center fw-medium">
+									<img src="${baseURL}public/images/profile/flat-faces-icons-circle-woman-1.png" class="avatar-xs rounded-circle me-2">
+									<div>
+										<span class="d-block fw-medium">${fullName}</span>
+										<i class="mdi mdi-gender-female text-danger"></i>
+										<small>${gender}</small>
+									</div>
+								</div>
+							`
+						}
+					},
+				},
+
+				// Date Filed
+				{
+					data: null,
+					render: (data) => {
+						const date = moment(data.pending_for_clearance).format('DD, MMM. YYYY')
+						const time = moment(data.pending_for_clearance).format('hh:mm A')
+
+						return `
+							<div class="d-flex align-items-center">
+								<i class="ri-calendar-todo-fill text-primary"></i>
+								<span class="ms-2">${date}<small class="text-muted ms-1">${time}</small></span>
+							</div>
+						`
+					},
+				},
+
+				// Payment Status
+				{
+					data: null,
+					render: (data) => {
+						if (data.payment_status === 'Cancelled') {
+							return `
+								<div class="d-flex badge badge-soft-danger align-items-center justify-content-center">
+									<i class="me-2 mdi mdi-cash-remove fs-15"></i>
+									<span class="text-uppercase fw-bold">${data.payment_status}</span>
+								</div>
+							`
+						} else if (data.payment_status === 'Paid') {
+							return `
+								<div class="d-flex badge badge-soft-success align-items-center justify-content-center">
+									<i class="me-2 mdi mdi-cash-check fs-15"></i>
+									<span class="text-uppercase fw-bold">${data.payment_status}</span>
+								</div>
+								<span class="mt-1 d-block badge badge-soft-dark badge-border">OR No. ${data.or_no} </span>
+							`
+						}
+					},
+				},
+
+				// Details
+				{
+					data: null,
+					width: '30%',
+					render: (data) => {
+						const course = data.user_assigned_to_request.education_profile.course_when_admitted
+						const purpose = data.purpose_of_request
+						return `
+							<table>
+								<tbody>
+									<tr>
+										<td>
+											<span class="fw-medium badge bg-primary">Course: </span>
+										</td>
+										<td><span class="text-uppercase">${course}</span></td>
+									</tr>
+									<tr>
+										<td>
+											<span class="fw-medium badge bg-primary me-3">Purpose: </span>
+										</td>
+										<td>${purpose}</td>
+									</tr>
+								</tbody>
+							</table>
+						`
+					},
+				},
+
+				// Date Deleted
+				{
+					data: null,
+					render: (data) => {
+						const date = moment(data.deleted).format('DD, MMM. YYYY')
+						const time = moment(data.deleted).format('hh:mm A')
+
+						return `
+							<div class="d-flex align-items-center">
+								<i class="ri-calendar-todo-fill text-primary"></i>
+								<span class="ms-2">${date}<small class="text-muted ms-1">${time}</small></span>
+							</div>
+						`
+					},
+				},
+			],
+			order: [[0, 'desc']],
+		})
+	}
+}
+
 // Delete Request
 deleteRequest = () => {
 	$.ajaxSetup({
