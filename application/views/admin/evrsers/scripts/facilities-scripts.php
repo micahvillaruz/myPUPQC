2a7c7e2632ed9854a62b6a18b65d5c9416293bb8
Zsyrhill Soria <zsyrhillsoria@gmail.com>
2022-12-24 19:56:36 +0800

    Added and modified facilities scripts

---
+++ application/views/admin/evrsers/scripts/facilities-scripts.php
@@ -0,0 +1,19 @@
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+<!-- validation init -->
+<script src="<?= base_url() ?>public/js/pages/form-validation.init.js"></script>
+
+<!-- flatpickr.js -->
+<script type='text/javascript' src='<?= base_url() ?>public/libs/flatpickr/flatpickr.min.js'></script>
+
+<!-- form wizard init -->
+<script src="<?= base_url() ?>public/js/pages/form-wizard.init.js"></script>
+
+<!-- Sweet alert init js-->
+<script src="<?= base_url() ?>public/js/pages/sweetalerts.init.js"></script>
+
+<!-- student ajax -->
+<script src="<?= base_url() ?>public/js/ajax/admin/evrsers/facilities.ajax.js"></script>
+
+<!-- App js -->
+<script src="<?= base_url() ?>public/js/app.js"></script>
