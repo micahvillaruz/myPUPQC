30bbd6168363e357ebb0990e647b9e158ed0d25a
Zsyrhill Soria <zsyrhillsoria@gmail.com>
2022-12-24 19:54:53 +0800

    Improved facilities page responsiveness

--- application/views/admin/evrsers/facilities.php
+++ application/views/admin/evrsers/facilities.php
@@ -1,97 +1,88 @@
-<div class="row">
-    <div class="col-lg-12 w-100">
-        <button class="btn btn-primary btn-md float-end mb-3 fs-5"><i class="bx bx-plus"></i> Add New Facility</button>
-    </div>
-    <div class="col-lg">
-        <div class="card">
-            <img src="../../public/images/facilities/img-5.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h1 class="card-title text-dark fw-bold">PUP QC Gymnasium</h1>
-                <p class="card-text">
-                    Ususally used for sports and other activities. It is located at the front of the Miserior Building and front of the Acad Building.
-                </p>
-                <h5>
-                    <span class="badge badge-soft-primary badge-border">
-                        <span class="fw-bold">Capacity: </span>50-500 People
-                    </span>
-                    <span class="badge badge-soft-success">Available</span>
-                    <button class="btn btn-primary float-end">Edit Facility</button>
-                </h5>
-            </div>
+<div>
+    <div class="row">
+        <div class="col-lg-12 w-100 pe-4 mx-auto">
+            <button class="btn btn-primary btn-lg float-end mb-1 fs-5"><i class="bx bx-plus"></i> Add New Facility</button>
         </div>
     </div>
-    <div class="col-lg">
-        <div class="card">
-            <img src="../../public/images/facilities/img-11.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h1 class="card-title text-dark fw-bold">Computer Laboratory</h1>
-                <p class="card-text">
-                    Located beside the Audio-Visual Room, near Rothlener Building. It is used for computer related activities including laboratory subjects, computing, extension, etc.
-                </p>
-                <h5>
-                    <span class="badge badge-soft-primary badge-border">
-                        <span class="fw-bold">Capacity: </span>10-15 People
-                    </span>
-                    <span class="badge badge-soft-success">Available</span>
-                    <button class="btn btn-primary float-end">Edit Facility</button>
-                </h5>
+    <div class="row row-cols-xl-3 row-cols-lg-1 row-cols-sm-1 row-cols-md-3 p-3">
+        <div class="col-lg-4 col-md-8 col-sm-8 mx-auto">
+            <div class="card">
+                <img src="../../public/images/facilities/img-5.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h1 class="card-title text-dark fw-bold">
+                        <span id="facility-name">PUP QC Gymnasium</span>
+                    </h1>
+                    <p class="card-text">
+                        <span id="facility-description">
+                            Ususally used for sports and other activities. It is located at the front of the Miserior Building and front of the Acad Building.
+                        </span>
+                    </p>
+                    <h5>
+                        <span id="facility-status" class="badge badge-soft-success">Available</span>
+                        <button id="edit-facility" class="btn btn-primary float-end">Edit Facility</button>
+                    </h5>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col-lg">
-        <div class="card">
-            <img src="../../public/images/facilities/img-6.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h1 class="card-title text-dark fw-bold">Audio-Visual Room</h1>
-                <p class="card-text">
-                    Located beside the Computer Laboratory, near Rothlener Building. It is used for audio-visual related activities including laboratory subjects, extension, etc.
-                </p>
-                <h5>
-                    <span class="badge badge-soft-primary badge-border">
-                        <span class="fw-bold">Capacity: </span>25-50 People
-                    </span>
-                    <span class="badge badge-soft-success">Available</span>
-                    <button class="btn btn-primary float-end">Edit Facility</button>
-                </h5>
+        <div class="col-lg-4 col-md-8 col-sm-8 mx-auto">
+            <div class="card">
+                <img src="../../public/images/facilities/img-11.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h1 class="card-title text-dark fw-bold">Computer Laboratory</h1>
+                    <p class="card-text">
+                        Located beside the Audio-Visual Room, near Rothlener Building. It is used for computer related activities including laboratory subjects, computing, extension, etc.
+                    </p>
+                    <h5>
+                        <span class="badge badge-soft-success">Available</span>
+                        <button class="btn btn-primary float-end">Edit Facility</button>
+                    </h5>
+                </div>
             </div>
         </div>
-    </div>
-</div>
-<div class="row">
-    <div class="col-lg">
-        <div class="card">
-            <img src="../../public/images/facilities/img-8.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h1 class="card-title text-dark fw-bold">Chapel</h1>
-                <p class="card-text">
-                    Located at the side of the Miserior Building. It is used for activities like seminars, gatherings, general assemblies & course orientation.
-                </p>
-                <h5>
-                    <span class="badge badge-soft-primary badge-border">
-                        <span class="fw-bold">Capacity: </span>50-100 People
-                    </span>
-                    <span class="badge badge-soft-danger">Unavailable</span>
-                    <button class="btn btn-primary float-end">Edit Facility</button>
-                </h5>
+        <div class="col-lg-4 col-md-8 col-sm-8 mx-auto">
+            <div class="card">
+                <img src="../../public/images/facilities/img-6.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h1 class="card-title text-dark fw-bold">Audio-Visual Room</h1>
+                    <p class="card-text">
+                        Located beside the Computer Laboratory, near Rothlener Building. It is used for audio-visual related activities including laboratory subjects, extension, etc.
+                    </p>
+                    <h5>
+                        <span class="badge badge-soft-success">Available</span>
+                        <button class="btn btn-primary float-end">Edit Facility</button>
+                    </h5>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col-lg">
-        <div class="card">
-            <img src="../../public/images/facilities/img-4.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h1 class="card-title text-dark fw-bold">Library</h1>
-                <p class="card-text">
-                    Some quick example text to build on the card title and make up the bulk of the card's content.
-                </p>
-                <h5>
-                    <span class="badge badge-soft-primary badge-border">
-                        <span class="fw-bold">Capacity: </span>Max 15 People
-                    </span>
-                    <span class="badge badge-soft-danger">Unavailable</span>
-                    <button class="btn btn-primary float-end">Edit Facility</button>
-                </h5>
+            <div class="col-lg-4 col-md-8 col-sm-8">
+                <div class="card">
+                    <img src="../../public/images/facilities/img-8.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h1 class="card-title text-dark fw-bold">Chapel</h1>
+                        <p class="card-text">
+                            Located at the side of the Miserior Building. It is used for activities like seminars, gatherings, general assemblies & course orientation.
+                        </p>
+                        <h5>
+                            <span class="badge badge-soft-danger">Unavailable</span>
+                            <button class="btn btn-primary float-end">Edit Facility</button>
+                        </h5>
+                    </div>
+                </div>
             </div>
-        </div>
+            <div class="col-lg-4 col-md-8 col-sm-8">
+                <div class="card">
+                    <img src="../../public/images/facilities/img-4.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h1 class="card-title text-dark fw-bold">Library</h1>
+                        <p class="card-text">
+                            Some quick example text to build on the card title and make up the bulk of the card's content.
+                        </p>
+                        <h5>
+                            <span class="badge badge-soft-danger">Unavailable</span>
+                            <button class="btn btn-primary float-end">Edit Facility</button>
+                        </h5>
+                    </div>
+                </div>
+            </div>
     </div>
 </div>
