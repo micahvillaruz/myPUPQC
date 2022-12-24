eb3d59b3cb0c16f7fe90000dc6e314933d6778a8
Zsyrhill Soria <zsyrhillsoria@gmail.com>
2022-12-24 19:59:06 +0800

    Refactored evrsers hist functions student / staff

--- public/js/ajax/pup-staff/evrsers/evrsershist.ajax.js
+++ public/js/ajax/pup-staff/evrsers/evrsershist.ajax.js
@@ -1,65 +1,60 @@
 $(function() {
-    // viewReservationDetails()
 
     viewAllReservationHistoryStaff()
 
 })
 
 // View Own Reservation details
-// viewReservationDetails = (reservation_id) => {
-//     $.ajaxSetup({
-//         headers: {
-//             Accept: 'application/json',
-//             Authorization: 'Bearer ' + TOKEN,
-//             ContentType: 'application/x-www-form-urlencoded',
-//         },
-//     })
+viewReservationDetails = (reservation_id) => {
+    $.ajaxSetup({
+        headers: {
+            Accept: 'application/json',
+            Authorization: 'Bearer ' + TOKEN,
+            ContentType: 'application/x-www-form-urlencoded',
+        },
+    })
 
-//     $.ajax({
-//         type: 'GET',
-//         cache: false,
-//         url: apiURL + `evrsers/student/view_reservation/${reservation_id}`,
-//         dataType: 'json',
-//         success: (result) => {
-//             console.log(result)
-//             const userData = result.data
-//             const venue = userData.facilities_assigned_to_reservation.facility_name
-//             console.log(venue)
+    $.ajax({
+        type: 'GET',
+        cache: false,
+        url: apiURL + `evrsers/pup_staff/view_details_reservation/${reservation_id}`,
+        dataType: 'json',
+        success: (result) => {
+            console.log(result)
+            const userData = result.data
+            const venue = userData.facilities_assigned_to_reservation.facility_name
+            console.log(venue)
 
-//             $('#reserve_number').html(userData.reservation_number)
-//             $('#organization').html(userData.organization_name)
-//             $('#event_title').html(userData.event_title)
-//             $('#facility_name').html(venue)
-//             $('#event_details').html(userData.event_details)
-//             $('#reserve_date').html(moment(userData.reserve_date).format('LL'))
-//             const time = `${userData.time_from} - ${userData.time_to}`
-//             $('#time').html(time)
-//             $('#remarks').html(userData.remarks)
-//             console.log(userData.reserve_attachments_1)
-//             $('#attachment1').html(userData.reserve_attachments_1)
-//             $('#attachment2').html(userData.reserve_attachments_2)
-//             $('#attachment3').html(userData.reserve_attachments_3)
-//             let reservation_status = userData.reserve_status
-//             if (reservation_status == 'Pending') {
-//                 $('#reservation_status').html(
-//                     `<span class="badge rounded-pill bg-secondary">${reservation_status}</span>`,
-//                 )
-//             } else if (reservation_status == 'Cancelled by Staff') {
-//                 $('#reservation_status').html(
-//                     `<span class="badge rounded-pill bg-danger">${reservation_status}</span>`,
-//                 )
-//             } else if (reservation_status == 'Cancelled by Student') {
-//                 $('#reservation_status').html(
-//                     `<span class="badge rounded-pill bg-info">${reservation_status}</span>`,
-//                 )
-//             } else {
-//                 $('#reservation_status').html(
-//                     `<span class="badge rounded-pill bg-success">${reservation_status}</span>`,
-//                 )
-//             }
-//         },
-//     })
-// }
+            $('#reserve_number').html(userData.reservation_number)
+            $('#organization').html(userData.organization_name)
+            $('#event_title').html(userData.event_title)
+            $('#facility_name').html(venue)
+            $('#event_details').html(userData.event_details)
+            $('#reserve_date').html(moment(userData.reserve_date).format('LL'))
+            const time = `${userData.time_from} - ${userData.time_to}`
+            $('#time').html(time)
+            $('#remarks').html(userData.remarks)
+            console.log(userData.reserve_attachments_1)
+            $('#attachment1').html(userData.reserve_attachments_1)
+            $('#attachment2').html(userData.reserve_attachments_2)
+            $('#attachment3').html(userData.reserve_attachments_3)
+            let reservation_status = userData.reserve_status
+            if (reservation_status == 'Cancelled by Staff') {
+                $('#reservation_status').html(
+                    `<span class="badge rounded-pill badge-soft-danger">${reservation_status}</span>`,
+                )
+            } else if (reservation_status == 'Cancelled by Student') {
+                $('#reservation_status').html(
+                    `<span class="badge rounded-pill badge-soft-danger">${reservation_status}</span>`,
+                )
+            } else {
+                $('#reservation_status').html(
+                    `<span class="badge rounded-pill badge-soft-success">${reservation_status}</span>`,
+                )
+            }
+        },
+    })
+}
 
 //View All Reservation History
 viewAllReservationHistoryStaff = () => {
@@ -76,7 +71,7 @@ viewAllReservationHistoryStaff = () => {
         dt.DataTable({
             bDestroy: true,
             ajax: {
-                url: apiURL + `evrsers/pup_staff/view_done`,
+                url: apiURL + `evrsers/pup_staff/view_done/`,
                 type: 'GET',
                 ContentType: 'application/x-www-form-urlencoded',
             },
@@ -155,9 +150,10 @@ viewAllReservationHistoryStaff = () => {
                     data: null,
                     class: 'text-center',
                     render: (data) => {
+                        // console.log(data.reservation_id)
                         return `
                             <div class="dropdown d-inline-block">
-                                <button type="button" class="btn btn-info btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#viewReservationModal" onclick="viewDetailsReservationStaff('${data.reservation_id}')"><i class="ri-eye-fill fs-5"></i></button>
+                                <button type="button" class="btn btn-info btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#viewReservationDetails" onclick="viewReservationDetails('${data.reservation_id}')"><i class="ri-eye-fill fs-5"></i></button>
                             </div>
                                 `
                     },
--- public/js/ajax/student/evrsers/evrsershist.ajax.js
+++ public/js/ajax/student/evrsers/evrsershist.ajax.js
@@ -1,68 +1,58 @@
 $(function() {
-    // viewReservationDetails()
-
     ownReservationsHistory()
 
 })
 
-
 // View Own Reservation details
-// viewReservationDetails = (reservation_id) => {
-//     $.ajaxSetup({
-//         headers: {
-//             Accept: 'application/json',
-//             Authorization: 'Bearer ' + TOKEN,
-//             ContentType: 'application/x-www-form-urlencoded',
-//         },
-//     })
+viewReservationDetails = (reservation_id) => {
+    $.ajaxSetup({
+        headers: {
+            Accept: 'application/json',
+            Authorization: 'Bearer ' + TOKEN,
+            ContentType: 'application/x-www-form-urlencoded',
+        },
+    })
 
-//     $.ajax({
-//         type: 'GET',
-//         cache: false,
-//         url: apiURL + `evrsers/student/view_reservation/${reservation_id}`,
-//         dataType: 'json',
-//         success: (result) => {
-//             console.log(result)
-//             const userData = result.data
-//             const venue = userData.facilities_assigned_to_reservation.facility_name
-//             console.log(venue)
+    $.ajax({
+        type: 'GET',
+        cache: false,
+        url: apiURL + `evrsers/student/view_reservation/${reservation_id}`,
+        dataType: 'json',
+        success: (result) => {
+            console.log(result)
+            const userData = result.data
+            const venue = userData.facilities_assigned_to_reservation.facility_name
+            console.log(venue)
 
-//             $('#reserve_number').html(userData.reservation_number)
-//             $('#organization').html(userData.organization_name)
-//             $('#event_title').html(userData.event_title)
-//             $('#facility_name').html(venue)
-//             $('#event_details').html(userData.event_details)
-//             $('#reserve_date').html(moment(userData.reserve_date).format('LL'))
-//             const time = `${userData.time_from} - ${userData.time_to}`
-//             $('#time').html(time)
-//             $('#remarks').html(userData.remarks)
-//             console.log(userData.reserve_attachments_1)
-//             $('#attachment1').html(userData.reserve_attachments_1)
-//             $('#attachment2').html(userData.reserve_attachments_2)
-//             $('#attachment3').html(userData.reserve_attachments_3)
-//             let reservation_status = userData.reserve_status
-//             if (reservation_status == 'Pending') {
-//                 $('#reservation_status').html(
-//                     `<span class="badge rounded-pill bg-secondary">${reservation_status}</span>`,
-//                 )
-//             } else if (reservation_status == 'Cancelled by Staff') {
-//                 $('#reservation_status').html(
-//                     `<span class="badge rounded-pill bg-danger">${reservation_status}</span>`,
-//                 )
-//             } else if (reservation_status == 'Cancelled by Student') {
-//                 $('#reservation_status').html(
-//                     `<span class="badge rounded-pill bg-info">${reservation_status}</span>`,
-//                 )
-//             } else {
-//                 $('#reservation_status').html(
-//                     `<span class="badge rounded-pill bg-success">${reservation_status}</span>`,
-//                 )
-//             }
-//         },
-//     })
-// }
-
-// console.log(user_id)
+            $('#reserve_number').html(userData.reservation_number)
+            $('#organization').html(userData.organization_name)
+            $('#event_title').html(userData.event_title)
+            $('#facility_name').html(venue)
+            $('#event_details').html(userData.event_details)
+            $('#reserve_date').html(moment(userData.reserve_date).format('LL'))
+            const time = `${userData.time_from} - ${userData.time_to}`
+            $('#time').html(time)
+            $('#remarks').html(userData.remarks)
+            $('#attachment1').html(userData.reserve_attachments_1)
+            $('#attachment2').html(userData.reserve_attachments_2)
+            $('#attachment3').html(userData.reserve_attachments_3)
+            let reservation_status = userData.reserve_status
+            if (reservation_status == 'Cancelled by Staff') {
+                $('#reservation_status').html(
+                    `<span class="badge rounded-pill badge-soft-danger">${reservation_status}</span>`,
+                )
+            } else if (reservation_status == 'Cancelled by Student') {
+                $('#reservation_status').html(
+                    `<span class="badge rounded-pill badge-soft-danger">${reservation_status}</span>`,
+                )
+            } else {
+                $('#reservation_status').html(
+                    `<span class="badge rounded-pill bg-success">${reservation_status}</span>`,
+                )
+            }
+        },
+    })
+}
 
 //View All Own Reservation History
 ownReservationsHistory = (user_id) => {
@@ -135,7 +125,27 @@ ownReservationsHistory = (user_id) => {
                     class: 'text-center',
                     render: (data) => {
                         const reserve_status = data.reserve_status
-                        return `<span class="badge rounded-pill bg-success">${reserve_status}</span>`
+                        if (reserve_status == "Done") {
+                            return `<span class="badge rounded-pill bg-success">Done</span>`
+                        } else if (reserve_status == "Cancelled by Staff") {
+                            return `<span class="badge rounded-pill badge-soft-danger">Cancelled by Staff</span>`
+                        } else if (reserve_status == "Cancelled by Student") {
+                            return `<span class="badge rounded-pill badge-soft-danger">Cancelled by Student</span>`
+                        }
+                    },
+                },
+
+                //Action
+                {
+                    data: null,
+                    class: 'text-center',
+                    render: (data) => {
+                        // console.log(data.reservation_id)
+                        return `
+                            <div class="dropdown d-inline-block">
+                                <button type="button" class="btn btn-info btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#viewOwnReservation" onclick="viewReservationDetails('${data.reservation_id}')"><i class="ri-eye-fill fs-5"></i></button>
+                            </div>
+                                `
                     },
                 },
             ],
