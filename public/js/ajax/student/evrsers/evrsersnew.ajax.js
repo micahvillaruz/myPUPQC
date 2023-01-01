a8860715b953b0fde9961478f7eb9cfc8edb6bc3
Zsyrhill Soria <zsyrhillsoria@gmail.com>
2023-01-02 01:27:49 +0800

    Connected add new reservation ajax leg8

--- public/js/ajax/student/evrsers/evrsersnew.ajax.js
+++ public/js/ajax/student/evrsers/evrsersnew.ajax.js
@@ -75,37 +75,24 @@ loadFacilities = () => {
 }
 
 addNewReservation = () => {
-    // manually validate form before submitting
-    // with the id's: orgfloatingInput, eventTitlefloatingInput, eventDetailsfloatingInput, 
-    // reserveDatefloatingInput, timeFromfloatingInput, timeTofloatingInput
-    // inputGroupFile01, inputGroupFile02, inputGroupFile03
-    // if all are valid, then proceed to ajax call, else, display invalid feedback using custom bootstrap form validation
 
-    // get all the values from the form
-    const facility = $('input[name="facility"]:checked').val()
-    const org = $('#orgfloatingInput').val()
-    const event_title = $('#eventTitlefloatingInput').val()
-    const event_details = $('#eventDetailsfloatingInput').val()
-    const reserve_date = $('#reserveDatefloatingInput').val()
-    const time_from = $('#timeFromfloatingInput').val()
-    const time_to = $('#timeTofloatingInput').val()
-    const file1 = $('#inputGroupFile01').val()
-    const file2 = $('#inputGroupFile02').val()
-    const file3 = $('#inputGroupFile03').val()
+    if ($('#addNewReservation')[0].checkValidity()) {
+        // no validation error
+        const form = new FormData($('#addNewReservation')[0])
+            // with the id's: orgfloatingInput, eventTitlefloatingInput, eventDetailsfloatingInput,
+            // reserveDatefloatingInput, timeFromfloatingInput, timeTofloatingInput
+            // inputGroupFile01, inputGroupFile02, inputGroupFile03
+        const organization_name = form.get('orgfloatingInput')
+        const event_title = form.get('eventTitlefloatingInput')
+        const event_details = form.get('eventDetailsfloatingInput')
+        const reserve_date = form.get('reserveDatefloatingInput')
+        const time_from = form.get('timeFromfloatingInput')
+        const time_to = form.get('timeTofloatingInput')
+        const reserve_attachments_1 = form.get('inputGroupFile01')
+        const reserve_attachments_2 = form.get('inputGroupFile02')
+        const reserve_attachments_3 = form.get('inputGroupFile03')
+        const facility_id = $('input[name="facility"]:checked').attr('id')
 
-    // check if all the values are not empty
-    if (
-        facility &&
-        org &&
-        event_title &&
-        event_details &&
-        reserve_date &&
-        time_from &&
-        time_to &&
-        file1 &&
-        file2 &&
-        file3
-    ) {
         // all values are not empty, proceed to ajax call
         $.ajaxSetup({
             headers: {
@@ -119,37 +106,48 @@ addNewReservation = () => {
             type: 'POST',
             url: apiURL + `evrsers/student/add_reservation`,
             data: {
-                facility: facility,
-                org: org,
+                organization_name: organization_name,
                 event_title: event_title,
                 event_details: event_details,
                 reserve_date: reserve_date,
                 time_from: time_from,
                 time_to: time_to,
-                file1: file1,
-                file2: file2,
-                file3: file3,
+                reserve_attachments_1: reserve_attachments_1,
+                reserve_attachments_2: reserve_attachments_2,
+                reserve_attachments_3: reserve_attachments_3,
+                facility_id: facility_id,
             },
             success: (result) => {
                 const data = result.data
                 console.log(data)
 
                 //  display success message using sweetalert2
-                Swal.fire({
-                    icon: 'success',
-                    title: 'Reservation Added',
-                    text: 'Your reservation has been added successfully',
-                    showConfirmButton: false,
-                    timer: 1500,
-                }).then(() => {
-                    // redirect to view reservations page
-                    window.location.href = '/student/evrsers/view'
-
-                })
+                if (result) {
+                    Swal.fire({
+                        html: '<div class="mt-3"><lord-icon src="https://cdn.lordicon.com/lupuorrc.json" trigger="loop" colors="primary:#0ab39c,secondary:#405189" style="width:120px;height:120px"></lord-icon><div class="mt-4 pt-2 fs-15"><h4>Well done !</h4><p class="text-muted mx-4 mb-0">You have successfully created a request!</p></div></div>',
+                        showCancelButton: !0,
+                        showConfirmButton: !1,
+                        cancelButtonClass: 'btn btn-success w-xs mb-1',
+                        cancelButtonText: 'Ok',
+                        buttonsStyling: !1,
+                        showCloseButton: !0,
+                    }).then(function() {
+                        window.location.href = `${baseURL}student/evrsers/view-reservation`
+                    })
+                }
             },
-            error: (error) => {
-                console.log(error)
-            }
+        }).fail((xhr) => {
+            Swal.fire({
+                html: `<div class="mt-3"><lord-icon src="https://cdn.lordicon.com/tdrtiskw.json" trigger="loop" colors="primary:#f06548,secondary:#f7b84b" style="width:120px;height:120px"></lord-icon><div class="mt-4 pt-2 fs-15"><h4>Something went Wrong !</h4><p class="text-muted mx-4 mb-0">${
+				JSON.parse(xhr.responseText).message
+			}</p></div></div>`,
+                showCancelButton: !0,
+                showConfirmButton: !1,
+                cancelButtonClass: 'btn btn-danger w-xs mb-1',
+                cancelButtonText: 'Dismiss',
+                buttonsStyling: !1,
+                showCloseButton: !0,
+            })
         })
     }
 }
