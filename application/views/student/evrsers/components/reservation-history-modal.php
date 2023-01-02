117114854424eaca7290c641145416d46f0061e1
Zsyrhill Soria <zsyrhillsoria@gmail.com>
2023-01-03 01:40:29 +0800

    Minor UI improvements in reservation details

--- application/views/student/evrsers/components/reservation-history-modal.php
+++ application/views/student/evrsers/components/reservation-history-modal.php
@@ -1,135 +1,57 @@
 <!-- View Reservation modal -->
 <div class="modal fade" id="viewOwnReservation" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-lg">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalScrollableTitle">
-          Reservation Information
-        </h5>
+    <div class="modal-content px-3">
+      <div class="modal-header position-relative">
+        <div class="d-flex position-absolute top-50 start-50 translate-middle-x">
+          <h5 id="reserve_number" class="fs-4 badge badge-outline-primary fw-bold mb-0"></h5>
+        </div>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        <div data-simplebar class="mx-n3">
-          <ul class="list list-group list-group-flush mb-0">
-            <!-- end list item -->
-            <li class="list-group-item">
-              <div class="d-flex align-items-center">
-                <div class="col-4">
-                  <h5 class="fs-13 mb-1 fw-medium"><a href="#" class="link text-dark">Reservation Control #:</a></h5>
+        <div class="row">
+          <div class="col-lg-8">
+            <h5 class="fs-5 badge badge-outline-info fw-bold mb-3" id="organization"></h5><br>
+            <h6 class="text-medium">EVENT TITLE</h6>
+            <h1 class="text-dark fw-bold mb-3 ms-2" id="event_title"></h1>
+            <div class="alert alert-border-left alert-primary mb-4 me-3 ms-2">
+              <div class="row">
+                <div class="col-lg-6">
+                  <h6 class="text-medium">DESCRIPTION</h6>
+                  <p id="event_details"></p>
                 </div>
-                <div class="col-8">
-                  <div class="d-flex fs-13 fw-medium" id="reserve_number"></div>
-                </div>
-              </div>
-            </li>
-            <!-- end list item -->
-            <li class="list-group-item">
-              <div class="d-flex align-items-center">
-                <div class="col-4">
-                  <h5 class="fs-13 mb-1 fw-medium"><a href="#" class="link text-dark">Event Title</a></h5>
-                </div>
-                <div class="col-8">
-                  <div class="d-flex fs-13 fw-medium" id="event_title"></div>
+                <div class="col-lg-6">
+                  <h6 class="text-medium">DATE</h6>
+                  <p id="reserve_date"></p>
+                  <h6 class="text-medium">TIME</h6>
+                  <p id="time"></p>
                 </div>
               </div>
-            </li>
-            <!-- end list item -->
-            <li class="list-group-item">
-              <div class="d-flex align-items-center">
-                <div class="col-4">
-                  <h5 class="fs-13 mb-1">Organizaton</h5>
-                </div>
-                <div class="col-8">
-                  <div class="d-flex fs-13 fw-medium" id="organization"></div>
-                </div>
+              <hr>
+              <div class="row">
+                <h6 class="text-medium">REMARKS</h6>
+                <p id="remarks"></p>
               </div>
-            </li>
-            <!-- end list item -->
-            <li class="list-group-item">
-              <div class="d-flex align-items-center">
-                <div class="col-4">
-                  <h5 class="fs-13 mb-1">Venue</h5>
-                </div>
-                <div class="col-8">
-                  <div class="d-flex fs-13 fw-medium" id="facility_name"></div>
-                </div>
+            </div>
+            <h6 class="text-medium">RESERVATION ATTACHMENTS</h6>
+            <div class="ms-2">
+              <span class="fs-5" id="attachment1"></span><br>
+              <span class="fs-5" id="attachment2"></span><br>
+              <span class="fs-5" id="attachment3"></span><br>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="row h-75 mb-3">
+              <div class="col">
+              <a href="" class="btn btn-soft-dark w-100 h-100" disabled>Dark</a>
               </div>
-            </li>
-            <!-- end list item -->
-            <li class="list-group-item">
-              <div class="d-flex align-items-center">
-                <div class="col-4">
-                  <h5 class="fs-13 mb-1">Event Details</h5>
-                </div>
-                <div class="col-8">
-                  <div class="d-flex fs-13 fw-medium" id="event_details"></div>
-                </div>
+            </div>
+            <div class="row">
+              <div class="col">
+              <h6 class="text-medium">RESERVATION STATUS</h6>
               </div>
-            </li>
-            <!-- end list item -->
-            <li class="list-group-item">
-              <div class="d-flex align-items-center">
-                <div class="col-4">
-                  <h5 class="fs-13 mb-1">Date</h5>
-                </div>
-                <div class="col-8">
-                  <div class="d-flex fs-13 fw-medium" id="reserve_date"></div>
-                </div>
-              </div>
-            </li>
-            <!-- end list item -->
-            <li class="list-group-item">
-              <div class="d-flex align-items-center">
-                <div class="col-4">
-                  <h5 class="fs-13 mb-1">Time</h5>
-                </div>
-                <div class="col-8">
-                  <div class="fw-medium">
-                    <span id="time"></span>
-                  </div>
-                </div>
-              </div>
-            </li>
-            <!-- end list item -->
-            <li class="list-group-item">
-              <div class="d-flex align-items-center">
-                <div class="col-4">
-                  <h5 class="fs-13 mb-1 fw-medium"><a href="#" class="link text-dark">Status</a></h5>
-                </div>
-                <div class="col-8" id="reservation_status"></div>
-              </div>
-            </li>
-            <!-- end list item -->
-            <li class="list-group-item">
-              <div class="d-flex align-items-center">
-                <div class="col-4">
-                  <h5 class="fs-13 mb-1">Attachments</h5>
-                </div>
-                <div class="col-8">
-                  <div class="fw-medium">
-                    <span id="attachment1"></span>
-                    <span id="attachment2"></span>
-                    <span id="attachment3"></span>
-                  </div>
-                </div>
-              </div>
-            </li>
-            <!-- end list item -->
-            <li class="list-group-item">
-              <div class="d-flex align-items-center">
-                <div class="col-4">
-                  <h5 class="fs-13 mb-1">Remarks</h5>
-                </div>
-                <div class="col-8">
-                  <div class="fw-medium">
-                    <span id="remarks"></span>
-                  </div>
-                </div>
-              </div>
-            </li>
-            <!-- end list item -->
-          </ul>
-          <!-- end ul list -->
+            </div>
+          </div>
         </div>
       </div>
       <div class="modal-footer">
