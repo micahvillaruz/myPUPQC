c8fce5378d3a565e71c2b4008f972c4d723139d4
Zsyrhill Soria <zsyrhillsoria@gmail.com>
2023-01-03 01:40:45 +0800

    Minor ajax changes in reservation history

--- public/js/ajax/student/evrsers/evrsershist.ajax.js
+++ public/js/ajax/student/evrsers/evrsershist.ajax.js
@@ -1,6 +1,5 @@
 $(function() {
     ownReservationsHistory()
-
 })
 
 // View Own Reservation details
@@ -21,21 +20,46 @@ viewReservationDetails = (reservation_id) => {
         success: (result) => {
             console.log(result)
             const userData = result.data
-            const venue = userData.facilities_assigned_to_reservation.facility_name
-            console.log(venue)
+                // const venue = userData.facilities_assigned_to_reservation.facility_name
+                // console.log(venue)
 
             $('#reserve_number').html(userData.reservation_number)
+            let organization_name = userData.organization_name
+            if (
+                organization_name == 'CHRS' ||
+                'COMMITS' ||
+                'DOMT.CS' ||
+                'FBTO' ||
+                'JMS' ||
+                'SPAS' ||
+                'YES'
+            ) {
+                $('#organization').html(`<span>${organization_name}</span>`)
+            } else if (organization_name == 'KATAGA' || 'MUSA' || 'PSC' || 'Vox Nova' || 'Other') {
+                $('#organization').html(`<span>${organization_name}</span>`)
+            } else if (organization_name == 'SSC' || 'COL') {
+                $('#organization').html(`<span>${organization_name}</span>`)
+            }
             $('#organization').html(userData.organization_name)
-            $('#event_title').html(userData.event_title)
-            $('#facility_name').html(venue)
+            event_title = userData.event_title
+                // convert to all caps
+            event_title = event_title.toUpperCase()
+            $('#event_title').html(event_title)
+                // $('#facility_name').html(venue)
             $('#event_details').html(userData.event_details)
             $('#reserve_date').html(moment(userData.reserve_date).format('LL'))
             const time = `${userData.time_from} - ${userData.time_to}`
             $('#time').html(time)
             $('#remarks').html(userData.remarks)
-            $('#attachment1').html(userData.reserve_attachments_1)
-            $('#attachment2').html(userData.reserve_attachments_2)
-            $('#attachment3').html(userData.reserve_attachments_3)
+            $('#attachment1').html(
+                `<i class="ri-file-fill text-primary me-2"></i><a href="${userData.reserve_attachments_1}" target="_blank" class="link fw-bold">Event Request</a>`,
+            )
+            $('#attachment2').html(
+                `<i class="ri-file-text-fill text-primary me-2"></i><a href="${userData.reserve_attachments_2}" target="_blank" class="link fw-bold">Concept Paper</a>`,
+            )
+            $('#attachment3').html(
+                `<i class="ri-file-copy-2-fill text-primary me-2"></i><a href="${userData.reserve_attachments_2}" target="_blank" class="link fw-bold">Others</a>`,
+            )
             let reservation_status = userData.reserve_status
             if (reservation_status == 'Cancelled by Staff') {
                 $('#reservation_status').html(
@@ -125,11 +149,11 @@ ownReservationsHistory = (user_id) => {
                     class: 'text-center',
                     render: (data) => {
                         const reserve_status = data.reserve_status
-                        if (reserve_status == "Done") {
+                        if (reserve_status == 'Done') {
                             return `<span class="badge rounded-pill bg-success">Done</span>`
-                        } else if (reserve_status == "Cancelled by Staff") {
+                        } else if (reserve_status == 'Cancelled by Staff') {
                             return `<span class="badge rounded-pill badge-soft-danger">Cancelled by Staff</span>`
-                        } else if (reserve_status == "Cancelled by Student") {
+                        } else if (reserve_status == 'Cancelled by Student') {
                             return `<span class="badge rounded-pill badge-soft-danger">Cancelled by Student</span>`
                         }
                     },
