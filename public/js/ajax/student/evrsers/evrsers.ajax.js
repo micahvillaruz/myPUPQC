5604a8bacb5bf8a5b6bab6e5e71302da6859a326
Zsyrhill Soria <zsyrhillsoria@gmail.com>
2023-01-02 22:09:26 +0800

    Restored view evrsers student table

--- public/js/ajax/student/evrsers/evrsers.ajax.js
+++ public/js/ajax/student/evrsers/evrsers.ajax.js
@@ -9,12 +9,6 @@ $(function() {
         addNewReservation()
     })
 
-    // $('#updateStudentForm').on('submit', function (e) {
-    // 	e.preventDefault() // prevent page refresh
-
-    // 	// pass data to API for updating of student's info
-    // 	updateStudentAJAX($('#edit_user_id').val())
-    // })
 })
 
 // View Own Reservation details
@@ -153,7 +147,7 @@ ownReservationsTable = () => {
                         } else if (reserve_status == 'Cancelled by Admin') {
                             return `<span class="badge rounded-pill bg-danger">${reserve_status}</span>`
                         } else if (reserve_status == 'Cancelled by Student') {
-                            return `<span class="badge rounded-pill bg-info">${reserve_status}</span>`
+                            return `<span class="badge rounded-pill bg-danger">${reserve_status}</span>`
                         }
                     },
                 },
@@ -169,7 +163,7 @@ ownReservationsTable = () => {
                             <div class="dropdown d-inline-block">
                                 <button type="button" class="btn btn-info btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#viewReservationModal" onclick="viewReservationDetails('${data.reservation_id}')"><i class="ri-eye-fill fs-5"></i></button>
                                 <button type="button" class="btn btn-warning btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#editReservationModal" onclick = "editReservation('${data.reservation_id}')"><i class="ri-edit-2-fill fs-5"></i></button>
-                                <button type="button" class="btn btn-danger btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#cancelReservationModal" onclick=""><i class="ri-close-fill fs-5"></i></button> 
+                                <button type="button" class="btn btn-danger btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#cancelReservationModal" onclick="cancelReservation('${data.reservation_id}')"><i class="ri-close-fill fs-5"></i></button> 
                             </div>
                                 `
                         } else if (reserve_status == 'Cancelled by Student') {
@@ -193,119 +187,22 @@ ownReservationsTable = () => {
     }
 }
 
-// Edit Reservation
-// editReservation = (reservation_id) => {
-//     $.ajaxSetup({
-//         headers: {
-//             Accept: 'application/json',
-//             Authorization: 'Bearer ' + TOKEN,
-//             ContentType: 'application/x-www-form-urlencoded',
-//         },
-//     })
-
-//     $.ajax({
-//         type: 'GET',
-//         cache: false,
-//         url: apiURL + `evrsers/student/view_reservation/${reservation_id}`,
-//         dataType: 'json',
-//         success: (result) => {
-//             console.log(result)
-//             const userData = result.data
-//             $('#reserve-number').html(userData.reservation_number)
-//         },
-//     })
-// }
-
-// addNewReservation = () => {
-//     // Create New Reservation
-//     if ($('#addNewReservation')[0].checkValidity()) {
-//         // no validation error
-//         Swal.fire({
-//             html: '<div class="mt-3"><lord-icon src="https://cdn.lordicon.com/lupuorrc.json" trigger="loop" colors="primary:#0ab39c,secondary:#405189" style="width:120px;height:120px"></lord-icon><div class="mt-4 pt-2 fs-15"><h4>Reservation Success !</h4><p class="text-muted mx-4 mb-0">Please wait for your reservation to be approved.</p></div></div>',
-//             showCancelButton: !0,
-//             showConfirmButton: !1,
-//             cancelButtonClass: 'btn btn-success w-xs mb-1',
-//             cancelButtonText: 'Ok',
-//             buttonsStyling: !1,
-//             showCloseButton: !0,
-//         }).then(function() {
-//             window.location = '/view-reservations';
-//         })
-//     }
-// }
-
-addNewReservation = () => {
-    // Create New Reservation
-    // no validation error
-    Swal.fire({
-        html: '<div class="mt-3"><lord-icon src="https://cdn.lordicon.com/lupuorrc.json" trigger="loop" colors="primary:#0ab39c,secondary:#405189" style="width:120px;height:120px"></lord-icon><div class="mt-4 pt-2 fs-15"><h4>Reservation Success !</h4><p class="text-muted mx-4 mb-0">Please wait for your reservation to be approved.</p></div></div>',
-        showCancelButton: !0,
-        showConfirmButton: !1,
-        cancelButtonClass: 'btn btn-success w-xs mb-1',
-        cancelButtonText: 'Ok',
-        buttonsStyling: !1,
-        showCloseButton: !0,
-    }).then(function() {
-        window.location = 'view-reservations'
+cancelReservation = (reservation_id) => {
+    $.ajaxSetup({
+        headers: {
+            Accept: 'application/json',
+            Authorization: 'Bearer ' + TOKEN,
+            ContentType: 'application/x-www-form-urlencoded',
+        },
+    })
+    $.ajax({
+        url: apiURL + 'evrsers/student/cancel_reservation/' + reservation_id,
+        type: 'DELETE',
+        ContentType: 'application/x-www-form-urlencoded',
+        success: (response) => {
+            console.log(response)
+            $('#cancelReservationModal').modal('hide')
+                // load reservations table prevent page reload
+        },
     })
 }
-
-//         const form = new FormData($('#addNewReservation')[0])
-
-//         data = {
-//             user_no: form.get('user_no'),
-//             password: form.get('password'),
-//             first_name: form.get('first_name'),
-//             middle_name: form.get('middle_name'),
-//             last_name: form.get('last_name'),
-//             extension_name: form.get('extension_name'),
-//             contact_number: form.get('contact_number'),
-//             birth_date: form.get('birth_date'),
-//             gender: form.get('gender'),
-//             house_street: form.get('house_street'),
-//             barangay: form.get('barangay'),
-//             municipality: form.get('municipality'),
-//             province: form.get('province'),
-//             region: form.get('region'),
-//             email_address: form.get('email_address'),
-//             civil_status: form.get('civil_status'),
-//             citizenship: form.get('citizenship'),
-//             religion: form.get('religion'),
-//         }
-
-//         $.ajax({
-//             url: apiURL + 'super_admin/student/add',
-//             type: 'POST',
-//             data: data,
-//             dataType: 'json',
-//             headers: AJAX_HEADERS,
-//             success: (result) => {
-//                 if (result) {
-//                     Swal.fire({
-//                         html: '<div class="mt-3"><lord-icon src="https://cdn.lordicon.com/lupuorrc.json" trigger="loop" colors="primary:#0ab39c,secondary:#405189" style="width:120px;height:120px"></lord-icon><div class="mt-4 pt-2 fs-15"><h4>Well done !</h4><p class="text-muted mx-4 mb-0">You have successfully enrolled a student!</p></div></div>',
-//                         showCancelButton: !0,
-//                         showConfirmButton: !1,
-//                         cancelButtonClass: 'btn btn-sucess w-xs mb-1',
-//                         cancelButtonText: 'Ok',
-//                         buttonsStyling: !1,
-//                         showCloseButton: !0,
-//                     }).then(function() {
-//                         $('#addStudentModal').modal('hide')
-//                         $('form#enrollStudentForm')[0].reset()
-
-//                         // Reload Student Datatable
-//                         loadStudentTable()
-//                     })
-//                 }
-//             },
-//         }).fail(() => {
-//             Swal.fire({
-//                 html: '<div class="mt-3"><lord-icon src="https://cdn.lordicon.com/tdrtiskw.json" trigger="loop" colors="primary:#f06548,secondary:#f7b84b" style="width:120px;height:120px"></lord-icon><div class="mt-4 pt-2 fs-15"><h4>Something went Wrong !</h4><p class="text-muted mx-4 mb-0">There was an error while enrolling a student. Please try again.</p></div></div>',
-//                 showCancelButton: !0,
-//                 showConfirmButton: !1,
-//                 cancelButtonClass: 'btn btn-danger w-xs mb-1',
-//                 cancelButtonText: 'Dismiss',
-//                 buttonsStyling: !1,
-//                 showCloseButton: !0,
-//             })
-// })
