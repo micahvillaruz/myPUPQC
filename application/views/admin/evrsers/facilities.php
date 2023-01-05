eb333d3f3ae1e9289a82dbb5af1291af8858f23a
Zsyrhill Soria <zsyrhillsoria@gmail.com>
2023-01-05 08:07:56 +0800

    UI fixes in facilities

--- application/views/admin/evrsers/facilities.php
+++ application/views/admin/evrsers/facilities.php
@@ -1,7 +1,7 @@
 <div>
     <div class="row">
         <div class="col-lg-12 w-100 pe-4 mx-auto">
-            <button class="btn btn-primary btn-lg float-end mb-1 fs-5"><i class="bx bx-plus"></i> Add New Facility</button>
+            <button class="btn btn-primary btn-lg float-end mb-1 fs-5" data-bs-toggle="modal" data-bs-target="#add-facility-modal" id="add-facility"><i class="bx bx-plus"></i> Add New Facility</button>
         </div>
     </div>
     <div class="row row-cols-xl-3 row-cols-lg-1 row-cols-sm-1 row-cols-md-3 p-3" id="facilities">
