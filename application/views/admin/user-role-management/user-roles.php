cce653eec9bbcb818a1a11f5afe8f0b1ac17e1c6
Albert Angelo Lupo <blaterwolf@gmail.com>
2023-01-09 01:02:35 +0800

    Adding Roles to Users Update 1

--- application/views/admin/user-role-management/user-roles.php
+++ application/views/admin/user-role-management/user-roles.php
@@ -1,45 +1,43 @@
 <div class="row">
-    <div class="col-lg-5">
-        <div class="card">
-            <div class="card-header d-flex">
-                <h5 class="card-title mb-0 flex-grow-1">Select Roles for Users</h5>
-            </div>
-            <div class="card-body justify-content-center text-center">
-                <div class="row my-3 py-3">
-                    <div class="col-lg h-100">
-                        <select class="js-example-basic-single" name="select-users" id="select-users">
-                            <option class="text-center" value="" selected disabled>- Select Users without Roles -</option>
-                        </select>
-                    <br></br>
-                    <select class="js-example-basic-multiple" name="select-role" id="select-role">
-                            <option class="text-center" value="" selected disabled>- Select Role -</option>
-                        </select>
-                    </div>
+    <div class="card">
+        <div class="card-header d-flex">
+            <h5 class="card-title mb-0 flex-grow-1">Select Roles for Users</h5>
+        </div>
+        <div class="card-body justify-content-center text-center">
+            <form id="selectRoleToUsersForm" class="needs-validation" novalidate>
+            <div class="row my-3 py-3">
+                <div class="col-lg h-100">
+                <select class="js-example-basic-single" name="select_users" id="select_users" placeholder="Select Roles">
+                    <option class="text-center" value="" selected disabled>- Select Users without Roles -</option>
+                </select>
+                <br></br>
+                <span>Select Multiple Roles:</span>
+                <select class="js-example-basic-multiple " multiple="multiple" name="select_roles" id="select_roles">
+                </select>
                 </div>
-                <button id="add-student-organizer" class="btn btn-primary waves-effect waves-light mt-2 mb-3">Add Roles</button>
             </div>
+            <button id="add_roles" class="btn btn-primary waves-effect waves-light mt-2 mb-3">Add Roles</button>
+            </form>
         </div>
     </div>
-    <div class="col-lg-7">
-        <div class="card">
-            <div class="card-header d-flex align-items-center">
-                <h5 class="card-title mb-0 flex-grow-1">Role Management</h5>
-            </div>
-            <div class="card-body">
-                <table id="users-with-roles" class="table table-bordered dt-responsive nowrap table-striped align-middle text-center w-100">
-                    <thead>
-                        <tr>
-                            <th>User ID</th>
-                            <th>Full Name</th>
-                            <th>User Type</th>
-                            <th>User Role</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    </tbody>
-                </table>
-            </div>
+    <div class="card">
+        <div class="card-header d-flex align-items-center">
+            <h5 class="card-title mb-0 flex-grow-1">Role Management</h5>
+        </div>
+        <div class="card-body">
+            <table id="users-with-roles" class="table table-bordered dt-responsive nowrap table-striped align-middle text-center w-100">
+                <thead>
+                    <tr>
+                        <th>User Number</th>
+                        <th>Full Name</th>
+                        <th>User Type</th>
+                        <th>User Role</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
         </div>
     </div>
     <!--end col-->
--- public/js/ajax/admin/user_roles/user-roles.ajax.js
+++ public/js/ajax/admin/user_roles/user-roles.ajax.js
@@ -1,7 +1,13 @@
 $(function () {
-	studentNoOrganizer()
-
 	viewRoleManagementTable()
+
+	populateSelect2()
+
+	$('#selectRoleToUsersForm').on('submit', function (e) {
+		e.preventDefault() // prevent page refresh
+		// pass data to API for updating of student's info
+		addRolesToUser()
+	})
 })
 
 viewStudentDetails = (user_id) => {
@@ -125,121 +131,89 @@ viewRoleManagementTable = () => {
 	}
 }
 
-// Populate select2 option with id student-no-organizer names of the students
-studentNoOrganizer = () => {
+// Populate select2 options
+populateSelect2 = () => {
+	// * First Phase: Populate select2 option: select_users which is a js-example-basic-single class
+	const select2Users = $('#select_users')
 	$.ajax({
-		url: apiURL + 'evrsers/pup_staff/view_student_no_organizer/',
+		url: apiURL + 'super_admin/users_without_roles',
 		type: 'GET',
 		headers: AJAX_HEADERS,
-		ContentType: 'application/x-www-form-urlencoded',
 		success: (data) => {
 			const students = data.data
-			const select2 = $('#student-no-organizer')
 			students.forEach((student) => {
-				// console.log(student)
 				const userNo = student.user_no
 				const fullName = student.user_profiles[0].full_name
-				const option = new Option(`${userNo} - ${fullName}`, student.user_no, false, false)
-				select2.append(option)
+				const option = new Option(`${userNo} - ${fullName}`, student.user_id, false, false)
+				select2Users.append(option)
 			})
-
-			// if option is selected and add-student-organizer button is clicked, get student.user_id
-			// and pass it to addStudentOrganizer function and prevent page from reloading
-			$('#add-student-organizer').on('click', function (e) {
-				// get selected option user_id
-				const selectedOption = select2.find(':selected')
-				const student = students.find((student) => student.user_no === selectedOption.val())
-				// prevent page from reloading
-				e.preventDefault()
-				addStudentOrganizer(student.user_id)
+		},
+	})
+	// * Second Phase: Populate select2 option: select_roles which is a js-example-basic-multiple
+	const select2Roles = $('#select_roles')
+	$.ajax({
+		url: apiURL + 'super_admin/role/',
+		type: 'GET',
+		headers: AJAX_HEADERS,
+		success: (data) => {
+			const roles = data.data.map((role) => {
+				return role.role_name
+			})
+			roles.forEach((role) => {
+				const option = new Option(role)
+				select2Roles.append(option)
 			})
 		},
 	})
 }
 
-addStudentOrganizer = (user_id) => {
-	$('#addStudentOrganizerModal').modal('show')
-	$('#add-organizer').on('click', function () {
-		$.ajaxSetup({
-			headers: {
-				Accept: 'application/json',
-				Authorization: 'Bearer ' + TOKEN,
-				ContentType: 'application/x-www-form-urlencoded',
-			},
-		})
-		$.ajax({
-			url: apiURL + `evrsers/pup_staff/set_organizer_role/${user_id}`,
-			type: 'PUT',
-			ContentType: 'application/x-www-form-urlencoded',
-			success: (result) => {
-				$('#addStudentOrganizerModal').modal('hide')
-				if (result) {
-					Swal.fire({
-						html: '<div class="mt-3"><lord-icon src="https://cdn.lordicon.com/lupuorrc.json" trigger="loop" colors="primary:#0ab39c,secondary:#405189" style="width:120px;height:120px"></lord-icon><div class="mt-4 pt-2 fs-15"><h4>Well done !</h4><p class="text-muted mx-4 mb-0">You have successfully added an organizer!</p></div></div>',
-						showCancelButton: !0,
-						showConfirmButton: !1,
-						cancelButtonClass: 'btn btn-success w-xs mb-1',
-						cancelButtonText: 'Ok',
-						buttonsStyling: !1,
-						showCloseButton: !0,
-					}).then(function () {
-						// reload viewStudentOrganizer table
-						viewStudentOrganizerStaff()
-					})
-				}
-			},
-		}).fail((xhr) => {
-			Swal.fire({
-				html: `<div class="mt-3"><lord-icon src="https://cdn.lordicon.com/tdrtiskw.json" trigger="loop" colors="primary:#f06548,secondary:#f7b84b" style="width:120px;height:120px"></lord-icon><div class="mt-4 pt-2 fs-15"><h4>Something went Wrong!</h4><p class="text-muted mx-4 mb-0">${
-					JSON.parse(xhr.responseText).message
-				}</p></div></div>`,
-				showCancelButton: !0,
-				showConfirmButton: !1,
-				cancelButtonClass: 'btn btn-danger w-xs mb-1',
-				cancelButtonText: 'Dismiss',
-				buttonsStyling: !1,
-				showCloseButton: !0,
-			})
-		})
-	})
-}
+// * Add roles to users
+addRolesToUser = () => {
+	// ! Major Loophole: Kailangan ma-detect niya if yung user_id ay Student or PUP Staff
+	if ($('#selectRoleToUsersForm')[0].checkValidity()) {
+		const form = new FormData($('#selectRoleToUsersForm')[0])
 
-removeStudentOrganizer = (user_id) => {
-	$('#removeStudentOrganizerModal').modal('show')
-	$('#remove-organizer').on('click', function () {
-		$.ajaxSetup({
-			headers: {
-				Accept: 'application/json',
-				Authorization: 'Bearer ' + TOKEN,
-				ContentType: 'application/x-www-form-urlencoded',
-			},
-		})
+		let users_selected = form.get('select_users')
+		let roles_selected = []
+
+		for (var pair of form.entries()) {
+			if (pair[0] == 'select_roles') {
+				roles_selected.push(pair[1])
+			}
+			console.log(pair[0] + ': ' + pair[1])
+		}
+
+		roles_selected = roles_selected.join(';')
+		console.log(roles_selected)
+
+		data = {
+			roles: roles_selected,
+		}
+
 		$.ajax({
-			url: apiURL + `evrsers/pup_staff/set_organizer_role/${user_id}`,
-			type: 'PUT',
-			ContentType: 'application/x-www-form-urlencoded',
+			url: apiURL + `super_admin/user_role/${users_selected}`,
+			type: 'POST',
+			headers: AJAX_HEADERS,
+			data: data,
 			success: (result) => {
-				$('#removeStudentOrganizerModal').modal('hide')
-				if (result) {
-					Swal.fire({
-						html: '<div class="mt-3"><lord-icon src="https://cdn.lordicon.com/lupuorrc.json" trigger="loop" colors="primary:#0ab39c,secondary:#405189" style="width:120px;height:120px"></lord-icon><div class="mt-4 pt-2 fs-15"><h4>Well done !</h4><p class="text-muted mx-4 mb-0">You have successfully removed an organizer!</p></div></div>',
-						showCancelButton: !0,
-						showConfirmButton: !1,
-						cancelButtonClass: 'btn btn-success w-xs mb-1',
-						cancelButtonText: 'Ok',
-						buttonsStyling: !1,
-						showCloseButton: !0,
-					}).then(function () {
-						// reload viewStudentOrganizer table
-						viewStudentOrganizerStaff()
-					})
-				}
+				Swal.fire({
+					html: '<div class="mt-3"><lord-icon src="https://cdn.lordicon.com/lupuorrc.json" trigger="loop" colors="primary:#0ab39c,secondary:#405189" style="width:120px;height:120px"></lord-icon><div class="mt-4 pt-2 fs-15"><h4>Well done!</h4><p class="text-muted mx-4 mb-0">You have successfully added a new role to this user!</p></div></div>',
+					showCancelButton: !0,
+					showConfirmButton: !1,
+					cancelButtonClass: 'btn btn-success w-xs mb-1',
+					cancelButtonText: 'Ok',
+					buttonsStyling: !1,
+					showCloseButton: !0,
+				}).then(function () {
+					setTimeout(() => {
+						location.reload()
+					}, 1000)
+				})
 			},
 		}).fail((xhr) => {
+			console.log(xhr.responseJSON.message)
 			Swal.fire({
-				html: `<div class="mt-3"><lord-icon src="https://cdn.lordicon.com/tdrtiskw.json" trigger="loop" colors="primary:#f06548,secondary:#f7b84b" style="width:120px;height:120px"></lord-icon><div class="mt-4 pt-2 fs-15"><h4>Something went Wrong!</h4><p class="text-muted mx-4 mb-0">${
-					JSON.parse(xhr.responseText).message
-				}</p></div></div>`,
+				html: '<div class="mt-3"><lord-icon src="https://cdn.lordicon.com/tdrtiskw.json" trigger="loop" colors="primary:#f06548,secondary:#f7b84b" style="width:120px;height:120px"></lord-icon><div class="mt-4 pt-2 fs-15"><h4>Something went Wrong!</h4><p class="text-muted mx-4 mb-0">There was an error while adding a new role to this user. Please try again.</p></div></div>',
 				showCancelButton: !0,
 				showConfirmButton: !1,
 				cancelButtonClass: 'btn btn-danger w-xs mb-1',
@@ -248,5 +222,5 @@ removeStudentOrganizer = (user_id) => {
 				showCloseButton: !0,
 			})
 		})
-	})
+	}
 }
