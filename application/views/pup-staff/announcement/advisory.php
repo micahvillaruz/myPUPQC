f16f22b2eef478f90f2222e39436c80e959cfddc
Albert Angelo Lupo <blaterwolf@gmail.com>
2023-01-11 13:57:41 +0800

    Added My Advisories AJAX Phase 2

--- application/views/pup-staff/announcement/advisory.php
+++ application/views/pup-staff/announcement/advisory.php
@@ -19,7 +19,7 @@
                     </div>
                 </div>
                 <div class="card-body">
-                    <form action="javascript:void(0);" class="row g-3">
+                    <form id="NewAdvisory" action="javascript:void(0);" class="row g-3">
                         <div class="col-md-12">
                             <label for="announcement_title" class="form-label">Title <span class="text-danger">*</span></label>
                             <input type="text" class="form-control" id="announcement_title" placeholder="Enter the title of the advisory">
@@ -95,26 +95,6 @@
                                         <th>Action</th>
                                     </tr>
                                 </thead>
-                                <tbody>
-                                    <tr>
-                                        <td width="30%" class="text-wrap text-center"><a href="#" class="fw-medium link-primary">Admission Services Advisory for Transferees (PUP QC)</a></td>
-                                        <td width='50%' class="text-wrap">The Polytechnic University of the Philippines Main Campus will start accepting application to transfer from August 8 to 12, 2022.</td>
-                                        <td>20 Dec, 2021, <small class="text-muted">02:21 AM</small></td>
-                                        <td class="text-center">
-                                            <div class="d-flex gap-1 justify-content-center">
-                                                <button type="button" class="btn btn-info btn-icon waves-effect waves-light">
-                                                    <i class="ri-eye-fill"></i>
-                                                </button>
-                                                <button type="button" class="btn btn-warning btn-icon waves-effect waves-light">
-                                                    <i class="ri-edit-2-fill"></i>
-                                                </button>
-                                                <button type="button" class="btn btn-danger btn-icon waves-effect waves-light">
-                                                    <i class="ri-delete-bin-fill"></i>
-                                                </button>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                </tbody>
                             </table>
                         </div>
                     </div>
--- public/js/ajax/pup-staff/annsys/advisory.ajax.js
+++ public/js/ajax/pup-staff/annsys/advisory.ajax.js
@@ -4,14 +4,14 @@ $(() => {
 
 loadAdvisoryTables = () => {
 	// * Phase 1: Populate all_advisories_table (except user's own advisory)
-	const dt = $('#all_advisories_table')
+	const all_advisories_dt = $('#all_advisories_table')
 
 	$.ajaxSetup({
 		headers: AJAX_HEADERS,
 	})
 
-	if (dt.length) {
-		dt.DataTable({
+	if (all_advisories_dt.length) {
+		all_advisories_dt.DataTable({
 			bDestroy: true,
 			ajax: {
 				url: apiURL + 'annsys/pup_staff/get_all_advisory',
@@ -25,7 +25,9 @@ loadAdvisoryTables = () => {
 					class: 'text-wrap text-center',
 					render: (data) => {
 						const advisoryTitle = data.announcement_title
-						return `${advisoryTitle}`
+						const advisoryLink = data.announcement_link
+						const link = `<a href="${advisoryLink}">${advisoryTitle}</a>`
+						return `${link}`
 					},
 				},
 				// * Advisory Description
@@ -84,5 +86,84 @@ loadAdvisoryTables = () => {
 	}
 
 	// * Phase 2: Populate my_advisories_table (user's own advisory)
-	$.ajax({})
+	const my_advisories_dt = $('#my_advisories_table')
+
+	if (my_advisories_dt.length) {
+		my_advisories_dt.DataTable({
+			bDestroy: true,
+			ajax: {
+				url: apiURL + 'annsys/pup_staff/get_advisory_from_user',
+				type: 'GET',
+				headers: AJAX_HEADERS,
+			},
+			columns: [
+				// * Advisory Title
+				{
+					data: null,
+					width: '40%',
+					class: 'text-wrap text-center',
+					render: (data) => {
+						const advisoryTitle = data.announcement_title
+						const advisoryLink = data.announcement_link
+						const link = `<a href="${advisoryLink}">${advisoryTitle}</a>`
+						return `${link}`
+					},
+				},
+				// * Advisory Description
+				{
+					data: null,
+					width: '40%',
+					class: 'text-wrap',
+					render: (data) => {
+						const advisoryDescription = data.announcement_description
+						return `${advisoryDescription}`
+					},
+				},
+				// * Create Date
+				{
+					data: null,
+					render: (data) => {
+						let date = new Date(data.created_at)
+						const article_date = date.toLocaleDateString('en-US', {
+							year: 'numeric',
+							month: 'long',
+							day: 'numeric',
+						})
+						const article_time = date.toLocaleTimeString('en-US', {
+							hour: 'numeric',
+							minute: 'numeric',
+							hour12: true,
+						})
+						return `${article_date}<br/>${article_time}`
+					},
+				},
+				// * Action
+				{
+					data: null,
+					render: (data) => {
+						const viewButton = `
+                        <button type="button" class="btn btn-info btn-icon waves-effect waves-light">
+                            <i class="ri-eye-fill"></i>
+                        </button>
+                        `
+						const editButton = `
+                        <button type="button" class="btn btn-warning btn-icon waves-effect waves-light">
+                            <i class="ri-edit-2-fill"></i>
+                        </button>
+                        `
+						const deleteButton = `
+                        <button type="button" class="btn btn-danger btn-icon waves-effect waves-light">
+                            <i class="ri-delete-bin-fill"></i>
+                        </button>
+                        `
+						return `<div class="text-center">
+                        ${viewButton} ${editButton} ${deleteButton}
+                        </div>
+                        `
+					},
+				},
+			],
+			order: [[0, 'asc']],
+		})
+	}
 }
