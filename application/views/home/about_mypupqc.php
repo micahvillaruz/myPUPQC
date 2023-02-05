db2c1e36d822a6c3066f8b3db36fc3d907fe609a
Micah Villaruz <micahmvillaruz@gmail.com>
2023-02-05 23:06:15 +0800

    Fix page format

--- application/views/home/about_mypupqc.php
+++ application/views/home/about_mypupqc.php
@@ -44,76 +44,69 @@
                   <div class="col-xxl-9">
                     <div class="card">
                       <div class="card-body">
-                      <div class="m-3">
-                        <h2 class="mb-3 text-primary">About myPUPQC</h2>
-                            <p>
-                              myPUPQC is the official website and portal of the Polytechnic University of the Phillipines - Quezon City Branch.
-                              It serve as an effective medium to reach out to an interconnected world, giving the online 
-                              community better access to University information, activities, news and insights.
-                            </p>
-                            <br>
-                            <h4 class="mb-3 text-primary">Managing myPUPQC</h4>
-                            <p>
-                              We never expect that myPUPQC would go on a long way since it only started as a college capstone project.
-                              It has been an experience getting this site into what it is today and we want to share those experiences with you,
-                              so perhaps you'll be able to apply them as you build and manage your own website.
-                            </p>
-                            <p>
-                              When the myPUPQC project intially launched in 2022, it was designed to display basic
-                              information about the University. The site was then hosted via Name.com. As the site took 
-                              of in February 2022, more and more responses and requests began filling its online guestbook.
-                              In the later months, a number of pages containing relevant information about the University were added.
-                            </p>
-                            <p>
-                              Today, myPUPQC continues to evolve dramatically as it gives its visitors information that is
-                              relevant and useful to them and the website and its social network channels has matured into 
-                              one of the University's most important medium for information dissemination. Our visitors
-                              send us feedback through email or website survey. That information helps us focus on what
-                              content and features to further develop for an improved myPUPQC experience.
-                            </p>
-                            <p>
-                              The name is stylized as "myPUPQC" in reference to my.harvard, Harvard University's Student, Teaching, Advising, or Administrative Portal. It was also a reference to myUSTe, 
-                              University of Santo Tomas' version of Student Self-Service Portal. This website idea took primarily in ideas of those systems thus in the succeeding updates, we want to cater 
-                              this system to everyone in PUP Quezon City
-                            </p>
-                            <br></br>
-                            <h3 class="mb-3 text-primary">myPUPQC Beta Preview</h3>
-                            <p>
-                              myPUPQC is so far the best online presentation of the Universities of all branches branding and service offerings.
-                              You will notice a modern and profeesional design with functionality, ease-of-use, accessibility, and service in mind,
-                              with focus on the clients of the University. It is built using the latest Web Standards (HTML5, CSS3) and applies a Responsive Web Design (RWD) 
-                              approach which is aimed at providing an optimal viewing experience - easy reading and navigation - across a wide range of devices (from mobile devices to desktop computer monitors).
-                            </p>
-                            <p>
-                              Other improvements include:
-                            <ul class="mx-3">
-                              <li> Streamlined display of information on the homepage and Web pages for easy reading;</li>
-                              <li> Bigger hero banner to showcase the University;</li>
-                              <li> Summary representation of active articles to facilitate quick reading with link to read on to details;</li>
-                              <li> A more simple (and straightforward) site menu for fast and easy navigation;</li>
-                              <li> Social media widgets and buttons so that visitors can follow us through our official Facebook, Twitter, LinkedIn, and YouTube channel.</li>
-                            </ul>
-                              myPUPQC will always be a work in progress. We will continue to update it regularly with more features and functionalities. We always welcome and highly appreciate visitor feedback as we continue to improve it.
-                            </p>
-
-                            <br></br>
-                            <h4 class="mb-3 text-primary">myPUPQC Beta Preview</h4>
-                            <p>
-                              The following technologies and solutions made it possible to build myPUPQC:
-                            <ul class="mx-3">
-                              <li> <a href="https://themesbrand.com/velzon/html/default/index.html" target="_blank"> Velzon</a>  with Regular License for the basis of the site template</li>
-                              <li> <a href="https://jquery.com/" target="_blank"> jQuery</a> for the things like HTML document traversal and manipulation, event handling, animation, and Ajax much simpler with an easy-to-use API that works across a multitude of browsers. With a combination of versatility and extensibility, jQuery has changed the way that millions of people write JavaScript.</li>
-                              <li> <a href="https://codeigniter.com/" target="_blank"> CodeIgniter3</a> and <a href="https://www.php.net/" target="_blank"> PHP</a> for web application framework</li>
-                              <li> HTML5 and CSS3 for the frontend framework</li>
-                              <li> Azure for the Image Hosting and DevOps Operations</li>
-                            </ul>
-                            </p>
+                        <div class="mx-3">
+                          <h3 class="mb-3 text-primary">About myPUPQC</h3>
+                          <p>
+                            myPUPQC is the official website and portal of the Polytechnic University of the Phillipines - Quezon City Branch.
+                            It serve as an effective medium to reach out to an interconnected world, giving the online
+                            community better access to University information, activities, news and insights.
+                          </p>
+                          <h4 class="mt-4 mb-3 text-primary">Managing myPUPQC</h4>
+                          <p>
+                            We never expect that myPUPQC would go on a long way since it only started as a college capstone project.
+                            It has been an experience getting this site into what it is today and we want to share those experiences with you,
+                            so perhaps you'll be able to apply them as you build and manage your own website.
+                          </p>
+                          <p>
+                            When the myPUPQC project intially launched in 2022, it was designed to display basic
+                            information about the University. The site was then hosted via Name.com. As the site took
+                            of in February 2022, more and more responses and requests began filling its online guestbook.
+                            In the later months, a number of pages containing relevant information about the University were added.
+                          </p>
+                          <p>
+                            Today, myPUPQC continues to evolve dramatically as it gives its visitors information that is
+                            relevant and useful to them and the website and its social network channels has matured into
+                            one of the University's most important medium for information dissemination. Our visitors
+                            send us feedback through email or website survey. That information helps us focus on what
+                            content and features to further develop for an improved myPUPQC experience.
+                          </p>
+                          <p>
+                            The name is stylized as "myPUPQC" in reference to my.harvard, Harvard University's Student, Teaching, Advising, or Administrative Portal. It was also a reference to myUSTe,
+                            University of Santo Tomas' version of Student Self-Service Portal. This website idea took primarily in ideas of those systems thus in the succeeding updates, we want to cater
+                            this system to everyone in PUP Quezon City
+                          </p>
+                          <h3 class="mt-5 mb-3 text-primary">myPUPQC Beta Preview</h3>
+                          <p>
+                            myPUPQC is so far the best online presentation of the Universities of all branches branding and service offerings.
+                            You will notice a modern and profeesional design with functionality, ease-of-use, accessibility, and service in mind,
+                            with focus on the clients of the University. It is built using the latest Web Standards (HTML5, CSS3) and applies a Responsive Web Design (RWD)
+                            approach which is aimed at providing an optimal viewing experience - easy reading and navigation - across a wide range of devices (from mobile devices to desktop computer monitors).
+                          </p>
+                          <p>
+                            Other improvements include:
+                          <ul class="mx-3">
+                            <li> Streamlined display of information on the homepage and Web pages for easy reading;</li>
+                            <li> Bigger hero banner to showcase the University;</li>
+                            <li> Summary representation of active articles to facilitate quick reading with link to read on to details;</li>
+                            <li> A more simple (and straightforward) site menu for fast and easy navigation;</li>
+                            <li> Social media widgets and buttons so that visitors can follow us through our official Facebook, Twitter, LinkedIn, and YouTube channel.</li>
+                          </ul>
+                          myPUPQC will always be a work in progress. We will continue to update it regularly with more features and functionalities. We always welcome and highly appreciate visitor feedback as we continue to improve it.
+                          </p>
+                          <h4 class="mt-4 mb-3 text-primary">Credits</h4>
+                          <p>
+                            The following technologies and solutions made it possible to build myPUPQC:
+                          <ul class="mx-3">
+                            <li> <a href="https://themesbrand.com/velzon/html/default/index.html" target="_blank"> Velzon</a> with Regular License for the basis of the site template</li>
+                            <li> <a href="https://jquery.com/" target="_blank"> jQuery</a> for the things like HTML document traversal and manipulation, event handling, animation, and Ajax much simpler with an easy-to-use API that works across a multitude of browsers. With a combination of versatility and extensibility, jQuery has changed the way that millions of people write JavaScript.</li>
+                            <li> <a href="https://codeigniter.com/" target="_blank"> CodeIgniter3</a> and <a href="https://www.php.net/" target="_blank"> PHP</a> for web application framework</li>
+                            <li> HTML5 and CSS3 for the frontend framework</li>
+                            <li> Azure for the Image Hosting and DevOps Operations</li>
+                          </ul>
+                          </p>
                         </div>
                       </div>
                     </div>
-                      <!--end card-body-->
-                    </div>
-                    <!-- end card -->
                   </div>
                   <!--end col-->
                   <div class="col-xxl-3">
@@ -125,15 +118,15 @@
                           </div>
                         </div>
                         <div>
-                        <ul class="list-group list-group-flush">
+                          <ul class="list-group list-group-flush">
                             <li class="list-group-item">
-                              <a class ="text-muted" href="<?= base_url() ?>terms-of-use">Terms of Use</a>
+                              <a class="text-muted" href="<?= base_url() ?>terms-of-use">Terms of Use</a>
                             </li>
                             <li class="list-group-item text-muted">
-                              <a class ="text-muted" href="<?= base_url() ?>privacy-statement">Privacy Statement</a>
+                              <a class="text-muted" href="<?= base_url() ?>privacy-statement">Privacy Statement</a>
                             </li>
                             <li class="list-group-item text-muted">
-                              <a class ="text-muted" href="<?= base_url() ?>home/about_mypupqc">About myPUPQC</a>
+                              <a class="text-muted" href="<?= base_url() ?>home/about_mypupqc">About myPUPQC</a>
                             </li>
                           </ul>
                         </div>
@@ -141,18 +134,12 @@
                       <!-- end card body -->
                     </div>
                     <!--end card-->
+                  </div>
                 </div>
-                <!--end row-->
               </div>
             </div>
-            <!--end col-->
           </div>
-          <!--end row-->
         </div>
-        <!-- container-fluid -->
       </div>
-      <!-- End Page-content -->
     </div>
-    <!-- end main content-->
   </div>
-  <!-- END layout-wrapper -->
