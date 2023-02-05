311c1d92a769f15a638d71b89a720da57535537d
Zsyrhill Soria <zsyrhillsoria@gmail.com>
2023-02-06 03:10:29 +0800

    Added Facility Capacity

--- application/views/admin/evrsers/scripts/facilities-scripts.php
+++ application/views/admin/evrsers/scripts/facilities-scripts.php
@@ -1,26 +1,32 @@
-<!-- jquery cdn -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
-<!-- App favicon -->
-<link rel="shortcut icon" href="../../public/images/favicon.ico" />
-<!-- Layout config Js -->
-<script src="../../public/js/layout.js"></script>
-<!-- Bootstrap Css -->
-<link href="../../public/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-<!-- Icons Css -->
-<link href="../../public/css/icons.min.css" rel="stylesheet" type="text/css" />
-<!-- App Css-->
-<link href="../../public/css/app.min.css" rel="stylesheet" type="text/css" />
-<!-- custom Css-->
-<link href="../../public/css/custom.min.css" rel="stylesheet" type="text/css" />
+<!--datatable js-->
+<script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
+<script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.2.2/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.print.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.html5.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
 
-<!-- JAVASCRIPT -->
-<script src="../../public/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?= base_url() ?>public/js/pages/datatables.init.js"></script>
 
-<script src="../../public/js/app.js"></script>
+<!-- validation init -->
+<script src="<?= base_url() ?>public/js/pages/form-validation.init.js"></script>
 
-<script src="../../public/js/ajax/admin/evrsers/facilities.ajax.js"></script>
+<!-- flatpickr.js -->
+<script type='text/javascript' src='<?= base_url() ?>public/libs/flatpickr/flatpickr.min.js'></script>
 
-</body>
+<!-- form wizard init -->
+<script src="<?= base_url() ?>public/js/pages/form-wizard.init.js"></script>
 
-</html>
+<!-- Sweet alert init js-->
+<script src="<?= base_url() ?>public/js/pages/sweetalerts.init.js"></script>
+
+<!-- student ajax -->
+<script src="<?= base_url() ?>public/js/ajax/admin/evrsers/facilities.ajax.js"></script>
+
+<!-- App js -->
+<script src="<?= base_url() ?>public/js/app.js"></script>
