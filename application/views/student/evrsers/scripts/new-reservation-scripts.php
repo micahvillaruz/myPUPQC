37d9751906c6c7b62c51df9d082170fc48f88b29
Zsyrhill Soria <zsyrhillsoria@gmail.com>
2023-02-13 12:19:04 +0800

    Added flatpickr in scripts

--- application/views/student/evrsers/scripts/new-reservation-scripts.php
+++ application/views/student/evrsers/scripts/new-reservation-scripts.php
@@ -1,19 +1,26 @@
 <!-- scripts -->
-<script src="../../public/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="../../public/libs/simplebar/simplebar.min.js"></script>
-<script src="../../public/libs/node-waves/waves.min.js"></script>
-<script src="../../public/libs/feather-icons/feather.min.js"></script>
-<script src="../../public/js/pages/plugins/lord-icon-2.1.0.js"></script>
-<script src="../../public/js/plugins.js"></script>
+<script src="<?= base_url() ?>public/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?= base_url() ?>public/libs/simplebar/simplebar.min.js"></script>
+<script src="<?= base_url() ?>public/libs/node-waves/waves.min.js"></script>
+<script src="<?= base_url() ?>public/libs/feather-icons/feather.min.js"></script>
+<script src="<?= base_url() ?>public/js/pages/plugins/lord-icon-2.1.0.js"></script>
+<script src="<?= base_url() ?>ublic/js/plugins.js"></script>
+
+<!-- form picker js -->
+<script src="../../public/js/pages/form-pickers.init.js"></script>
+<!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css"> -->
+<script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
 
 <!-- form wizard init -->
-<script src="../../public/js/pages/form-wizard.init.js"></script>
+<script src="<?= base_url() ?>public/js/pages/form-wizard.init.js"></script>
 
 <!-- form validation script -->
 <script src="<?= base_url() ?>public/js/pages/form-validation.init.js"></script>
 
-<!-- App js -->
-<script src="../../public/js/app.js"></script>
-
 <!-- new reservations ajax -->
 <script src="<?= base_url() ?>public/js/ajax/student/evrsers/evrsersnew.ajax.js"></script>
+
+<!-- App js -->
+<script src="<?= base_url() ?>public/js/app.js"></script>
+
+</body>
