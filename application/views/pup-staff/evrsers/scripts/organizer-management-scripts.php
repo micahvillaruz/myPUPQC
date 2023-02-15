569f2f23945b63711372e026e4afe68aa1261307
Zsyrhill Soria <zsyrhillsoria@gmail.com>
2023-02-15 17:14:48 +0800

    Modified organizer management scripts

--- application/views/pup-staff/evrsers/scripts/organizer-management-scripts.php
+++ application/views/pup-staff/evrsers/scripts/organizer-management-scripts.php
@@ -1,5 +1,6 @@
 <!--jquery cdn-->
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
 <!--select2 cdn-->
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
