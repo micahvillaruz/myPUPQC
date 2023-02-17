18fb543fa284e1754da2a3a4b9f5fc436ff039e2
Zsyrhill Soria <zsyrhillsoria@gmail.com>
2023-02-17 19:33:27 +0800

    Added reservation approval checker UI

---
+++ application/views/pup-staff/evrsers/reservation-approval.php
@@ -0,0 +1,8 @@
+<div id="has_no_signatory_requests" class="row my-5 py-5">
+    <div class="py-5 col-12 d-flex justify-content-center align-items-center text-center flex-column gap-2">
+        <lord-icon src="https://cdn.lordicon.com/kdookkse.json" trigger="loop-on-hover" style="width:180px;height:180px"></lord-icon>
+        <h4 class="fw-bold">No Reservations To Sign</h4>
+        <p class="fs-15">You currently have no reservations to sign. <br> Please contact EVRSERS support if you think this is a mistake or click <span class="fw-medium">"View Reservations"</span> button below.</p>
+        <a href="<?= base_url() ?>pupstaff/evrsers/manage-reservations" role="button" class="btn btn-primary bg-gradient waves-effect waves-light">View Reservations</a>
+    </div>
+</div>
