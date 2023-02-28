2cbe6a8ed7d5041b202d4f6e812b98a14f8748d2
Mico Arevalo <69579685+MicoAye13@users.noreply.github.com>
2023-02-28 16:13:13 +0800

    Replace sweetalert with toast in admin settings

--- public/js/ajax/admin/settings.ajax.js
+++ public/js/ajax/admin/settings.ajax.js
@@ -14,6 +14,18 @@ $(function () {
 	})
 })
 
+const Toast = Swal.mixin({
+	toast: true,
+	position: 'top-end',
+	showConfirmButton: false,
+	timer: 2000,
+	timerProgressBar: true,
+	didOpen: (toast) => {
+		toast.addEventListener('mouseenter', Swal.stopTimer)
+		toast.addEventListener('mouseleave', Swal.resumeTimer)
+	},
+})
+
 // Edit Admin Details
 editProfile = () => {
 	$.ajax({
@@ -76,14 +88,9 @@ editProfileAJAX = () => {
 			headers: AJAX_HEADERS,
 			success: (result) => {
 				if (result) {
-					Swal.fire({
-						html: '<div class="mt-3"><lord-icon src="https://cdn.lordicon.com/lupuorrc.json" trigger="loop" colors="primary:#0ab39c,secondary:#405189" style="width:120px;height:120px"></lord-icon><div class="mt-4 pt-2 fs-15"><h4>Well done !</h4><p class="text-muted mx-4 mb-0">You have successfully updated your information!</p></div></div>',
-						showCancelButton: !0,
-						showConfirmButton: !1,
-						cancelButtonClass: 'btn btn-success w-xs mb-1',
-						cancelButtonText: 'Ok',
-						buttonsStyling: !1,
-						showCloseButton: !0,
+					Toast.fire({
+						icon: 'success',
+						title: 'Update Profile Successfully!',
 					}).then(function () {
 						loadProfile()
 					})
@@ -123,14 +130,9 @@ changePasswordAJAX = () => {
 		success: (result) => {
 			console.log(result)
 			if (result) {
-				Swal.fire({
-					html: '<div class="mt-3"><lord-icon src="https://cdn.lordicon.com/lupuorrc.json" trigger="loop" colors="primary:#0ab39c,secondary:#405189" style="width:120px;height:120px"></lord-icon><div class="mt-4 pt-2 fs-15"><h4>Well done!</h4><p class="text-muted mx-4 mb-0">You have successfully updated your password!</p></div></div>',
-					showCancelButton: !0,
-					showConfirmButton: !1,
-					cancelButtonClass: 'btn btn-success w-xs mb-1',
-					cancelButtonText: 'Ok',
-					buttonsStyling: !1,
-					showCloseButton: !0,
+				Toast.fire({
+					icon: 'success',
+					title: 'Change Password Successfully!',
 				}).then(function () {
 					window.location.reload()
 				})
