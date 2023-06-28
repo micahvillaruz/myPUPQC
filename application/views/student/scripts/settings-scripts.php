eccc9dec6590c01ab5a24ab618e73534f27b98b1
Albert Angelo Lupo <blaterwolf@gmail.com>
2023-06-28 12:50:43 +0800

    MQC-4: Logout Upon Change of Password

--- application/views/student/scripts/settings-scripts.php
+++ application/views/student/scripts/settings-scripts.php
@@ -16,12 +16,9 @@
 		<!-- Settings ajax -->
 		<script src="<?= base_url() ?>public/js/ajax/student/settings.ajax.js"></script>
 
-		<!-- Changepassword and logout js -->
-		<script src="<?= base_url() ?>public/js/common.js"></script>
-
 		<!-- App js -->
 		<script src="<?= base_url() ?>public/js/app.js"></script>
 
 		</body>
-		
+
 		</html>
