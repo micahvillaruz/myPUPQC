b0854fb1d2dc6fb137eda4e1771ff935255ad899
Racquel Ulibas <racquelfulibas@gmail.com>
2023-06-28 23:31:27 +0800

    Edit Announcement

--- public/js/ajax/pup-staff/annsys/announcement.ajax.js
+++ public/js/ajax/pup-staff/annsys/announcement.ajax.js
@@ -3,7 +3,10 @@ $(() => {
 
 	$('#NewAnnouncement').on('submit', function (e) {
 		e.preventDefault() // prevent page refresh
-		addAnnouncement()
+		if ($('#announcement_id').val() == "")
+			addAnnouncement()
+		else
+			updateAnnouncement();
 	})
 })
 
@@ -195,7 +198,7 @@ loadAnnouncementTables = () => {
                         </button>
                         `
 						const editButton = `
-                        <button type="button" class="btn btn-warning btn-icon waves-effect waves-light">
+                        <button type="button" class="btn btn-warning btn-icon waves-effect waves-light" onclick="editAnnouncement('${data.announcement_id}')">
                             <i class="ri-edit-2-fill"></i>
                         </button>
                         `
@@ -242,6 +245,60 @@ addAnnouncement = () => {
 				if (result) {
 					Swal.fire({
 						html: '<div class="mt-3"><lord-icon src="https://cdn.lordicon.com/lupuorrc.json" trigger="loop" colors="primary:#0ab39c,secondary:#405189" style="width:120px;height:120px"></lord-icon><div class="mt-4 pt-2 fs-15"><h4>Well done !</h4><p class="text-muted mx-4 mb-0">You have successfully added an announcement!</p></div></div>',
+						showCancelButton: !0,
+						showConfirmButton: !1,
+						cancelButtonClass: 'btn btn-success w-xs mb-1',
+						cancelButtonText: 'Ok',
+						buttonsStyling: !1,
+						showCloseButton: !0,
+					}).then(function () {
+						// reload Pending Reservations table
+						refreshPage()
+					})
+				}
+			},
+		}).fail((xhr) => {
+			Swal.fire({
+				html: `<div class="mt-3"><lord-icon src="https://cdn.lordicon.com/tdrtiskw.json" trigger="loop" colors="primary:#f06548,secondary:#f7b84b" style="width:120px;height:120px"></lord-icon><div class="mt-4 pt-2 fs-15"><h4>Something went Wrong!</h4><p class="text-muted mx-4 mb-0">${
+					JSON.parse(xhr.responseText).message
+				}</p></div></div>`,
+				showCancelButton: !0,
+				showConfirmButton: !1,
+				cancelButtonClass: 'btn btn-danger w-xs mb-1',
+				cancelButtonText: 'Dismiss',
+				buttonsStyling: !1,
+				showCloseButton: !0,
+			})
+		})
+	}
+}
+
+updateAnnouncement = () => {
+	if ($('#NewAnnouncement')[0].checkValidity()) {
+		// * No error in validation
+		const form = new FormData($('#NewAnnouncement')[0])
+		form.set('announcement_type', 'Announcement')
+		for (var pair of form.entries()) {
+			console.log(pair[0] + ': ' + pair[1])
+		}
+
+		const data = {
+			announcement_title: form.get('announcement_title'),
+			announcement_description: form.get('announcement_description'),
+			announcement_type: form.get('announcement_type'),
+			announcement_content: form.get('announcement_content'),
+		}
+
+		$.ajax({
+			url: apiURL + 'annsys/pup_staff/edit_announcement/' + $('#announcement_id').val(),
+			type: 'PUT',
+			headers: AJAX_HEADERS,
+			data: data,
+			dataType: 'json',
+			success: (result) => {
+				if (result) {
+					Swal.fire({
+						html: '<div class="mt-3"><lord-icon src="https://cdn.lordicon.com/lupuorrc.json" trigger="loop" colors="primary:#0ab39c,secondary:#405189" style="width:120px;height:120px"></lord-icon><div class="mt-4 pt-2 fs-15"><h4>Well done !</h4><p class="text-muted mx-4 mb-0">You have successfully updated an announcement!</p></div></div>',
 						showCancelButton: !0,
 						showConfirmButton: !1,
 						cancelButtonClass: 'btn btn-success w-xs mb-1',
@@ -325,6 +382,36 @@ changeAnnouncementStatus = (announcement_id, announcement_status) => {
 				},
 			})
 		}
+	})
+}
+
+editAnnouncement = (announcement_id) => {
+	$.ajax({
+		url: apiURL + 'annsys/pup_staff/get_announcement/' + announcement_id,
+		type: 'GET',
+		headers: AJAX_HEADERS,
+		success: (result) => {
+			if (result) {
+				$('#newAnnouncementBtn').trigger('click');
+				$('#newAnnouncementBtn').hide();
+				$('#addAnnouncementLabel').html('Edit Announcement');
+				$('#announcement_id').val(result.data.announcement_id);
+				$('#announcement_title').val(result.data.announcement_title);
+				$('#announcement_description').val(result.data.announcement_description);
+				$('#announcement_content').val(result.data.announcement_content);
+				tinymce.get("announcement_content").setContent(result.data.announcement_content);
+			}
+		},
+	}).fail((xhr) => {
+		Swal.fire({
+			html: `<div class="mt-3"><lord-icon src="https://cdn.lordicon.com/tdrtiskw.json" trigger="loop" colors="primary:#f06548,secondary:#f7b84b" style="width:120px;height:120px"></lord-icon><div class="mt-4 pt-2 fs-15"><h4>Something went Wrong!</h4><p class="text-muted mx-4 mb-0">${xhr.responseJSON.message}</p></div></div>`,
+			showCancelButton: !0,
+			showConfirmButton: !1,
+			cancelButtonClass: 'btn btn-danger w-xs mb-1',
+			cancelButtonText: 'Dismiss',
+			buttonsStyling: !1,
+			showCloseButton: !0,
+		})
 	})
 }
 
