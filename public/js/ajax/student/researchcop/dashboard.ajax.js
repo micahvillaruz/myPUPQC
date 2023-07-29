256f11a9c7f2ccff9df0015c427eed7b602a73d5
PapiChans <130991249+PapiChans@users.noreply.github.com>
2023-07-30 00:38:28 +0800

    ResearchCop: Search Engine

--- public/js/ajax/student/researchcop/dashboard.ajax.js
+++ public/js/ajax/student/researchcop/dashboard.ajax.js
@@ -9,11 +9,9 @@ fetchResearch = () => {
       dataType: 'json',
       headers: AJAX_HEADERS,
       success: (result) => {
-        const researchData = result.data;
+        const randomData = result.data;
 
         let display = $('#research_display')
-
-        const randomData = researchData.sort(() => Math.random() - 0.5);
 
         if (randomData.length > 0) {
           randomData.forEach((researchData) => {
@@ -66,38 +64,43 @@ fetchResearch = () => {
 
 search = () => {
       const query = $('#searchQuery').val();
-
-
+      let display = $('#research_display')
+      display.empty()
+      
+      if (!query) {
+        $('#search_result').html('<h5 class="fs-16 fw-semibold text-center mb-0">Search query is empty.</h5>');
+        return;
+      }
       $('#search_result').html(`
-      <h5 class="fs-16 fw-semibold text-center mb-0">Showing results for "<span class="text-primary fw-medium fst-italic">${query}</span> "</h5>
+      <h5 class="fs-16 fw-semibold text-center mb-0">Loading...</h5>
       `)
+
+      data = {query: $('#searchQuery').val()}
+
       // Perform the search and get the results using AJAX
       $.ajax({
-        url: apiURL + 'researchcop/dashboard/research',
-        type: 'GET',
+        url: apiURL + `researchcop/dashboard/research`,
+        type: 'POST',
+        data: data,
         dataType: 'json',
         headers: AJAX_HEADERS,
         success: (result) => {
-          const researchData = result.data
-          let display = $('#research_display')
-          display.empty()
-
-        const randomData = researchData.sort(() => Math.random() - 0.5);
+          const randomData = result.data
 
         if (randomData.length > 0) {
-          randomData.forEach((researchData) => {
-            let showViewButton = researchData.research_pdf != null;
-            let categ = researchData.research_category === 'Research';
-            let rtype = researchData.research_type === 'Copyrighted';
+          randomData.forEach((randomData) => {
+            let showViewButton = randomData.research_pdf != null;
+            let categ = randomData.research_category === 'Research';
+            let rtype = randomData.research_type === 'Copyrighted';
 
             let researchFormat = `
               <div class="card-body p-4"> 
                 <div class="tab-content text-muted">
                   <div class="tab-pane active" id="all" role="tabpanel">
                     <div class="pb-3">
-                      <h5 class="text-primary mb-1">${researchData.research_title}</h5>
-                      <p class="text-success mb-2">${researchData.research_author}</p>
-                      <p class="text-dark mb-2" style="width: 1000px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">${researchData.research_abstract}</p>
+                      <h5 class="text-primary mb-1">${randomData.research_title}</h5>
+                      <p class="text-success mb-2">${randomData.research_author}</p>
+                      <p class="text-dark mb-2" style="width: 1000px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">${randomData.research_abstract}</p>
                       <p class="text-primary mb-2">
                       ${categ
                         ? `<span class="badge rounded-pill bg-success">Research</span>`
@@ -107,23 +110,29 @@ search = () => {
                           : `<span class="badge rounded-pill bg-danger">Non-Copyrighted</span>`}
                         </p>
                       <div>
-                          <button type="button" class="btn btn-info btn-icon waves-effect waves-light" onclick="viewResearchRecord('${researchData.research_id}')" data-bs-toggle="modal" data-bs-target="#viewResearchInfo"><i class="ri-eye-fill fs-5"></i></button>
+                          <button type="button" class="btn btn-info btn-icon waves-effect waves-light" onclick="viewResearchRecord('${randomData.research_id}')" data-bs-toggle="modal" data-bs-target="#viewResearchInfo"><i class="ri-eye-fill fs-5"></i></button>
                           ${showViewButton
-                          ? `<button type="button" class="btn btn-success btn-label waves-effect waves-light" onclick="viewResearchRecord('${researchData.research_id}')" data-bs-toggle="modal" data-bs-target="#research_document_preview"><i class="ri-file-line label-icon align-middle fs-16 me-2"></i>View</button>`
+                          ? `<button type="button" class="btn btn-success btn-label waves-effect waves-light" onclick="viewResearchRecord('${randomData.research_id}')" data-bs-toggle="modal" data-bs-target="#research_document_preview"><i class="ri-file-line label-icon align-middle fs-16 me-2"></i>View</button>`
                           : ''
                           }
                       </div>
                     </div>
-                    <div class="border border-dashed"><p class="text-dark mb-2">${researchData.research_program}</p></div>
+                    <div class="border border-dashed"><p class="text-dark mb-2">${randomData.research_program}</p></div>
                   </div>
                 </div>
               </div>
             `;
             display.append(researchFormat);
+      
         });
+        $('#search_result').html(`
+      <h5 class="fs-16 fw-semibold text-center mb-0">Showing results for "<span class="text-primary fw-medium fst-italic">${query}</span> "</h5>
+      `)
       } else {
         // If there are no research records, display a message or handle the empty case as needed
-        display.append('No research records found.');
+        $('#search_result').html(`
+      <h5 class="fs-16 fw-semibold text-center mb-0">No Results Found.</h5>
+      `)
       }
     },
     error: () => {
