a8921c218980991d3ecc62d82c4d90f51ca3191f
codelala <93863020+Axyszce@users.noreply.github.com>
2023-07-30 11:52:20 +0800

    ResearchCop: Update the Author Page

--- public/js/ajax/student/researchcop/author.ajax.js
+++ public/js/ajax/student/researchcop/author.ajax.js
@@ -1,7 +1,26 @@
 $(function () {
 	loadResearchTable()
+
+	$('#editCoAuthorsForm').on('submit', function (e) {
+		e.preventDefault() // prevent page refresh
+		const research_id = $('#re_id').val()
+		editCoAuthorsAJAX(research_id)
+	})
     
 })
+
+const Toast = Swal.mixin({
+	toast: true,
+	position: 'top-end',
+	showConfirmButton: false,
+	timer: 2000,
+	timerProgressBar: true,
+	didOpen: (toast) => {
+		toast.addEventListener('mouseenter', Swal.stopTimer)
+		toast.addEventListener('mouseleave', Swal.resumeTimer)
+	},
+})
+
 // Load  research datatables
 loadResearchTable = () => {
 	const dt = $('#research-author-datatable')
@@ -34,7 +53,7 @@ loadResearchTable = () => {
 					class: 'text-center',
 					render: (data) => {
 						const rTitle = data.research_title
-						return `<div style="width: 400px; white-space: nowrap; overflow-y: auto;">${rTitle}</div>`
+						return `${rTitle}`
 					},
 				},
 
@@ -54,7 +73,8 @@ loadResearchTable = () => {
 					data: null,
 					class: 'text-center',
 					render: (data) => {
-
+					const rCoAuthor = data.research_co_author
+					return `${rCoAuthor}`
 					},
 				},
 
@@ -66,7 +86,7 @@ loadResearchTable = () => {
 					render: (data) => {
                         return `
                           <div class="dropdown d-inline-block">
-                              <button type="button" class="btn btn-warning btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#editCoAuthorModal" onclick="populateEditForm('${data.user_id}')"><i class="ri-pencil-fill fs-5"></i></button> 
+                              <button type="button" class="btn btn-warning btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#editCoAuthorModal" onclick="editCoAuthors('${data.research_id}')"><i class="ri-pencil-fill fs-5"></i></button> 
                           </div>
                               `
 					},
@@ -74,6 +94,75 @@ loadResearchTable = () => {
 				
 			],
 			order: [[0, 'asc']],
+		})
+	}
+}
+
+// View Research Record Modal
+viewResearchRecord = (research_id) => {
+	$.ajax({
+		type: 'GET',
+		cache: false,
+		url: apiURL + `researchcop/my-submissions/${research_id}`,
+		headers: AJAX_HEADERS,
+		dataType: 'json',
+		success: (result) => {
+			const researchRecord = result.data
+				// For edit/resubmit
+				$('#re_id').val(researchRecord.research_id)
+				$('#re_title').val(researchRecord.research_title)
+				$('#re_title').prop('disabled', true)
+				$('#re_author').val(researchRecord.research_author)
+				$('#re_author').prop('disabled', true)
+				$('#re_co_author').val(researchRecord.research_co_author)
+		},
+	})
+
+}
+
+editCoAuthors = (research_id) => viewResearchRecord(research_id)
+
+editCoAuthorsAJAX = (research_id) => {
+	// Enroll Student
+	if ($('#editCoAuthorsForm')[0].checkValidity()) {
+		// no validation error
+		const form = new FormData($('#editCoAuthorsForm')[0])
+
+		data = {
+			research_co_author: form.get('re_co_author'),
+		}
+
+		$.ajax({
+			url: apiURL + `researchcop/author/editcoauthors/${research_id}`,
+			type: 'PUT',
+			data: data,
+			dataType: 'json',
+			headers: AJAX_HEADERS,
+			success: (result) => {
+				if (result) {
+					Toast.fire({
+						icon: 'success',
+						title: 'Updated Co-Authors Successfully!',
+					}).then(function () {
+						// Hide the update student details modal
+						$('form#editCoAuthorsForm')[0].reset()
+						$('#editCoAuthorModal').modal('hide')
+
+						// Reload Student Datatable
+						loadResearchTable()
+					})
+				}
+			},
+		}).fail(() => {
+			Swal.fire({
+				html: '<div class="mt-3"><lord-icon src="https://cdn.lordicon.com/tdrtiskw.json" trigger="loop" colors="primary:#f06548,secondary:#f7b84b" style="width:120px;height:120px"></lord-icon><div class="mt-4 pt-2 fs-15"><h4>Something went Wrong !</h4><p class="text-muted mx-4 mb-0">There was an error while updating Co-authors. Please try again.</p></div></div>',
+				showCancelButton: !0,
+				showConfirmButton: !1,
+				cancelButtonClass: 'btn btn-danger w-xs mb-1',
+				cancelButtonText: 'Dismiss',
+				buttonsStyling: !1,
+				showCloseButton: !0,
+			})
 		})
 	}
 }
